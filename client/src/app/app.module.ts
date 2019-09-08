aedd1aa4c5a32a838983739403a7d8d288b36326
xishec <xishec@gmail.com>
2019-09-07 22:13:17 -0400

    XS : Install and Setup Angular Material, Angular CDK and Angular Animations

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,17 @@
-import {HttpClientModule} from '@angular/common/http';
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppComponent} from './components/app/app.component';
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./components/app/app.component";
+
+// *********** Material Angular ******************
+import { MatButtonModule } from "@angular/material/button";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+//************************************************
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-  ],
+  declarations: [AppComponent],
+  imports: [MatButtonModule, BrowserAnimationsModule, BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
