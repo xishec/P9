08bf24a563f0feb315af2d6d99d489ed6c310e1d
Louis Popovic <louispop@gmail.com>
2019-09-10 17:09:33 -0400

    LP: D Create abstract class stroke

---
+++ client/src/classes/Stroke/stroke.spec.ts
@@ -0,0 +1,7 @@
+import { Stroke } from '../Stroke/stroke';
+
+describe('Stroke', () => {
+  it('should create an instance', () => {
+    expect(new Stroke()).toBeTruthy();
+  });
+});
---
+++ client/src/classes/Stroke/stroke.ts
@@ -0,0 +1,13 @@
+import {Coordinate} from './coordinate';
+
+export abstract class Stroke {
+    coordinates: Coordinate[];
+    constructor(x: number, y: number) {
+        this.coordinates = []; // NOT SURE IF NECESSARY
+        this.addCoordinate(x, y);
+    }
+
+    addCoordinate(x: number, y: number) {
+        this.coordinates.push(new Coordinate(x, y));
+    }
+}
