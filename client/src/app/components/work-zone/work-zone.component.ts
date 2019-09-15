2e8cc0a64cfe60bb355acd475e3eb78ca184c129
HPay <hakimpayman@gmail.com>
2019-09-15 14:54:24 -0400

    HP : D Add Renderer2

--- client/src/app/components/work-zone/work-zone.component.ts
+++ client/src/app/components/work-zone/work-zone.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, Renderer2, ViewChild } from '@angular/core';
 
+import { RectangleTool } from 'src/app/classes/RectangleTool/rectangle-tool';
 import { DrawingInfo } from '../../../classes/DrawingInfo';
 import { DrawingModalWindowService } from '../../services/drawing-modal-window/drawing-modal-window.service';
 
@@ -13,7 +14,13 @@ export class WorkZoneComponent implements OnInit {
     drawingInfo: DrawingInfo = new DrawingInfo();
     displayNewDrawingModalWindow = false;
 
-    constructor(drawingModalWindowService: DrawingModalWindowService) {
+    // NOT CLEAN //
+    @ViewChild('svgpad', {static: true}) ref: ElementRef<SVGElement>;
+    // @ViewChild(DrawableDirective, {static: true}) drawDir: DrawableDirective;
+    private currentTool: RectangleTool;
+    // NOT CLEAN //
+
+    constructor(drawingModalWindowService: DrawingModalWindowService, private renderer: Renderer2) {
         this.drawingModalWindowService = drawingModalWindowService;
     }
 
@@ -24,6 +31,10 @@ export class WorkZoneComponent implements OnInit {
         this.drawingModalWindowService.currentDisplayNewDrawingModalWindow.subscribe((displayNewDrawingModalWindow) => {
             this.displayNewDrawingModalWindow = displayNewDrawingModalWindow;
         });
+
+        // NOT CLEAN //
+        this.currentTool = new RectangleTool(this.ref, this.renderer);
+        // NOT CLEAN //
     }
 
     changeStyle() {
@@ -34,4 +45,34 @@ export class WorkZoneComponent implements OnInit {
             width: this.drawingInfo.width,
         };
     }
+
+    // LISTENERS //
+    @HostListener('mousemove', ['$event']) onMouseMove(event: MouseEvent): void{
+        this.currentTool.onMouseMove(event);
+    }
+
+    @HostListener('mousedown', ['$event']) onMouseDown(event: MouseEvent): void{
+        this.currentTool.onMouseDown(event);
+    }
+
+    @HostListener('mouseup', ['$event']) onMouseUp(event: MouseEvent): void{
+        this.currentTool.onMouseUp(event);
+    }
+
+    @HostListener('mouseenter', ['$event']) onMouseEnter(event: MouseEvent): void{
+        this.currentTool.onMouseEnter(event);
+    }
+
+    @HostListener('mouseleave', ['$event']) onMouseLeave(event: MouseEvent): void{
+        this.currentTool.onMouseLeave(event);
+    }
+
+    @HostListener('keydown', ['$event']) onKeyDown(event: KeyboardEvent): void{
+        this.currentTool.onKeyDown(event);
+    }
+
+    @HostListener('keyup', ['$event']) onKeyUp(event: KeyboardEvent): void{
+        this.currentTool.onKeyUp(event);
+    }
+    // LISTENERS //
 }
