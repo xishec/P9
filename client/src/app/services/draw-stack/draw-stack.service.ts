53b055ee533b3076ae4dfd80a8d34f243a7732c1
HPay <hakimpayman@gmail.com>
2019-09-18 15:25:40 -0400

    HP : R Change attributes name

--- client/src/app/services/draw-stack/draw-stack.service.ts
+++ client/src/app/services/draw-stack/draw-stack.service.ts
@@ -4,31 +4,27 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class DrawStackService {
-    private drawStack_: SVGElement[];
-    private drawCount_: number;
+    private drawStack: SVGElement[];
+    drawCount: number;
 
   constructor() {
-      this.drawStack_ = new Array<SVGElement>();
-      this.drawCount_ = 0;
+      this.drawStack = new Array<SVGElement>();
+      this.drawCount = 0;
   }
 
   push(el: SVGElement): void{
-      this.drawStack_.push(el);
-      this.drawCount_++;
+      this.drawStack.push(el);
+      this.drawCount++;
   }
 
   pop(): void{
-    this.drawStack_.pop();
-    this.drawCount_--;
+    this.drawStack.pop();
+    this.drawCount--;
   }
 
   reset(): SVGElement[]{
-      const ret = this.drawStack_.splice(0, this.drawCount_);
-      this.drawCount_ = 0;
+      const ret = this.drawStack.splice(0, this.drawCount);
+      this.drawCount = 0;
       return ret;
-  }
-
-  get drawCount(): number{
-      return this.drawCount_;
   }
 }
