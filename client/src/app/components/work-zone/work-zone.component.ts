486a0fbcf16f89caf579542ea6fe23cf8f112a9b
HPay <hakimpayman@gmail.com>
2019-09-19 08:39:50 -0400

    HP : R Prettier and remove comments

--- client/src/app/components/work-zone/work-zone.component.ts
+++ client/src/app/components/work-zone/work-zone.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { DrawingInfo } from '../../../classes/DrawingInfo';
 import { DrawingModalWindowService } from '../../services/drawing-modal-window/drawing-modal-window.service';
 
-
 @Component({
     selector: 'app-work-zone',
     templateUrl: './work-zone.component.html',
@@ -13,9 +12,6 @@ export class WorkZoneComponent implements OnInit {
     drawingInfo: DrawingInfo = new DrawingInfo();
     displayNewDrawingModalWindow = false;
 
-    // @ViewChild('svgpad', {static: true}) ref: ElementRef<SVGElement>;
-    // private currentTool: AbstractShapeToolService;
-
     constructor(drawingModalWindowService: DrawingModalWindowService) {
         this.drawingModalWindowService = drawingModalWindowService;
     }
@@ -23,18 +19,10 @@ export class WorkZoneComponent implements OnInit {
     ngOnInit() {
         this.drawingModalWindowService.currentInfo.subscribe((drawingInfo) => {
             this.drawingInfo = drawingInfo;
-            // for(let el of this.drawStack.reset()) {
-            //     this.renderer.removeChild(this.ref, el);
-            // }
         });
         this.drawingModalWindowService.currentDisplayNewDrawingModalWindow.subscribe((displayNewDrawingModalWindow) => {
             this.displayNewDrawingModalWindow = displayNewDrawingModalWindow;
         });
-
-        // this.toolsService.currTool.subscribe(()=>{
-        //     this.currentTool = this.toolsService.getCurrentTool(/*this.drawStack,*/ this.ref, this.renderer);});
-
-        // this.currentTool = this.toolsService.getCurrentTool(/*this.drawStack,*/ this.ref, this.renderer);
     }
 
     changeStyle() {
@@ -45,34 +33,4 @@ export class WorkZoneComponent implements OnInit {
             width: this.drawingInfo.width,
         };
     }
-
-    // LISTENERS //
-    // @HostListener('mousemove', ['$event']) onMouseMove(event: MouseEvent): void{
-    //     this.currentTool.onMouseMove(event);
-    // }
-
-    // @HostListener('mousedown', ['$event']) onMouseDown(event: MouseEvent): void{
-    //     this.currentTool.onMouseDown(event);
-    // }
-
-    // @HostListener('window:mouseup', ['$event']) onMouseUp(event: MouseEvent): void{
-    //     this.currentTool.onMouseUp(event);
-    // }
-
-    // @HostListener('mouseenter', ['$event']) onMouseEnter(event: MouseEvent): void{
-    //     this.currentTool.onMouseEnter(event);
-    // }
-
-    // @HostListener('mouseleave', ['$event']) onMouseLeave(event: MouseEvent): void{
-    //     this.currentTool.onMouseLeave(event);
-    // }
-
-    // @HostListener('window:keydown', ['$event']) onKeyDown(event: KeyboardEvent): void{
-    //     this.currentTool.onKeyDown(event);
-    // }
-
-    // @HostListener('window:keyup', ['$event']) onKeyUp(event: KeyboardEvent): void{
-    //     this.currentTool.onKeyUp(event);
-    // }
-    // LISTENERS //
 }
