bd0cc48041c1f2f54211d6fa4c0deb0abf31d28b
HPay <hakimpayman@gmail.com>
2019-09-19 14:35:56 -0400

    HP : R Prettier again

--- client/src/app/services/tools/abstract-tools/abstract-shape-tool/abstract-shape-tool.service.spec.ts
+++ client/src/app/services/tools/abstract-tools/abstract-shape-tool/abstract-shape-tool.service.spec.ts
@@ -3,10 +3,10 @@ import { TestBed } from '@angular/core/testing';
 import { AbstractShapeToolService } from './abstract-shape-tool.service';
 
 describe('AbstractShapeToolService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+    beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: AbstractShapeToolService = TestBed.get(AbstractShapeToolService);
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        const service: AbstractShapeToolService = TestBed.get(AbstractShapeToolService);
+        expect(service).toBeTruthy();
+    });
 });
--- client/src/app/services/tools/rectangle-tool/rectangle-tool.service.spec.ts
+++ client/src/app/services/tools/rectangle-tool/rectangle-tool.service.spec.ts
@@ -3,10 +3,10 @@ import { TestBed } from '@angular/core/testing';
 import { RectangleToolService } from './rectangle-tool.service';
 
 describe('RectangleToolService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+    beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: RectangleToolService = TestBed.get(RectangleToolService);
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        const service: RectangleToolService = TestBed.get(RectangleToolService);
+        expect(service).toBeTruthy();
+    });
 });
