d885e64abfb123983fc90ffa38cf6bf03ae2004d
Louis Popovic <louispop@gmail.com>
2019-09-20 12:18:49 -0400

    LP : D Add function createSVGWrapper to brush-tool

--- client/src/app/services/tools/brush-tool/brush-tool.service.ts
+++ client/src/app/services/tools/brush-tool/brush-tool.service.ts
@@ -1,16 +1,38 @@
-import { Injectable } from '@angular/core';
+import { ElementRef, Injectable, Renderer2 } from '@angular/core';
 
+import { Mouse, SVG_NS } from '../../constants';
 import { TracingToolService } from '../abstract-tools/tracing-tool/tracing-tool.service';
 
 @Injectable({
     providedIn: 'root',
 })
 export class BrushToolService extends TracingToolService {
-    constructor() {
+    private currentPath = '';
+
+    private currentColor = 'black';
+    private svgPathRef = this.renderer.createElement('path', SVG_NS);
+    private svgWrapRef = this.renderer.createElement('svg', SVG_NS);
+
+    constructor(private renderer: Renderer2, private elementRef: ElementRef<SVGElement>) {
         super();
     }
 
-    onMouseDown(e: MouseEvent){}
+    onMouseDown(e: MouseEvent): void {
+        if (e.button === Mouse.LeftButton) {
+            super.onMouseDown(e);
+            this.createSVGWrapper();
+            //this.createPattern();
+            //this.createSVGCircle();
+            //this.createSVGPath();
+        }
+    }
+
+    createSVGWrapper(): void {
+        this.svgWrapRef = this.renderer.createElement('svg', SVG_NS);
+        this.renderer.appendChild(this.elementRef.nativeElement, this.svgWrapRef);
+    }
+
+    createSVGCircle(): void {}
 
     onMouseMove(e: MouseEvent){}
 }
