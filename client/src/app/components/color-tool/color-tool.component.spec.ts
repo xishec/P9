0ed27e47a6736b2c659c01b315ccf55b942ee74d
Roman <Romazho123@gmail.com>
2019-09-21 16:16:47 -0400

    RZ : T Add a new test.

--- client/src/app/components/color-tool/color-tool.component.spec.ts
+++ client/src/app/components/color-tool/color-tool.component.spec.ts
@@ -1,13 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ColorToolComponent } from './color-tool.component';
+//import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, FormsModule } from '@angular/forms';
 
-describe('ColorToolComponent', () => {
+fdescribe('ColorToolComponent', () => {
     let component: ColorToolComponent;
     let fixture: ComponentFixture<ColorToolComponent>;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
+            imports: [ReactiveFormsModule, FormsModule],
             declarations: [ColorToolComponent],
         }).compileComponents();
     }));
@@ -20,5 +22,13 @@ describe('ColorToolComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    let colortool: ColorToolComponent;
+    colortool = new ColorToolComponent(new FormBuilder());
+    it('should change primaryColorOn to true and return true', () => {
+        colortool.primaryColorOn = false;
+        colortool.chosePrimaryColor();
+        expect(colortool.primaryColorOn).toBeTruthy();
     });
 });
