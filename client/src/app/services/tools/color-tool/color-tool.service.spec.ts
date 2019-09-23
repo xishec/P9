cc0d9c057536f7754a12219a3ba2fb7dcf255470
Noboru Yoshida <noboruyoshida1997@gmail.com>
2019-09-23 17:30:33 -0400

    NY : D Create color tool service

---
+++ client/src/app/services/tools/color-tool/color-tool.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ColorToolService } from './color-tool.service';
+
+describe('ColorToolService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: ColorToolService = TestBed.get(ColorToolService);
+    expect(service).toBeTruthy();
+  });
+});
