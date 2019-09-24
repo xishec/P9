8d58c3f891605204e891e3222478e3cfd29330af
Noboru Yoshida <noboruyoshida1997@gmail.com>
2019-09-24 18:33:28 -0400

    NY : R Import variables and changeColor function from color tool component

--- client/src/app/services/tools/color-tool/color-tool.service.ts
+++ client/src/app/services/tools/color-tool/color-tool.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
 
 import { Color } from '../../../../classes/Color';
+import { COLORS, ColorType } from '../../../services/constants';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class ColorToolService {
+    readonly colors: Color[] = COLORS;
 
-  primaryColor: Color = new Color();
-  secondaryColor: Color = new Color();
-  lastTenColorsQueue: Color[] = [];
+    primaryColor: Color = new Color();
+    secondaryColor: Color = new Color('000000');
 
-  constructor() { }
+    changeColor(color: Color, colorType: ColorType) {
+      if (colorType === ColorType.primaryColor) {
+        this.primaryColor = color;
+      } else if (colorType === ColorType.secondaryColor) {
+        this.secondaryColor = color;
+      }
+    }
 }
