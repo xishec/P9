e9ab95c7e5b3d992df75af598f5f213a4f8b12fd
Xi Chen <xishec@gmail.com>
2019-09-25 18:57:54 -0400

    XS : D Update tool-service with BehaviorSubject

--- client/src/app/components/sidebar-tools/attribute-panel/color-attributes/color-attributes.component.ts
+++ client/src/app/components/sidebar-tools/attribute-panel/color-attributes/color-attributes.component.ts
@@ -4,13 +4,17 @@ import { Color } from '../../../../../classes/Color';
 import { ColorType, COLORS } from 'src/app/services/constants';
 import { ColorToolService } from '../../../../services/tools/color-tool/color-tool.service';
 
+interface ColorStyle {
+    backgroundColor: string;
+    border?: string;
+}
 @Component({
     selector: 'app-color-attributes',
     templateUrl: './color-attributes.component.html',
     styleUrls: ['./color-attributes.component.scss'],
 })
 export class ColorAttributesComponent implements OnInit {
-    selectedColor: ColorType = ColorType.primaryColor;
+    selectedColor: ColorType | undefined = undefined;
     showColorPalette = false;
 
     backgroundColor: Color = COLORS[0];
@@ -23,33 +27,59 @@ export class ColorAttributesComponent implements OnInit {
         this.backgroundColor = this.colorToolService.backgroundColor;
         this.primaryColor = this.colorToolService.primaryColor;
         this.secondaryColor = this.colorToolService.secondaryColor;
+        this.colorToolService.currentSelectedColor.subscribe((selectedColor: ColorType | undefined) => {
+            this.selectedColor = selectedColor;
+        });
     }
 
+    onClickBackgroundColor(): void {
+        this.colorToolService.changeSelectedColor(ColorType.backgroundColor);
+        this.showColorPalette = true;
+    }
     onClickPrimaryColor(): void {
-        this.selectedColor = ColorType.primaryColor;
+        this.colorToolService.changeSelectedColor(ColorType.primaryColor);
+        this.showColorPalette = true;
     }
-
     onClickSecondaryColor(): void {
-        this.selectedColor = ColorType.secondaryColor;
+        this.colorToolService.changeSelectedColor(ColorType.secondaryColor);
+        this.showColorPalette = true;
     }
 
-    getBackgroundColorIcon() {
+    getBackgroundColorIcon(): ColorStyle {
+        if (this.selectedColor === ColorType.backgroundColor) {
+            return {
+                backgroundColor: '#' + this.backgroundColor.hex,
+                border: 'solid 1px black',
+            };
+        }
         return {
             backgroundColor: '#' + this.backgroundColor.hex,
         };
     }
-    getPrimaryColorIcon() {
+    getPrimaryColorIcon(): ColorStyle {
+        if (this.selectedColor === ColorType.primaryColor) {
+            return {
+                backgroundColor: '#' + this.primaryColor.hex,
+                border: 'solid 1px black',
+            };
+        }
         return {
             backgroundColor: '#' + this.primaryColor.hex,
         };
     }
-    getSecondaryColorIcon() {
+    getSecondaryColorIcon(): ColorStyle {
+        if (this.selectedColor === ColorType.secondaryColor) {
+            return {
+                backgroundColor: '#' + this.secondaryColor.hex,
+                border: 'solid 1px black',
+            };
+        }
         return {
             backgroundColor: '#' + this.secondaryColor.hex,
         };
     }
 
     switchColors() {
-        this.showColorPalette = !this.showColorPalette;
+        this.showColorPalette = false;
     }
 }
