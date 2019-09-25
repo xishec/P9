65405ee313c96ac8d12f022eb8193406baba3c88
HPay <hakimpayman@gmail.com>
2019-09-24 22:59:07 -0400

    HP : F Change square behavior to snap on corner

--- client/src/app/services/tools/rectangle-tool/rectangle-tool.service.ts
+++ client/src/app/services/tools/rectangle-tool/rectangle-tool.service.ts
@@ -142,10 +142,10 @@ export class RectangleToolService extends AbstractShapeToolService {
                 'height',
                 this.previewRectangle.height.baseVal.valueAsString,
             );
-            this.renderer.setAttribute(this.drawRectangle, 'fill', this.fillColor.toString());
-            this.renderer.setAttribute(this.drawRectangle, 'stroke', this.strokeColor.toString());
-            this.renderer.setAttribute(this.drawRectangle, 'stroke-width', this.strokeWidth.toString());
         }
+        this.renderer.setAttribute(this.drawRectangle, 'fill', this.fillColor.toString());
+        this.renderer.setAttribute(this.drawRectangle, 'stroke', this.strokeColor.toString());
+        this.renderer.setAttribute(this.drawRectangle, 'stroke-width', this.strokeWidth.toString());
     }
 
     updatePreviewSquare(): void {
@@ -159,14 +159,14 @@ export class RectangleToolService extends AbstractShapeToolService {
             this.renderer.setAttribute(
                 this.drawRectangle,
                 'x',
-                (this.initialMouseX - deltaX + deltaX / 2 - minLen / 2).toString(),
+                (this.initialMouseX - minLen).toString(),
             );
             this.renderer.setAttribute(this.drawRectangle, 'width', minLen.toString());
         } else {
             this.renderer.setAttribute(
                 this.drawRectangle,
                 'x',
-                (this.initialMouseX + deltaX / 2 - minLen / 2).toString(),
+                (this.initialMouseX).toString(),
             );
             this.renderer.setAttribute(this.drawRectangle, 'width', minLen.toString());
         }
@@ -177,14 +177,14 @@ export class RectangleToolService extends AbstractShapeToolService {
             this.renderer.setAttribute(
                 this.drawRectangle,
                 'y',
-                (this.initialMouseY - deltaY + deltaY / 2 - minLen / 2).toString(),
+                (this.initialMouseY - minLen).toString(),
             );
             this.renderer.setAttribute(this.drawRectangle, 'height', minLen.toString());
         } else {
             this.renderer.setAttribute(
                 this.drawRectangle,
                 'y',
-                (this.initialMouseY + deltaY / 2 - minLen / 2).toString(),
+                (this.initialMouseY).toString(),
             );
             this.renderer.setAttribute(this.drawRectangle, 'height', minLen.toString());
         }
