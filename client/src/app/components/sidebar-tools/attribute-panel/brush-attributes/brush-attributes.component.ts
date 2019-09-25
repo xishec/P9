719252060bdc4ccd133a87a14d24e7bf01f3eb7d
Louis Popovic <louispop@gmail.com>
2019-09-24 20:01:37 -0400

    LP : D Add brushToolService to tool-selector.service and add brush-attributes.components

--- client/src/app/components/sidebar-tools/attribute-panel/brush-attributes/brush-attributes.component.ts
+++ client/src/app/components/sidebar-tools/attribute-panel/brush-attributes/brush-attributes.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+
+import { AttributesManagerService } from 'src/app/services/tools/attributes-manager/attributes-manager.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MIN_THICKNESS, DEFAULT_THICKNESS } from 'src/app/services/constants';
+import { ToolSelectorService } from 'src/app/services/tools/tool-selector/tool-selector.service';
 
 @Component({
   selector: 'app-brush-attributes',
   templateUrl: './brush-attributes.component.html',
-  styleUrls: ['./brush-attributes.component.scss']
+  styleUrls: ['./brush-attributes.component.scss'],
+  providers: [AttributesManagerService],
 })
-export class BrushAttributesComponent implements OnInit {
+export class BrushAttributesComponent implements OnInit, AfterViewInit {
+  toolName = 'Pinceau';
+  brushAttributesForm: FormGroup;
+  brushToolService: BrushToolService;
 
-  constructor() { }
+  readonly MIN_THICKNESS = MIN_THICKNESS;
+  readonly MAX_THICKNESS = MAX_THICKNESS;
 
-  ngOnInit() {
+  constructor(
+    private formBuilder: FormBuilder,
+    private attributesManagerService: AttributesManagerService,
+    private toolSelectorService: ToolSelectorService,
+  ) { 
+    this.formBuilder = formBuilder;
   }
 
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  ngAfterViewInit(): void {
+    thi.brushToolService = this.toolSelectorService.getBrushTool(); // hfbuyiefbay uigbfr
+  }
+
+
+  initializeForm(): void {
+    this.brushAttributesForm = this.formBuilder.group({
+      thickness : [
+        DEFAULT_THICKNESS,
+        [Validators.required, Validators.min(MIN_THICKNESS), Validators.max(this.MAX_THICKNESS),]
+      ]
+    })
+  }
 }
