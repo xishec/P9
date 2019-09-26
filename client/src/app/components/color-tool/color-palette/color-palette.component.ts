f6b0386aad0260201657df396addf2acca499892
Noboru Yoshida <noboruyoshida1997@gmail.com>
2019-09-26 16:30:37 -0400

    NY : D Create new color-palette component

---
+++ client/src/app/components/color-tool/color-palette/color-palette.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-color-palette',
+  templateUrl: './color-palette.component.html',
+  styleUrls: ['./color-palette.component.scss']
+})
+export class ColorPaletteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
