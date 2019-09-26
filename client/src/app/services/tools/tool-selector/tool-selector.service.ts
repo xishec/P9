120a9f080d4835e94c122d6af2f0a8795235dad2
Noboru Yoshida <noboruyoshida1997@gmail.com>
2019-09-26 13:04:54 -0400

    NY : D Display new drawing modal from tool selector service

--- client/src/app/services/tools/tool-selector/tool-selector.service.ts
+++ client/src/app/services/tools/tool-selector/tool-selector.service.ts
@@ -1,13 +1,14 @@
 import { ElementRef, Injectable, Renderer2 } from '@angular/core';
+import { MatDialog } from '@angular/material';
 import { BehaviorSubject, Observable } from 'rxjs';
 
+import { DrawingModalWindowComponent } from 'src/app/components/drawing-modal-window/drawing-modal-window.component';
+import { ToolName } from '../../constants';
 import { DrawStackService } from '../../draw-stack/draw-stack.service';
-import { DrawingModalWindowService } from '../../drawing-modal-window/drawing-modal-window.service';
 import { AbstractToolService } from '../abstract-tools/abstract-tool.service';
 import { BrushToolService } from '../brush-tool/brush-tool.service';
 import { PencilToolService } from '../pencil-tool/pencil-tool.service';
 import { RectangleToolService } from '../rectangle-tool/rectangle-tool.service';
-import { ToolName } from '../../constants';
 
 @Injectable({
     providedIn: 'root',
@@ -22,7 +23,7 @@ export class ToolSelectorService {
     private brushTool: BrushToolService;
     currentTool: AbstractToolService | undefined;
 
-    constructor(private drawingModalWindowService: DrawingModalWindowService) {}
+    constructor(private dialog: MatDialog) {}
 
     initTools(drawStack: DrawStackService, ref: ElementRef<SVGElement>, renderer: Renderer2): void {
         this.rectangleTool = new RectangleToolService(drawStack, ref, renderer);
@@ -42,10 +43,19 @@ export class ToolSelectorService {
         return this.brushTool;
     }
 
+    displayNewDrawingModal(): void {
+        const dialogRef = this.dialog.open(DrawingModalWindowComponent, {
+            panelClass: 'myapp-max-width-dialog',
+        });
+        dialogRef.afterClosed().subscribe(() => {
+            console.log('modal closed');
+        });
+    }
+
     changeTool(tooltipName: string): void {
         switch (tooltipName) {
             case ToolName.NewDrawing:
-                this.drawingModalWindowService.changeDisplayNewDrawingModalWindow(true);
+                this.displayNewDrawingModal();
                 break;
             case ToolName.Rectangle:
                 this.currentTool = this.rectangleTool;
