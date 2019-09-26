45e3088e5e17ff0a08727ace521d0ad539ae4a58
Noboru Yoshida <noboruyoshida1997@gmail.com>
2019-09-26 16:48:51 -0400

    NY : D Instantiate a form in color palette component

--- client/src/app/components/color-tool/color-palette/color-palette.component.ts
+++ client/src/app/components/color-tool/color-palette/color-palette.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-color-palette',
-  templateUrl: './color-palette.component.html',
-  styleUrls: ['./color-palette.component.scss']
+    selector: 'app-color-palette',
+    templateUrl: './color-palette.component.html',
+    styleUrls: ['./color-palette.component.scss'],
 })
 export class ColorPaletteComponent implements OnInit {
+    myForm: FormGroup;
+    formBuilder: FormBuilder;
 
-  constructor() { }
+    constructor(formBuilder: FormBuilder) {
+        this.formBuilder = formBuilder;
+        this.initializeForm();
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {}
 
+    initializeForm(): void {
+        this.myForm = this.formBuilder.group({
+            hex: ['000000', [Validators.pattern('^([A-Fa-f0-9]{3}$)|([A-Fa-f0-9]{6}$)')]],
+            R: ['0', [Validators.required, Validators.min(0), Validators.max(255)]],
+            G: ['0', [Validators.required, Validators.min(0), Validators.max(255)]],
+            B: ['0', [Validators.required, Validators.min(0), Validators.max(255)]],
+            A: [1, [Validators.required, Validators.min(0), Validators.max(1)]],
+        });
+    }
 }
