30628e1cbcb7434cbaa81336ae8c2568c742ddab
Louis Popovic <louispop@gmail.com>
2019-09-27 19:21:22 -0400

    LP : D Setup testBed for tracing tools test

--- client/src/app/services/tools/abstract-tools/tracing-tool/tracing-tool.service.spec.ts
+++ client/src/app/services/tools/abstract-tools/tracing-tool/tracing-tool.service.spec.ts
@@ -1,12 +1,42 @@
-import { TestBed } from '@angular/core/testing';
+import { Renderer2, ElementRef } from '@angular/core';
+import { getTestBed, TestBed } from '@angular/core/testing';
 
 import { TracingToolService } from './tracing-tool.service';
 
-describe('TracingToolService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+fdescribe('TracingToolService', () => {
+    let injector: TestBed;
+    let service: TracingToolService;
+    let mockMouseEvent: MouseEvent;
 
-  it('should be created', () => {
-    const service: TracingToolService = TestBed.get(TracingToolService);
-    expect(service).toBeTruthy();
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [TracingToolService,
+            {
+                provide: Renderer2,
+                useValue: {
+                    createElement: () => 0,
+                },
+            }, {
+                provide: ElementRef,
+                useValue: {
+                    nativeElement: 'allo',
+                },
+            }],
+        });
+
+        injector = getTestBed();
+        service = injector.get(TracingToolService);
+
+        Object.defineProperty(mockMouseEvent, 'offsetX')
+        mockMouseEvent.offsetX = 0;
+        mockMouseEvent.offsetY = 0;
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('onMouseDown isDrawing should be true', () => {
+        service.onMouseDown()
+    })
 });
