3b880e17fddd16c02bd8e854ce0dbfa7337e842b
Roman <Romazho123@gmail.com>
2019-09-28 00:04:01 -0400

    RZ : T Add tests for addColorToQueue and changePreviewColor functions.

--- client/src/app/services/tools/color-tool/color-tool.service.spec.ts
+++ client/src/app/services/tools/color-tool/color-tool.service.spec.ts
@@ -1,10 +1,13 @@
 import { /*async,*/ TestBed, /*ComponentFixture,*/ getTestBed } from '@angular/core/testing';
 
+import { BehaviorSubject /*Observable*/ } from 'rxjs';
 import { ColorToolService } from './color-tool.service';
+import { MAX_NUMBER_OF_LAST_COLORS, COLORS } from 'src/constants/color-constants';
 
-fdescribe('switchPrimarySecondary', () => {
+fdescribe('addColorToQueue', () => {
     let service: ColorToolService;
     let injector: TestBed;
+    let colorQueue: BehaviorSubject<string[]>;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -12,22 +15,40 @@ fdescribe('switchPrimarySecondary', () => {
         });
         injector = getTestBed();
         service = injector.get(ColorToolService);
+        colorQueue = service['colorQueue'];
     });
 
-    it('should be created', () => {
-        expect(service).toBeTruthy();
+    it('should add an element to the queue', () => {
+        let begingColorQueueLength = colorQueue.value.length;
+        service.addColorToQueue('string');
+        expect(colorQueue.value.length).toBe(begingColorQueueLength + 1);
     });
 
-    it('should change primaryColor to secondayColor after colors switch', () => {
-        let primaryColor = service['primaryColor'].value;
-        service.switchPrimarySecondary();
-        expect(primaryColor).toEqual(service['secondaryColor'].value);
+    it('should return 10 for the length of the array if the staring length is 10', () => {
+        colorQueue.value.length = MAX_NUMBER_OF_LAST_COLORS;
+        service.addColorToQueue('string');
+        expect(colorQueue.value.length).toBe(MAX_NUMBER_OF_LAST_COLORS);
+    });
+});
+
+fdescribe('changePreviewColor', () => {
+    let service: ColorToolService;
+    let injector: TestBed;
+    // let previewColor: BehaviorSubject<string>;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [ColorToolService],
+        });
+        injector = getTestBed();
+        service = injector.get(ColorToolService);
+        //previewColor = service['previewColor'];
     });
 
-    it('should change secondaryColor to primaryColor after colors switch', () => {
-        let secondayColor = service['secondaryColor'].value;
-        service.switchPrimarySecondary();
-        expect(secondayColor).toEqual(service['primaryColor'].value);
+    it('should change color to "a970eb" ', () => {
+        let color: BehaviorSubject<string> = new BehaviorSubject<string>(COLORS[4].hex);
+        service.changePreviewColor('a970eb');
+        expect(service['previewColor']).toEqual(color);
     });
 });
 
@@ -70,6 +91,11 @@ fdescribe('correctRGB', () => {
         injector = getTestBed();
         service = injector.get(ColorToolService);
     });
+
+    it('should return 25 if number is 37', () => {
+        let hexNumber = service.correctRGB(37);
+        expect(hexNumber).toBe('25');
+    });
     it('should return 00 if number is negative', () => {
         let hexNumber = service.correctRGB(-10);
         expect(hexNumber).toBe('00');
@@ -79,9 +105,33 @@ fdescribe('correctRGB', () => {
         let hexNumber = service.correctRGB(999);
         expect(hexNumber).toBe('ff');
     });
+});
 
-    it('should return 25 if number is 37', () => {
-        let hexNumber = service.correctRGB(37);
-        expect(hexNumber).toBe('25');
+fdescribe('switchPrimarySecondary', () => {
+    let service: ColorToolService;
+    let injector: TestBed;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [ColorToolService],
+        });
+        injector = getTestBed();
+        service = injector.get(ColorToolService);
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should change primaryColor to secondayColor after colors switch', () => {
+        let primaryColor = service['primaryColor'].value;
+        service.switchPrimarySecondary();
+        expect(primaryColor).toEqual(service['secondaryColor'].value);
+    });
+
+    it('should change secondaryColor to primaryColor after colors switch', () => {
+        let secondayColor = service['secondaryColor'].value;
+        service.switchPrimarySecondary();
+        expect(secondayColor).toEqual(service['primaryColor'].value);
     });
 });
