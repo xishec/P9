31fe3591a77c52dfc0284aa69e61528e77862016
Noboru Yoshida <noboruyoshida1997@gmail.com>
2019-09-28 00:41:45 -0400

    NY : D Update previewColor on color numeric values on init

--- client/src/app/components/color-palette/color-palette.component.ts
+++ client/src/app/components/color-palette/color-palette.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { ShortcutManagerService } from 'src/app/services/shortcut-manager/shortcut-manager.service';
 import { ColorToolService } from 'src/app/services/tools/color-tool/color-tool.service';
 import { ColorType } from 'src/constants/color-constants';
 import { Color } from '../../../classes/Color';
@@ -14,12 +13,23 @@ interface IconStyle {
     templateUrl: './color-palette.component.html',
     styleUrls: ['./color-palette.component.scss'],
 })
-export class ColorPaletteComponent {
+export class ColorPaletteComponent implements OnInit {
     selectedColor: ColorType = ColorType.primaryColor;
     previewColor = new Color().hex;
 
-    constructor(private colorToolService: ColorToolService, private shortcutManagerService: ShortcutManagerService) {}
+    constructor(private colorToolService: ColorToolService) {}
 
+    ngOnInit(): void {
+        this.colorToolService.previewColor.subscribe((previewColor) => {
+            this.previewColor = previewColor;
+        });
+
+        this.colorToolService.selectedColor.subscribe((selectedColor) => {
+            if (selectedColor) {
+                this.selectedColor = selectedColor;
+            }
+        });
+    }
     onClickColorQueueButton(color: string): void {
         this.changeColor(color);
     }
@@ -28,6 +38,14 @@ export class ColorPaletteComponent {
         this.previewColor = previewColor.slice(0, 6);
     }
 
+    // onSubmit(): void {
+    //     const alphaValue: number = this.colorNumericValuesForm.value.A;
+    //     this.colorToolService.changeColorOnFocus(this.previewColor + this.colorToolService.getOpacity(alphaValue));
+    //     this.colorToolService.addColorToQueue(this.previewColor + this.colorToolService.getOpacity(alphaValue));
+    //     this.colorToolService.changeCurrentShowColorPalette(false);
+    //     this.colorToolService.changeSelectedColor(undefined);
+    // }
+
     onCancel(): void {
         this.colorToolService.changeCurrentShowColorPalette(false);
         this.colorToolService.changeSelectedColor(undefined);
@@ -35,15 +53,7 @@ export class ColorPaletteComponent {
 
     getUserColorIcon(): IconStyle {
         return {
-            backgroundColor: '#' + this.previewColor.slice(0, 6) + this.getOpacity(),
+            backgroundColor: '#' + this.previewColor.slice(0, 6) + this.colorToolService.getPreviewColorOpacity(),
         };
-    }
-
-    onFocus() {
-        this.shortcutManagerService.changeIsOnInput(true);
-    }
-
-    onFocusOut() {
-        this.shortcutManagerService.changeIsOnInput(false);
     }
 }
