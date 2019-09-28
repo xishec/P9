721c96de3990eec29a446545bd2f4a2ee2280e2a
Roman <Romazho123@gmail.com>
2019-09-27 21:19:45 -0400

    RZ : T Add testing to the switchPrimarySecondary function.

--- client/src/app/services/tools/color-tool/color-tool.service.spec.ts
+++ client/src/app/services/tools/color-tool/color-tool.service.spec.ts
@@ -1,12 +1,34 @@
-import { TestBed } from '@angular/core/testing';
+import { /*async,*/ TestBed, /*ComponentFixture,*/ getTestBed } from '@angular/core/testing';
 
-import { ColorToolService } from './color-palette.service';
+import { ColorToolService } from './color-tool.service';
 
-describe('ColorToolService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+fdescribe('switchPrimarySecondary', () => {
+    let service: ColorToolService;
+    let injector: TestBed;
 
-  it('should be created', () => {
-    const service: ColorToolService = TestBed.get(ColorToolService);
-    expect(service).toBeTruthy();
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [ColorToolService],
+        });
+
+        injector = getTestBed();
+        service = injector.get(ColorToolService);
+    });
+
+    it('should be created', () => {
+        //const service: ColorToolService = TestBed.get(ColorToolService);
+        expect(service).toBeTruthy();
+    });
+
+    it('should change primaryColor to secondayColor after colors switch', () => {
+        let primaryColor = service['primaryColor'].value;
+        service.switchPrimarySecondary();
+        expect(primaryColor).toBe(service['secondaryColor'].value);
+    });
+
+    it('should change secondaryColor to primaryColor after colors switch', () => {
+        let secondayColor = service['secondaryColor'].value;
+        service.switchPrimarySecondary();
+        expect(secondayColor).toBe(service['primaryColor'].value);
+    });
 });
