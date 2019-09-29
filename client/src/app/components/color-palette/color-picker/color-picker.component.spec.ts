100fc2ede19fb7c8f0dcb1100dba274000508ad0
Roman <Romazho123@gmail.com>
2019-09-29 14:54:54 -0400

    RZ : R Removed all comments from color-picker.

--- client/src/app/components/color-palette/color-picker/color-picker.component.spec.ts
+++ client/src/app/components/color-palette/color-picker/color-picker.component.spec.ts
@@ -1,9 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { Renderer2, /*forwardRef, */ CUSTOM_ELEMENTS_SCHEMA /* Type/*/ } from '@angular/core';
-//import { FormsModule /*NG_VALUE_ACCESSOR*/ } from '@angular/forms';
-import { MatSliderChange } from '@angular/material';
-
-//import { BehaviorSubject, Observable } from 'rxjs';
+import { Renderer2, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { ColorPickerComponent } from './color-picker.component';
 import { ColorToolService } from '../../../services/tools/color-tool/color-tool.service';
@@ -13,12 +9,6 @@ const MOCK_COLOR = '#000000';
 fdescribe('ColorPickerComponent', () => {
     let component: ColorPickerComponent;
     let fixture: ComponentFixture<ColorPickerComponent>;
-    //let fakeColorToolService: ColorToolService;
-    let event: MatSliderChange;
-    //let rendererMock: Renderer2;
-    // let injector: TestBed;
-    // let spyOnSetAttribute: jasmine.Spy;
-    //let spyOnAppendChild: jasmine.Spy;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -39,45 +29,17 @@ fdescribe('ColorPickerComponent', () => {
                     },
                 },
             ],
-            //imports: [FormsModule, RouterTestingModule],
             schemas: [CUSTOM_ELEMENTS_SCHEMA],
         }).compileComponents();
-
-        //rendererMock = injector.get<Renderer2>(Renderer2 as Type<Renderer2>);
-
-        // spyOnSetAttribute = spyOn(rendererMock, 'setAttribute').and.returnValue();
-        // spyOnAppendChild = spyOn(rendererMock, 'appendChild').and.returnValue();
     }));
 
     beforeEach(() => {
         fixture = TestBed.createComponent(ColorPickerComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
-        event = new MatSliderChange();
-
-        // fakeColorToolService = {
-        //     // currentPreviewColor: new Observable<string>(new BehaviorSubject<string>('888888')),
-        //     //user: { name: 'Test User' },
-        //     // isLoggedIn: true,
-        // };
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
     });
-
-    it('#onSliderChange should stop if event is null', () => {
-        component.onSliderChange(event);
-        component.obscurity = 4;
-        //let renderer: Renderer2 = new Renderer2();
-        //component = new ColorPickerComponent(fakeColorToolService, Renderer2());
-
-        expect(component.obscurity).toEqual(4);
-    });
-
-    // it('#onSliderChange should change obscurity', () => {
-    //     let event: MatSliderChange = new MatSliderChange();
-    //     component.onSliderChange(event);
-    //     expect(component.obscurity).toEqual(event.value);
-    // });
 });
