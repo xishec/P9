70e709196a663f33e8c9acf06a6f2dcccaae7f12
Roman <Romazho123@gmail.com>
2019-09-29 18:26:37 -0400

    RZ : T Add mock of class DrawStackService.

--- client/src/app/services/tools/color-applicator-tool/color-applicator-tool.service.spec.ts
+++ client/src/app/services/tools/color-applicator-tool/color-applicator-tool.service.spec.ts
@@ -1,12 +1,44 @@
 import { TestBed } from '@angular/core/testing';
+import { /*Injectable,*/ Renderer2 } from '@angular/core';
+//import { BehaviorSubject } from 'rxjs';
 
 import { ColorApplicatorToolService } from './color-applicator-tool.service';
+import { DrawStackService } from '../../draw-stack/draw-stack.service';
+import { StackTargetInfo } from 'src/classes/StackTargetInfo';
 
-describe('ColorApplicatorToolService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+//const MOCK_COLOR = '#000000';
 
-  it('should be created', () => {
-    const service: ColorApplicatorToolService = TestBed.get(ColorApplicatorToolService);
-    expect(service).toBeTruthy();
-  });
+export class FakeDrawStackService extends DrawStackService {
+    // drawStack = new Array<SVGGElement>();
+    // stackTarget = new BehaviorSubject(new StackTargetInfo());
+    // currentStackTarget = this.stackTarget.asObservable();
+}
+
+fdescribe('ColorApplicatorToolService', () => {
+    beforeEach(() =>
+        TestBed.configureTestingModule({
+            providers: [
+                {
+                    provide: DrawStackService,
+                    useValue: {
+                        currentStackTarget: new StackTargetInfo(), //how do I do it???
+                    },
+                },
+                {
+                    provide: Renderer2,
+                    useValue: {
+                        createElement: () => null,
+                        setAttribute: () => null,
+                        appendChild: () => null,
+                    },
+                },
+            ],
+        }),
+    );
+
+    it('should be created', () => {
+        //spyOn(DrawStackService, 'currentStackTarget').and.returnValue({ subscribe: () => {} });
+        const service: ColorApplicatorToolService = TestBed.get(ColorApplicatorToolService);
+        expect(service).toBeTruthy();
+    });
 });
