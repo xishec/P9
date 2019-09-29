26d650a684729f9d06c423db10bddf02be7cdd86
Louis Popovic <louispop@gmail.com>
2019-09-29 17:05:34 -0400

    LP : D Add test to test changeCurrentToolName

--- client/src/app/services/tools/tool-selector/tool-selector.service.spec.ts
+++ client/src/app/services/tools/tool-selector/tool-selector.service.spec.ts
@@ -2,23 +2,22 @@ import { TestBed, getTestBed } from '@angular/core/testing';
 
 import { ToolSelectorService } from './tool-selector.service';
 import { ToolName } from 'src/constants/tool-constants';
+import { PencilToolService } from '../pencil-tool/pencil-tool.service';
 
 fdescribe('ToolSelectorService', () => {
     let injector: TestBed;
     let service: ToolSelectorService;
+    let mockPencilTool: PencilToolService;
 
     let spyOnChangeCurrentToolName: jasmine.Spy;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            providers: [ToolSelectorService]
+            providers: [ToolSelectorService],
         });
 
         injector = getTestBed();
         service = injector.get(ToolSelectorService);
-
-        spyOnChangeCurrentToolName = spyOn(service, 'changeCurrentToolName').and.returnValue();
-
     });
     
     it('should be created', () => {
@@ -26,50 +25,49 @@ fdescribe('ToolSelectorService', () => {
     });
 
     it('when changeTool with pencil changeCurrentToolName should be call with pencil', () => {
-
+        spyOnChangeCurrentToolName = spyOn(service, 'changeCurrentToolName').and.returnValue();
         service.changeTool(ToolName.Pencil);
 
         expect(spyOnChangeCurrentToolName).toHaveBeenCalledWith(ToolName.Pencil);
     });
 
     it('when changeTool with rectagnle changeCurrentToolName should be call with rectangle', () => {
-
+        spyOnChangeCurrentToolName = spyOn(service, 'changeCurrentToolName').and.returnValue();
         service.changeTool(ToolName.Rectangle);
 
         expect(spyOnChangeCurrentToolName).toHaveBeenCalledWith(ToolName.Rectangle);
     });
 
     it('when changeTool with brush changeCurrentToolName should be call with brush', () => {
-
+        spyOnChangeCurrentToolName = spyOn(service, 'changeCurrentToolName').and.returnValue();
         service.changeTool(ToolName.Brush);
 
         expect(spyOnChangeCurrentToolName).toHaveBeenCalledWith(ToolName.Brush);
     });
 
     it('when changeTool with colorApplicator changeCurrentToolName should be call with colorApplicator', () => {
-
+        spyOnChangeCurrentToolName = spyOn(service, 'changeCurrentToolName').and.returnValue();
         service.changeTool(ToolName.ColorApplicator);
 
         expect(spyOnChangeCurrentToolName).toHaveBeenCalledWith(ToolName.ColorApplicator);
     });
 
     it('when changeTool with tool not implemented yet changeCurrentToolName should be call with his name', () => {
-
+        spyOnChangeCurrentToolName = spyOn(service, 'changeCurrentToolName').and.returnValue();
         service.changeTool(ToolName.Polygon);
 
         expect(spyOnChangeCurrentToolName).toHaveBeenCalledWith(ToolName.Polygon);
     });
 
     it('when changeTool with non existing tool changeCurrentToolName should be call with Selection', () => {
-
+        spyOnChangeCurrentToolName = spyOn(service, 'changeCurrentToolName').and.returnValue();
         service.changeTool('non-existing-tool');
 
         expect(spyOnChangeCurrentToolName).toHaveBeenCalledWith(ToolName.Selection);
     });
 
-    it('when changeCurrentToolName is called with pencil toolName should be pencil', () => {
-
-        service.changeCurrentToolName(ToolName.Pencil);
-        console.log(service['toolName']);
-    })
+    it('when getPencilTool should return the pencilTool', () => {
+        const returnedTool = service.getPencilTool();
+        expect(returnedTool).toEqual(mockPencilTool);
+    });
 });
