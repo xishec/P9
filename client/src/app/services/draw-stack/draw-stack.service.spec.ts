50ac4c2641c049db6ee1bda642fcb8bc12239b0c
Louis Popovic <louispop@gmail.com>
2019-09-29 01:13:31 -0400

    LP : D Add tests for drawStack service

--- client/src/app/services/draw-stack/draw-stack.service.spec.ts
+++ client/src/app/services/draw-stack/draw-stack.service.spec.ts
@@ -1,18 +1,67 @@
-import { TestBed } from '@angular/core/testing';
+import { DrawStackService } from "./draw-stack.service";
 
-import { DrawStackService } from './draw-stack.service';
+const NB_PUSH = 3;
 
-fdescribe('DrawStackService', () => {
-  let service: DrawStackService;
+fdescribe("DrawStackService", () => {
+    let service: DrawStackService;
+    const mockSVGGElement: any = {};
+    //beforeEach(() => TestBed.configureTestingModule({}));
 
+    beforeEach(() => {
+        service = new DrawStackService();
+    });
 
-  //beforeEach(() => TestBed.configureTestingModule({}));
+    it("should be created", () => {
+        expect(service).toBeTruthy();
+    });
 
-  beforeEach(() => {
-    service = new DrawStackService();
-  })
+    it("when getElementByPosition then returns element in drawStack at position", () => {
+        // Arrange
+        service["drawStack"].push(mockSVGGElement);
+        // Act
+        const returnedElement: any = service.getElementByPosition(0);
+        // Assert
+        expect(returnedElement).toEqual(mockSVGGElement);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it(`when getDrawStackLenght after ${NB_PUSH} push then should return lenght of ${NB_PUSH}`, () => {
+        // Arrange
+        for(let i = 0; i < NB_PUSH; i++){
+            service['drawStack'].push(mockSVGGElement);
+        };
+        // Act
+        const nbElements = service.getDrawStackLength();
+        // Assert
+        expect(nbElements).toBe(NB_PUSH);
+    });
+    
+    it('when push SVGGElement drawStack should contain the element', () => {
+        // Arrange
+        // Act
+        service.push(mockSVGGElement);
+        // Assert
+        expect(service['drawStack']).toContain(mockSVGGElement);
+    });
+
+    it('when pop return the last element and drawStack does not contain element', () => {
+        // Arrange
+        service['drawStack'].push(mockSVGGElement);
+        // Act
+        let popElement = service.pop();
+        // Assert
+        expect(popElement).toEqual(mockSVGGElement);
+        expect(service['drawStack']).not.toContain(mockSVGGElement);
+    });
+
+    it('when reset then drawStack is empty and length zero', () => {
+        // Arrange
+        for(let i = 0; i < NB_PUSH; i++){
+            service['drawStack'].push(mockSVGGElement);
+        };
+        // Act
+        service.reset();
+        // Assert
+        expect(service['drawStack'].length).toBe(0);
+        expect(service['drawStack']).not.toContain(mockSVGGElement);
+    })
 });
--- client/src/app/services/tools/brush-tool/brush-tool.service.spec.ts
+++ client/src/app/services/tools/brush-tool/brush-tool.service.spec.ts
@@ -3,14 +3,14 @@ import { getTestBed, TestBed } from '@angular/core/testing';
 import { ElementRef, Renderer2, Type } from '@angular/core';
 import { SVG_NS } from 'src/constants/constants';
 import { DrawStackService } from '../../draw-stack/draw-stack.service';
-import { createMockFilter, createMockSVGCircle } from '../abstract-tools/test-helpers';
+import { createMockFilter, createMockSVGCircle } from '../../../../classes/test-helpers';
 import { TracingToolService } from '../abstract-tools/tracing-tool/tracing-tool.service';
 import { BrushToolService } from './brush-tool.service';
 
 const STACK_LENGTH = 1;
 const MOCK_FILTER = createMockFilter();
 
-describe('BrushToolService', () => {
+fdescribe('BrushToolService', () => {
     let injector: TestBed;
     let service: BrushToolService;
     let rendererMock: Renderer2;
