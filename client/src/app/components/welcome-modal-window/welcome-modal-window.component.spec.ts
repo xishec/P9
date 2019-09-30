869ef05e249defa2ae32e359e36e56ca5a7563ff
Noboru Yoshida <noboruyoshida1997@gmail.com>
2019-09-30 17:43:10 -0400

    NY : T Add test for welcome modal window component

--- client/src/app/components/welcome-modal-window/welcome-modal-window.component.spec.ts
+++ client/src/app/components/welcome-modal-window/welcome-modal-window.component.spec.ts
@@ -3,11 +3,17 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormBuilder } from '@angular/forms';
 import { MatDialogRef } from '@angular/material';
 
+import { WelcomeModalWindowService } from 'src/app/services/welcome-modal-window/welcome-modal-window.service';
 import { WelcomeModalWindowComponent } from './welcome-modal-window.component';
 
 describe('WelcomeModalWindowComponent', () => {
     let component: WelcomeModalWindowComponent;
     let fixture: ComponentFixture<WelcomeModalWindowComponent>;
+    let welcomeModalService: WelcomeModalWindowService;
+
+    const dialogMock = {
+        close: () => null,
+    };
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -15,7 +21,7 @@ describe('WelcomeModalWindowComponent', () => {
             providers: [
                 {
                     provide: MatDialogRef,
-                    useValue: {},
+                    useValue: dialogMock,
                 },
                 {
                     provide: FormBuilder,
@@ -23,13 +29,40 @@ describe('WelcomeModalWindowComponent', () => {
                 },
             ],
             schemas: [NO_ERRORS_SCHEMA],
-        }).compileComponents();
+        })
+            .overrideComponent(WelcomeModalWindowComponent, {
+                set: {
+                    providers: [
+                        {
+                            provide: WelcomeModalWindowService,
+                            useValue: {
+                                changeWelcomeModalWindowClosed: () => null,
+                            },
+                        },
+                    ],
+                },
+            })
+            .compileComponents();
 
         fixture = TestBed.createComponent(WelcomeModalWindowComponent);
         component = fixture.componentInstance;
+
+        welcomeModalService = fixture.debugElement.injector.get(WelcomeModalWindowService);
     }));
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should close dialog when form is submitted', () => {
+        const SPY = spyOn(component[`dialogRef`], 'close');
+        component.submitForm();
+        expect(SPY).toHaveBeenCalled();
+    });
+
+    it('should set Welcome Modal Window Closed flag to true when form is submitted', () => {
+        const SPY = spyOn(welcomeModalService, 'changeWelcomeModalWindowClosed');
+        component.submitForm();
+        expect(SPY).toHaveBeenCalledWith(true);
     });
 });
