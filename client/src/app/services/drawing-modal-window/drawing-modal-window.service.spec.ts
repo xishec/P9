31b06fd668ce3b92f4d6e0adec5b420b99c980d2
Louis Popovic <louispop@gmail.com>
2019-09-30 13:06:44 -0400

    LP : D Add test for drawingModalWindow and welcomeModalWindow

--- client/src/app/services/drawing-modal-window/drawing-modal-window.service.spec.ts
+++ client/src/app/services/drawing-modal-window/drawing-modal-window.service.spec.ts
@@ -1,12 +1,33 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, getTestBed } from '@angular/core/testing';
 
 import { DrawingModalWindowService } from './drawing-modal-window.service';
+import { DrawingInfo } from 'src/classes/DrawingInfo';
 
-describe('DrawingModalWindowService', () => {
-    beforeEach(() => TestBed.configureTestingModule({}));
+const WIDTH = 100;
+const HEIGHT = 100;
 
-    it('should be created', () => {
-        const service: DrawingModalWindowService = TestBed.get(DrawingModalWindowService);
+fdescribe('DrawingModalWindowService', () => {
+    let injector: TestBed;
+    let service: DrawingModalWindowService;
+
+    beforeEach(() =>  {
+        TestBed.configureTestingModule({
+            providers: [DrawingModalWindowService, DrawingInfo],
+        })
+
+        injector = getTestBed();
+        service = injector.get(DrawingModalWindowService);
+    });
+
+    it('should be created', () =>{
         expect(service).toBeTruthy();
     });
+
+    it(`changeDrawingInfoWidthHeight with ${WIDTH} and ${HEIGHT} should update drawingInfo`, () => {
+        service.changeDrawingInfoWidthHeight(WIDTH, HEIGHT);
+
+        expect(service.drawingInfo.value.width).toEqual(WIDTH);
+        expect(service.drawingInfo.value.height).toEqual(HEIGHT);
+    });
+
 });
