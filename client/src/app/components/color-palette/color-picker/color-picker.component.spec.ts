ca7da6fe13056300d415d4a869886b36667fc09b
Roman <Romazho123@gmail.com>
2019-09-30 19:30:38 -0400

    RZ : T Add onSliderChange tests and a draft for onCanvasClick

--- client/src/app/components/color-palette/color-picker/color-picker.component.spec.ts
+++ client/src/app/components/color-palette/color-picker/color-picker.component.spec.ts
@@ -1,12 +1,12 @@
 import { CUSTOM_ELEMENTS_SCHEMA, Renderer2 } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatSliderChange } from '@angular/material';
 
 import { ColorToolService } from '../../../services/tools/color-tool/color-tool.service';
 import { ColorPickerComponent } from './color-picker.component';
+import { createMouseEvent } from 'src/classes/test-helpers';
 
-const MOCK_COLOR = '#000000';
-
-describe('ColorPickerComponent', () => {
+fdescribe('ColorPickerComponent', () => {
     let component: ColorPickerComponent;
     let fixture: ComponentFixture<ColorPickerComponent>;
 
@@ -14,20 +14,16 @@ describe('ColorPickerComponent', () => {
         TestBed.configureTestingModule({
             declarations: [ColorPickerComponent],
             providers: [
-                {
-                    provide: ColorToolService,
-                    useValue: {
-                        currentPrimaryColor: () => MOCK_COLOR,
-                    },
-                },
-                {
-                    provide: Renderer2,
-                    useValue: {
-                        createElement: () => null,
-                        setAttribute: () => null,
-                        appendChild: () => null,
-                    },
-                },
+                ColorToolService,
+                Renderer2,
+                // {
+                //     provide: Renderer2,
+                //     useValue: {
+                //         createElement: () => null,
+                //         setAttribute: () => null,
+                //         appendChild: () => null,
+                //     },
+                // },
             ],
             schemas: [CUSTOM_ELEMENTS_SCHEMA],
         }).compileComponents();
@@ -41,5 +37,36 @@ describe('ColorPickerComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('#onSliderChange should stop execution if the value of the event is null', () => {
+        let event: MatSliderChange = new MatSliderChange();
+        component.obscurity = 1;
+        event.value = null;
+        component.onSliderChange(event);
+        expect(component.obscurity).toEqual(1);
+    });
+
+    it("#onSliderChange should change obscurity level to the event's value ", () => {
+        let event: MatSliderChange = new MatSliderChange();
+        component.obscurity = 1;
+        event.value = 2;
+        component.onSliderChange(event);
+        expect(component.obscurity).toEqual(2);
+    });
+
+    it('#onCanvasClick should not change obscurity to 0 if obscurity is defined', () => {
+        let event: MouseEvent = createMouseEvent(0, 0, 0);
+        component.obscurity = 1;
+        const valueServiceSpy: jasmine.SpyObj<CanvasRenderingContext2D> = jasmine.createSpyObj(
+            'CanvasRenderingContext2D',
+            ['getImageData'],
+        );
+        const stubValue: ImageData = new ImageData(10, 10);
+
+        valueServiceSpy.getImageData.and.returnValue(stubValue);
+
+        component.onCanvasClick(event);
+        expect(component.obscurity).toEqual(1);
     });
 });
