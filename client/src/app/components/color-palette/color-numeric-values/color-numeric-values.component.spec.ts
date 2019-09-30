1f1a3ffa0fd86f35075120369298b8147058903e
Louis Popovic <louispop@gmail.com>
2019-09-30 00:16:38 -0400

    LP : D Add tests for onFocus

--- client/src/app/components/color-palette/color-numeric-values/color-numeric-values.component.spec.ts
+++ client/src/app/components/color-palette/color-numeric-values/color-numeric-values.component.spec.ts
@@ -1,68 +1,122 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { ColorNumericValuesComponent } from './color-numeric-values.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { ShortcutManagerService } from '../../../services/shortcut-manager/shortcut-manager.service';
+import { ColorToolService } from "../../../services/tools/color-tool/color-tool.service";
+import { ColorNumericValuesComponent } from "./color-numeric-values.component";
+import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
 
-fdescribe('ColorNumericValuesComponent', () => {
-  let component: ColorNumericValuesComponent;
-  let fixture: ComponentFixture<ColorNumericValuesComponent>;
-  let form: FormGroup; 
+fdescribe("ColorNumericValuesComponent", () => {
+    let component: ColorNumericValuesComponent;
+    let fixture: ComponentFixture<ColorNumericValuesComponent>;
+    let form: FormGroup;
+    let shortCutManagerService: ShortcutManagerService;
 
-  const MOCK_COLOR = '01234567';
+    const MOCK_COLOR = "01234567";
+    const MOCK_COLOR_FROM_SERVICE = "ff00ff00";
+    const MOCK_OPACITY = "33";
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ColorNumericValuesComponent ],
-      schemas: [NO_ERRORS_SCHEMA],
-      providers: [FormBuilder],
-    }).compileComponents();
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ColorNumericValuesComponent],
+            schemas: [NO_ERRORS_SCHEMA],
+            providers: [
+                FormBuilder,
+                {
+                    provide: ColorToolService,
+                    useValue: {
+                        getPreviewColorOpacityHex: () => MOCK_OPACITY,
+                        translateRGBToHex: () => MOCK_COLOR_FROM_SERVICE,
+                        changePreviewColor: () => null,
+                    },
+                }, {
+                    provide: ShortcutManagerService,
+                    useValue: {
+                        changeIsOnInput: (b: boolean) => null,
+                    }
+                }
+            ],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(ColorNumericValuesComponent);
-    component = fixture.componentInstance;
+        fixture = TestBed.createComponent(ColorNumericValuesComponent);
+        component = fixture.componentInstance;
 
-    component.initializeForm();
+        shortCutManagerService = fixture.debugElement.injector.get(ShortcutManagerService);
 
-    form = component.colorNumericValuesForm;
-  }));
+        component.initializeForm();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        form = component.colorNumericValuesForm;
+    }));
 
-  it('by default RGB are 0 and A is 1', () => {
-      expect(form.controls['R'].value).toEqual(0);
-      expect(form.controls['G'].value).toEqual(0);
-      expect(form.controls['B'].value).toEqual(0);
-      expect(form.controls['A'].value).toEqual(1);
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 
-  it('setColorNumericValues should call setHexValues before setRGBValues', () => {
-      const spyOnSetHex = spyOn(component, 'setHexValues').and.returnValue();
-      const spyOnSetRGB = spyOn(component, 'setRGBValues').and.returnValue();
-      
-      component.setColorNumericValues();
+    it("by default RGB are 0 and A is 1", () => {
+        expect(form.controls["R"].value).toEqual(0);
+        expect(form.controls["G"].value).toEqual(0);
+        expect(form.controls["B"].value).toEqual(0);
+        expect(form.controls["A"].value).toEqual(1);
+    });
 
-      expect(spyOnSetHex).toHaveBeenCalledBefore(spyOnSetRGB);
-  });
+    it("setColorNumericValues should call setHexValues before setRGBValues", () => {
+        const spyOnSetHex = spyOn(component, "setHexValues").and.returnValue();
+        const spyOnSetRGB = spyOn(component, "setRGBValues").and.returnValue();
 
-  it(`when setHexValues with ${MOCK_COLOR} then this preview color is the first 6 char`, () => {
-      component.previewColor = MOCK_COLOR;
-      
-      component.setHexValues();
+        component.setColorNumericValues();
 
-      expect(form.controls['hex'].value).toEqual('012345');
-  });
+        expect(spyOnSetHex).toHaveBeenCalledBefore(spyOnSetRGB);
+    });
 
-  it(`when setRGBValues with ${MOCK_COLOR} then R=01, G=23, B=45 A=0.2`, () => {
-    component.previewColor = MOCK_COLOR;
+    it(`when setHexValues with ${MOCK_COLOR} then this preview color is the first 6 char`, () => {
+        component.previewColor = MOCK_COLOR;
 
-    component.setRGBValues();
+        component.setHexValues();
 
-    expect(form.controls['R'].value).toEqual(1);
-    expect(form.controls['G'].value).toEqual(35);
-    expect(form.controls['B'].value).toEqual(69);
-    expect(form.controls['A'].value).toEqual('0.4');
-  });
+        expect(form.controls["hex"].value).toEqual("012345");
+    });
 
+    it(`when setRGBValues with ${MOCK_COLOR} then R=01, G=23, B=45 A=0.2`, () => {
+        component.previewColor = MOCK_COLOR;
+
+        component.setRGBValues();
+
+        expect(form.controls["R"].value).toEqual(1);
+        expect(form.controls["G"].value).toEqual(35);
+        expect(form.controls["B"].value).toEqual(69);
+        expect(form.controls["A"].value).toEqual("0.4");
+    });
+
+    it("when onUserHexInput previewColor is colorNumericValuesForm.hex + opacity", () => {
+        const hexColor = "112233";
+        form.controls["hex"].setValue(hexColor);
+
+        component.onUserHexInput();
+
+        expect(component.previewColor).toEqual(hexColor + MOCK_OPACITY);
+    });
+
+    it(`when onUserColorRGBInput preview is ${MOCK_COLOR_FROM_SERVICE} (value provided from service)`, () => {
+        spyOn(component, 'setColorNumericValues').and.returnValue();
+
+        component.onUserColorRGBInput();
+
+        expect(component.previewColor).toEqual(MOCK_COLOR_FROM_SERVICE);
+    });
+
+    it('when onFocus shortCutManagerService.changeIsOnInput is called with true', () => {
+        const spy = spyOn(shortCutManagerService, 'changeIsOnInput').and.returnValue();
+
+        component.onFocus();
+
+        expect(spy).toHaveBeenCalledWith(true);
+    });
+
+    it('when onFocusOut shortCutManagerService.changeIsOnInput is called with false', () => {
+        const spy = spyOn(shortCutManagerService, 'changeIsOnInput').and.returnValue();
+
+        component.onFocusOut();
+
+        expect(spy).toHaveBeenCalledWith(false);
+    });
 });
