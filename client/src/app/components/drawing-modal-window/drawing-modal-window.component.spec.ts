0c4fd08402f35bd06bb0ccb2d0fa23fb20e51109
Louis Popovic <louispop@gmail.com>
2019-09-29 20:05:16 -0400

    LP : D Fix lint erros

--- client/src/app/components/drawing-modal-window/drawing-modal-window.component.spec.ts
+++ client/src/app/components/drawing-modal-window/drawing-modal-window.component.spec.ts
@@ -1,9 +1,9 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { MatDialogRef } from '@angular/material';
 
 import { DrawingModalWindowComponent } from './drawing-modal-window.component';
-import { MatDialogRef } from '@angular/material';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
 
 fdescribe('DrawingModalWindowComponent', () => {
     let component: DrawingModalWindowComponent;
@@ -14,17 +14,17 @@ fdescribe('DrawingModalWindowComponent', () => {
             declarations: [DrawingModalWindowComponent],
             providers: [{
                 provide: MatDialogRef,
-                useValue: { }
-            },{
+                useValue: { },
+            }, {
                 provide: FormBuilder,
                 useValue: { },
             }],
             schemas: [NO_ERRORS_SCHEMA],
         }).compileComponents();
-        
+
         fixture = TestBed.createComponent(DrawingModalWindowComponent);
         component = fixture.componentInstance;
-    
+
     }));
 
     it('should create', () => {
--- client/src/app/components/welcome-modal-window/welcome-modal-window.component.spec.ts
+++ client/src/app/components/welcome-modal-window/welcome-modal-window.component.spec.ts
@@ -1,33 +1,35 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { MatDialogRef } from '@angular/material';
 
 import { WelcomeModalWindowComponent } from './welcome-modal-window.component';
-import { MatDialogRef } from '@angular/material';
-import { FormBuilder } from '@angular/forms';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 fdescribe('WelcomeModalWindowComponent', () => {
-  let component: WelcomeModalWindowComponent;
-  let fixture: ComponentFixture<WelcomeModalWindowComponent>;
+    let component: WelcomeModalWindowComponent;
+    let fixture: ComponentFixture<WelcomeModalWindowComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ WelcomeModalWindowComponent ],
-      providers: [{
-        provide: MatDialogRef,
-        useValue: { }
-      },{
-        provide: FormBuilder,
-        useValue: { },
-      }],
-    schemas: [NO_ERRORS_SCHEMA],
-    }).compileComponents();
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [WelcomeModalWindowComponent],
+            providers: [
+                {
+                    provide: MatDialogRef,
+                    useValue: {},
+                },
+                {
+                    provide: FormBuilder,
+                    useValue: {},
+                },
+            ],
+            schemas: [NO_ERRORS_SCHEMA],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(WelcomeModalWindowComponent);
-    component = fixture.componentInstance;
+        fixture = TestBed.createComponent(WelcomeModalWindowComponent);
+        component = fixture.componentInstance;
+    }));
 
-  }));
-  
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
