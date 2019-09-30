7b66620fdea180b0e1e8077c78ba518e413a2bba
Louis Popovic <louispop@gmail.com>
2019-09-30 11:39:30 -0400

    LP : R Change tests

--- client/src/app/services/tools/color-applicator-tool/color-applicator-tool.service.spec.ts
+++ client/src/app/services/tools/color-applicator-tool/color-applicator-tool.service.spec.ts
@@ -3,19 +3,21 @@ import { Renderer2 } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
 import { ColorApplicatorToolService } from './color-applicator-tool.service';
-import { DrawStackService } from '../../draw-stack/draw-stack.service';
 import { createMockSVGCircle, createMouseEvent } from '../../../../classes/test-helpers';
 import { ToolName } from 'src/constants/tool-constants';
 import { ColorToolService } from '../color-tool/color-tool.service';
+import { Mouse } from 'src/constants/constants';
 
-describe('ColorApplicatorToolService', () => {
+fdescribe('ColorApplicatorToolService', () => {
     let service: ColorApplicatorToolService;
     let injector: TestBed;
+    let mockRenderer: Renderer2;
+
+    let spyOnSetAttribute: jasmine.Spy;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             providers: [
-                DrawStackService,
                 {
                     provide: Renderer2,
                     useValue: {
@@ -30,73 +32,85 @@ describe('ColorApplicatorToolService', () => {
         service = injector.get(ColorApplicatorToolService);
         service['currentStackTarget'].targetPosition = 0;
         service['drawStack'].push(createMockSVGCircle());
+
+        mockRenderer = injector.get(Renderer2);
+        spyOnSetAttribute = spyOn(mockRenderer, 'setAttribute');
+        
     });
 
     it('ColorApplicatorToolService should be created', () => {
         expect(service).toBeTruthy();
     });
 
-    it('#initializeColorToolService should ', () => {
+    it('initializeColorToolService should be primaryColor from colorService', () => {
         let colorService: ColorToolService = new ColorToolService();
         let primaryColorTmp: BehaviorSubject<string> = colorService['primaryColor'];
         service.initializeColorToolService(new ColorToolService());
-        expect('#' + primaryColorTmp.value).toEqual(service['primaryColor']);
+        expect(service['primaryColor']).toEqual('#' + primaryColorTmp.value);
     });
 
-    it('#onMouseDown should set attribute to stroke when left button clicked if tool is Brush', () => {
-        let mouseEventTmp = createMouseEvent(1, 1, 0);
+    it('onMouseDown should call setAttribute twice when left button clicked if tool is Brush', () => {
+        let mouseEventTmp = createMouseEvent(1, 1, Mouse.LeftButton);
         service['currentStackTarget'].toolName = ToolName.Brush;
+
         service.onMouseDown(mouseEventTmp);
-        expect(service['renderer']).toBeTruthy();
+
+        expect(spyOnSetAttribute).toHaveBeenCalledTimes(2);
     });
 
-    it('#onMouseDown should not set attribute to stroke when left button clicked if tool is Quill', () => {
-        let mouseEventTmp = createMouseEvent(1, 1, 0);
-        service['currentStackTarget'].toolName = ToolName.Quill;
+    it('onMouseDown should call setAttribute once when left button clicked if tool is Rectangle', () => {
+        let mouseEventTmp = createMouseEvent(1, 1, Mouse.LeftButton);
+        service['currentStackTarget'].toolName = ToolName.Rectangle;
+
         service.onMouseDown(mouseEventTmp);
-        expect(service['renderer']).toBeTruthy();
+
+        expect(spyOnSetAttribute).toHaveBeenCalledTimes(1);
     });
 
-    it('#onMouseDown should not set attribute to stroke when right button clicked if tool is Brush', () => {
-        let mouseEventTmp = createMouseEvent(1, 1, 2);
+    it('onMouseDown should not call setAttribute to stroke when right button clicked if tool is Brush', () => {
+        let mouseEventTmp = createMouseEvent(1, 1, Mouse.RightButton);
         service['currentStackTarget'].toolName = ToolName.Brush;
+
         service.onMouseDown(mouseEventTmp);
-        expect(service['renderer']).toBeTruthy();
+
+        expect(spyOnSetAttribute).not.toHaveBeenCalled();
     });
 
-    it('#onMouseDown should set attribute to stroke when right button clicked if tool is Quill ', () => {
-        let mouseEventTmp = createMouseEvent(1, 1, 2);
-        service['currentStackTarget'].toolName = ToolName.Quill;
+    it('onMouseDown should call setAttribute once when right button clicked if tool is Rectangle ', () => {
+        let mouseEventTmp = createMouseEvent(1, 1, Mouse.RightButton);
+        service['currentStackTarget'].toolName = ToolName.Rectangle;
+
         service.onMouseDown(mouseEventTmp);
-        expect(service['renderer']).toBeTruthy();
+
+        expect(spyOnSetAttribute).toHaveBeenCalledTimes(1);
     });
 
-    it('#onMouseUp should not change ColorApplicatorToolService', () => {
+    it('onMouseUp should do nothing', () => {
         let event: MouseEvent = new MouseEvent('click');
         expect(service.onMouseUp(event)).toEqual(undefined);
     });
 
-    it('#onMouseEnter should not change ColorApplicatorToolService', () => {
+    it('onMouseEnter should do nothing', () => {
         let event: MouseEvent = new MouseEvent('click');
         expect(service.onMouseEnter(event)).toEqual(undefined);
     });
 
-    it('#onMouseLeave should not change ColorApplicatorToolService', () => {
+    it('onMouseLeave should do nothing', () => {
         let event: MouseEvent = new MouseEvent('click');
         expect(service.onMouseLeave(event)).toEqual(undefined);
     });
 
-    it('#onKeyDown should not change ColorApplicatorToolService', () => {
+    it('onKeyDown should do nothing', () => {
         let event: KeyboardEvent = new KeyboardEvent('click');
         expect(service.onKeyDown(event)).toEqual(undefined);
     });
 
-    it('#onKeyUp should not change ColorApplicatorToolService', () => {
+    it('onKeyUp should do nothing', () => {
         let event: KeyboardEvent = new KeyboardEvent('click');
         expect(service.onKeyUp(event)).toEqual(undefined);
     });
 
-    it('#onMouseMove should not change ColorApplicatorToolService', () => {
+    it('onMouseMove should do nothing', () => {
         let event: MouseEvent = new MouseEvent('click');
         expect(service.onMouseMove(event)).toEqual(undefined);
     });
