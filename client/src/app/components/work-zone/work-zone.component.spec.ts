49f3ec82037b95653be102c60bcc1e588b7be7a3
Xi Chen <xishec@gmail.com>
2019-09-29 21:44:31 -0400

    XS : D Add providers for testing

--- client/src/app/components/work-zone/work-zone.component.spec.ts
+++ client/src/app/components/work-zone/work-zone.component.spec.ts
@@ -1,14 +1,51 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BehaviorSubject } from 'rxjs';
 
 import { WorkZoneComponent } from './work-zone.component';
+import { NO_ERRORS_SCHEMA, Renderer2 } from '@angular/core';
+import { DrawingModalWindowService } from 'src/app/services/drawing-modal-window/drawing-modal-window.service';
+import { ToolSelectorService } from 'src/app/services/tools/tool-selector/tool-selector.service';
+import { DrawStackService } from '../../services/draw-stack/draw-stack.service';
+import { ColorToolService } from 'src/app/services/tools/color-tool/color-tool.service';
+import { DrawingInfo } from 'src/classes/DrawingInfo';
+import { DEFAULT_WHITE } from 'src/constants/color-constants';
 
-describe('WorkZoneComponent', () => {
+fdescribe('WorkZoneComponent', () => {
     let component: WorkZoneComponent;
     let fixture: ComponentFixture<WorkZoneComponent>;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [WorkZoneComponent],
+            providers: [
+                {
+                    provide: DrawingModalWindowService,
+                    useValue: {
+                        drawingInfo: new BehaviorSubject<DrawingInfo>(new DrawingInfo(0, 0, DEFAULT_WHITE)),
+                        currentDisplayNewDrawingModalWindow: new BehaviorSubject<boolean>(false).asObservable(),
+                    },
+                },
+                {
+                    provide: Renderer2,
+                },
+                {
+                    provide: DrawStackService,
+                },
+                {
+                    provide: ToolSelectorService,
+                    useValue: {
+                        currentToolName: new BehaviorSubject<DrawingInfo>(new DrawingInfo(0, 0, DEFAULT_WHITE)),
+                        initTools: () => null,
+                    },
+                },
+                {
+                    provide: ColorToolService,
+                    useValue: {
+                        backgroundColor: new BehaviorSubject<string>(DEFAULT_WHITE),
+                    },
+                },
+            ],
+            schemas: [NO_ERRORS_SCHEMA],
         }).compileComponents();
     }));
 
