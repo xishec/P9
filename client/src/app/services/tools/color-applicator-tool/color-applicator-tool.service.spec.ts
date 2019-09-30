68074a55f41aba7b1b7df68bc5c044d73a4d9a90
Louis Popovic <louispop@gmail.com>
2019-09-30 11:42:34 -0400

    LP : R Fix lint

--- client/src/app/services/tools/color-applicator-tool/color-applicator-tool.service.spec.ts
+++ client/src/app/services/tools/color-applicator-tool/color-applicator-tool.service.spec.ts
@@ -1,12 +1,12 @@
-import { TestBed, getTestBed } from '@angular/core/testing';
 import { Renderer2 } from '@angular/core';
+import { getTestBed, TestBed } from '@angular/core/testing';
 import { BehaviorSubject } from 'rxjs';
 
-import { ColorApplicatorToolService } from './color-applicator-tool.service';
-import { createMockSVGCircle, createMouseEvent } from '../../../../classes/test-helpers';
-import { ToolName } from 'src/constants/tool-constants';
-import { ColorToolService } from '../color-tool/color-tool.service';
 import { Mouse } from 'src/constants/constants';
+import { ToolName } from 'src/constants/tool-constants';
+import { createMockSVGCircle, createMouseEvent } from '../../../../classes/test-helpers';
+import { ColorToolService } from '../color-tool/color-tool.service';
+import { ColorApplicatorToolService } from './color-applicator-tool.service';
 
 fdescribe('ColorApplicatorToolService', () => {
     let service: ColorApplicatorToolService;
@@ -30,12 +30,12 @@ fdescribe('ColorApplicatorToolService', () => {
         });
         injector = getTestBed();
         service = injector.get(ColorApplicatorToolService);
-        service['currentStackTarget'].targetPosition = 0;
-        service['drawStack'].push(createMockSVGCircle());
+        service[`currentStackTarget`].targetPosition = 0;
+        service[`drawStack`].push(createMockSVGCircle());
 
         mockRenderer = injector.get(Renderer2);
         spyOnSetAttribute = spyOn(mockRenderer, 'setAttribute');
-        
+
     });
 
     it('ColorApplicatorToolService should be created', () => {
@@ -43,15 +43,15 @@ fdescribe('ColorApplicatorToolService', () => {
     });
 
     it('initializeColorToolService should be primaryColor from colorService', () => {
-        let colorService: ColorToolService = new ColorToolService();
-        let primaryColorTmp: BehaviorSubject<string> = colorService['primaryColor'];
+        const colorService: ColorToolService = new ColorToolService();
+        const primaryColorTmp: BehaviorSubject<string> = colorService[`primaryColor`];
         service.initializeColorToolService(new ColorToolService());
-        expect(service['primaryColor']).toEqual('#' + primaryColorTmp.value);
+        expect(service[`primaryColor`]).toEqual('#' + primaryColorTmp.value);
     });
 
     it('onMouseDown should call setAttribute twice when left button clicked if tool is Brush', () => {
-        let mouseEventTmp = createMouseEvent(1, 1, Mouse.LeftButton);
-        service['currentStackTarget'].toolName = ToolName.Brush;
+        const mouseEventTmp = createMouseEvent(1, 1, Mouse.LeftButton);
+        service[`currentStackTarget`].toolName = ToolName.Brush;
 
         service.onMouseDown(mouseEventTmp);
 
@@ -59,8 +59,8 @@ fdescribe('ColorApplicatorToolService', () => {
     });
 
     it('onMouseDown should call setAttribute once when left button clicked if tool is Rectangle', () => {
-        let mouseEventTmp = createMouseEvent(1, 1, Mouse.LeftButton);
-        service['currentStackTarget'].toolName = ToolName.Rectangle;
+        const mouseEventTmp = createMouseEvent(1, 1, Mouse.LeftButton);
+        service[`currentStackTarget`].toolName = ToolName.Rectangle;
 
         service.onMouseDown(mouseEventTmp);
 
@@ -68,8 +68,8 @@ fdescribe('ColorApplicatorToolService', () => {
     });
 
     it('onMouseDown should not call setAttribute to stroke when right button clicked if tool is Brush', () => {
-        let mouseEventTmp = createMouseEvent(1, 1, Mouse.RightButton);
-        service['currentStackTarget'].toolName = ToolName.Brush;
+        const mouseEventTmp = createMouseEvent(1, 1, Mouse.RightButton);
+        service[`currentStackTarget`].toolName = ToolName.Brush;
 
         service.onMouseDown(mouseEventTmp);
 
@@ -77,8 +77,8 @@ fdescribe('ColorApplicatorToolService', () => {
     });
 
     it('onMouseDown should call setAttribute once when right button clicked if tool is Rectangle ', () => {
-        let mouseEventTmp = createMouseEvent(1, 1, Mouse.RightButton);
-        service['currentStackTarget'].toolName = ToolName.Rectangle;
+        const mouseEventTmp = createMouseEvent(1, 1, Mouse.RightButton);
+        service[`currentStackTarget`].toolName = ToolName.Rectangle;
 
         service.onMouseDown(mouseEventTmp);
 
@@ -86,32 +86,32 @@ fdescribe('ColorApplicatorToolService', () => {
     });
 
     it('onMouseUp should do nothing', () => {
-        let event: MouseEvent = new MouseEvent('click');
+        const event: MouseEvent = new MouseEvent('click');
         expect(service.onMouseUp(event)).toEqual(undefined);
     });
 
     it('onMouseEnter should do nothing', () => {
-        let event: MouseEvent = new MouseEvent('click');
+        const event: MouseEvent = new MouseEvent('click');
         expect(service.onMouseEnter(event)).toEqual(undefined);
     });
 
     it('onMouseLeave should do nothing', () => {
-        let event: MouseEvent = new MouseEvent('click');
+        const event: MouseEvent = new MouseEvent('click');
         expect(service.onMouseLeave(event)).toEqual(undefined);
     });
 
     it('onKeyDown should do nothing', () => {
-        let event: KeyboardEvent = new KeyboardEvent('click');
+        const event: KeyboardEvent = new KeyboardEvent('click');
         expect(service.onKeyDown(event)).toEqual(undefined);
     });
 
     it('onKeyUp should do nothing', () => {
-        let event: KeyboardEvent = new KeyboardEvent('click');
+        const event: KeyboardEvent = new KeyboardEvent('click');
         expect(service.onKeyUp(event)).toEqual(undefined);
     });
 
     it('onMouseMove should do nothing', () => {
-        let event: MouseEvent = new MouseEvent('click');
+        const event: MouseEvent = new MouseEvent('click');
         expect(service.onMouseMove(event)).toEqual(undefined);
     });
 });
