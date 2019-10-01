559f8909331baf2b5ed8cfe5ef76474c32b4b144
Xi Chen <xishec@gmail.com>
2019-09-30 22:00:57 -0400

    XS : D Add test for StackTargetInfo

---
+++ client/src/classes/StackTargetInfo.spec.ts
@@ -0,0 +1,24 @@
+import { TestBed } from '@angular/core/testing';
+
+import { StackTargetInfo } from './StackTargetInfo';
+import { ToolName } from 'src/constants/tool-constants';
+
+fdescribe('AttributesManagerService', () => {
+    beforeEach(() => TestBed.configureTestingModule({}));
+
+    it('should be created with valid parameters', () => {
+        let stackTargetInfo = new StackTargetInfo(11, ToolName.Ellipsis);
+        expect(stackTargetInfo.targetPosition).toEqual(11);
+        expect(stackTargetInfo.toolName).toEqual(ToolName.Ellipsis);
+    });
+
+    it('should be created with one parameter (first one)', () => {
+        let stackTargetInfo = new StackTargetInfo(11);
+        expect(stackTargetInfo.targetPosition).toEqual(11);
+    });
+
+    it('should be created with one parameter (second one)', () => {
+        let stackTargetInfo = new StackTargetInfo(undefined, ToolName.Ellipsis);
+        expect(stackTargetInfo.toolName).toEqual(ToolName.Ellipsis);
+    });
+});
