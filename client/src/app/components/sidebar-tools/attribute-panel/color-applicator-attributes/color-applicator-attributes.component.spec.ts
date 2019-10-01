54828115a98c62878a5b36b01c45dfc1ad75532b
Xi Chen <xishec@gmail.com>
2019-09-30 22:24:06 -0400

    XS : D Add test for color-applicator-attributes

--- client/src/app/components/sidebar-tools/attribute-panel/color-applicator-attributes/color-applicator-attributes.component.spec.ts
+++ client/src/app/components/sidebar-tools/attribute-panel/color-applicator-attributes/color-applicator-attributes.component.spec.ts
@@ -1,25 +1,42 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ColorApplicatorAttributesComponent } from './color-applicator-attributes.component';
+import { ToolSelectorService } from 'src/app/services/tools/tool-selector/tool-selector.service';
 
-describe('ColorApplicatorAttributesComponent', () => {
-  let component: ColorApplicatorAttributesComponent;
-  let fixture: ComponentFixture<ColorApplicatorAttributesComponent>;
+fdescribe('ColorApplicatorAttributesComponent', () => {
+    let component: ColorApplicatorAttributesComponent;
+    let fixture: ComponentFixture<ColorApplicatorAttributesComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ColorApplicatorAttributesComponent ],
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ColorApplicatorAttributesComponent],
+            providers: [
+                ColorApplicatorAttributesComponent,
+                {
+                    provide: ToolSelectorService,
+                    useValue: {
+                        getColorApplicatorTool: () => null,
+                    },
+                },
+            ],
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ColorApplicatorAttributesComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ColorApplicatorAttributesComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('shoud call ngAfterViewInit', () => {
+        const SPY1 = spyOn(component[`toolSelectorService`], 'getColorApplicatorTool');
+        const SPY2 = spyOn(component.colorApplicatorToolService, 'initializeColorToolService');
+        component.ngAfterViewInit();
+        expect(SPY1).toHaveBeenCalled;
+        expect(SPY2).toHaveBeenCalled;
+    });
 });
