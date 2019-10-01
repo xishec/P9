dfd0b16bec7fafaab272214bd8ae918e6530039f
Kevin <kevinscottsciliento@gmail.com>
2019-10-01 01:58:35 -0400

    KC : T Add additionnal tests to color-picker

--- client/src/app/components/color-palette/color-picker/color-picker.component.spec.ts
+++ client/src/app/components/color-palette/color-picker/color-picker.component.spec.ts
@@ -9,12 +9,12 @@ import { createMouseEvent } from 'src/classes/test-helpers';
 fdescribe('ColorPickerComponent', () => {
     let component: ColorPickerComponent;
     let fixture: ComponentFixture<ColorPickerComponent>;
+    let colorToolService: ColorToolService;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [ColorPickerComponent],
             providers: [
-                ColorToolService,
                 Renderer2,
                 // {
                 //     provide: Renderer2,
@@ -26,20 +26,42 @@ fdescribe('ColorPickerComponent', () => {
                 // },
             ],
             schemas: [CUSTOM_ELEMENTS_SCHEMA],
-        }).compileComponents();
-    }));
+        })
+            .overrideComponent(ColorPickerComponent, {
+                set: {
+                    providers: [
+                        {
+                            provide: ColorToolService,
+                            useValue: {
+                                translateRGBToHex: () => null,
+                                getPreviewColorOpacityHex: () => null,
+                                changePreviewColor: () => null,
+                            },
+                        },
+                        {
+                            provide: CanvasRenderingContext2D,
+                            useValue: {
+                                drawImage: () => null,
+                            },
+                        },
+                    ],
+                },
+            })
+            .compileComponents();
 
-    beforeEach(() => {
         fixture = TestBed.createComponent(ColorPickerComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
-    });
+        colorToolService = fixture.debugElement.injector.get<ColorToolService>(ColorToolService);
+    }));
+
+    beforeEach(() => {});
 
     it('should create', () => {
         expect(component).toBeTruthy();
     });
 
-    it('#onSliderChange should stop execution if the value of the event is null', () => {
+    it('onSliderChange should stop execution if the value of the event is null', () => {
         let event: MatSliderChange = new MatSliderChange();
         component.obscurity = 1;
         event.value = null;
@@ -47,7 +69,7 @@ fdescribe('ColorPickerComponent', () => {
         expect(component.obscurity).toEqual(1);
     });
 
-    it("#onSliderChange should change obscurity level to the event's value ", () => {
+    it("onSliderChange should change obscurity level to the event's value ", () => {
         let event: MatSliderChange = new MatSliderChange();
         component.obscurity = 1;
         event.value = 2;
@@ -55,12 +77,12 @@ fdescribe('ColorPickerComponent', () => {
         expect(component.obscurity).toEqual(2);
     });
 
-    it('#onCanvasClick should not change obscurity to 0 if obscurity is defined', () => {
-        let event: MouseEvent = createMouseEvent(0, 0, 0);
+    it('onCanvasClick should not change obscurity to 0 if obscurity is defined', () => {
+        let event: MouseEvent = createMouseEvent(0, 0, 0, 0, 0);
         component.obscurity = 1;
         const valueServiceSpy: jasmine.SpyObj<CanvasRenderingContext2D> = jasmine.createSpyObj(
             'CanvasRenderingContext2D',
-            ['getImageData'],
+            ['getImageData']
         );
         const stubValue: ImageData = new ImageData(10, 10);
 
@@ -68,5 +90,34 @@ fdescribe('ColorPickerComponent', () => {
 
         component.onCanvasClick(event);
         expect(component.obscurity).toEqual(1);
+    });
+
+    it('onCanvasClick should not call changePreviewColor, translateRGBToHex getPreviewColorOpacityHex, and changePreviewColor when obscurity is 0', () => {
+        let event: MouseEvent = createMouseEvent(2, 4, 6, 1, 1);
+
+        const TRANSLATE_SPY = spyOn(colorToolService, 'translateRGBToHex');
+        const GET_PREVIEW_SPY = spyOn(colorToolService, 'getPreviewColorOpacityHex');
+        const CHANGE_PREVIEW_SPY = spyOn(colorToolService, 'changePreviewColor');
+
+        component.onCanvasClick(event);
+
+        expect(TRANSLATE_SPY).not.toHaveBeenCalled();
+        expect(GET_PREVIEW_SPY).not.toHaveBeenCalled();
+        expect(CHANGE_PREVIEW_SPY).not.toHaveBeenCalled();
+    });
+
+    it('onCanvasClick should  call changePreviewColor, translateRGBToHex getPreviewColorOpacityHex, and changePreviewColor  obscurity is 1', () => {
+        let event: MouseEvent = createMouseEvent(2, 4, 6, 1, 1);
+
+        const TRANSLATE_SPY = spyOn(colorToolService, 'translateRGBToHex');
+        const GET_PREVIEW_SPY = spyOn(colorToolService, 'getPreviewColorOpacityHex');
+        const CHANGE_PREVIEW_SPY = spyOn(colorToolService, 'changePreviewColor');
+
+        component.obscurity = 1;
+        component.onCanvasClick(event);
+
+        expect(TRANSLATE_SPY).toHaveBeenCalled();
+        expect(GET_PREVIEW_SPY).toHaveBeenCalled();
+        expect(CHANGE_PREVIEW_SPY).toHaveBeenCalled();
     });
 });
