b91b0c5e669f0cf32f9f833f57283651e2066067
Xi Chen <xishec@gmail.com>
2019-10-01 10:37:45 -0400

    XS : R Remove fdescribe

--- client/src/app/services/shortcut-manager/shortcut-manager.service.spec.ts
+++ client/src/app/services/shortcut-manager/shortcut-manager.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { ShortcutManagerService } from './shortcut-manager.service';
 
-fdescribe('ShortcutManagerService', () => {
+describe('ShortcutManagerService', () => {
     beforeEach(() => TestBed.configureTestingModule({}));
 
     it('should be created', () => {
