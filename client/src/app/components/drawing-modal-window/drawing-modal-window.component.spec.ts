f5b80740cffc692961d7f16bfb81d24752657abf
Noboru Yoshida <noboruyoshida1997@gmail.com>
2019-09-30 20:33:47 -0400

    NY : T Set up spec files with service imports and other functions

--- client/src/app/components/drawing-modal-window/drawing-modal-window.component.spec.ts
+++ client/src/app/components/drawing-modal-window/drawing-modal-window.component.spec.ts
@@ -1,13 +1,21 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatDialogRef } from '@angular/material';
 
+import { DrawingModalWindowService } from 'src/app/services/drawing-modal-window/drawing-modal-window.service';
+import { ShortcutManagerService } from 'src/app/services/shortcut-manager/shortcut-manager.service';
+import { ColorToolService } from 'src/app/services/tools/color-tool/color-tool.service';
 import { DrawingModalWindowComponent } from './drawing-modal-window.component';
 
-describe('DrawingModalWindowComponent', () => {
+fdescribe('DrawingModalWindowComponent', () => {
     let component: DrawingModalWindowComponent;
     let fixture: ComponentFixture<DrawingModalWindowComponent>;
+    let form: FormGroup;
+
+    let drawingModalService: DrawingModalWindowService;
+    let colorToolService: ColorToolService;
+    let shortCutManagerService: ShortcutManagerService;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -20,12 +28,43 @@ describe('DrawingModalWindowComponent', () => {
                 useValue: { },
             }],
             schemas: [NO_ERRORS_SCHEMA],
-        }).compileComponents();
+        }).overrideComponent(DrawingModalWindowComponent, {
+            set: {
+                providers: [
+                    {
+                        provide: DrawingModalWindowService,
+                        useValue: {
+                        },
+                    },
+                    {
+                        provide: ColorToolService,
+                        useValue: {
+                        },
+                    },
+                    {
+                        provide: ShortcutManagerService,
+                        useValue: {
+                        },
+                    },
+                ],
+            },
+        })
+        .compileComponents();
 
         fixture = TestBed.createComponent(DrawingModalWindowComponent);
         component = fixture.componentInstance;
 
+        component.initializeForm();
+        form = component.drawingModalForm;
+
+        drawingModalService = fixture.debugElement.injector.get(DrawingModalWindowService);
+        colorToolService = fixture.debugElement.injector.get(ColorToolService);
+        shortCutManagerService = fixture.debugElement.injector.get(ShortcutManagerService);
     }));
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 
     it('should create', () => {
         expect(component).toBeTruthy();
