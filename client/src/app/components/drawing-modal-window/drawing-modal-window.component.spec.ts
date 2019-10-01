6f603137d6641ff8ffbe090d208527eb7c9340b7
Noboru Yoshida <noboruyoshida1997@gmail.com>
2019-09-30 23:21:32 -0400

    NY : T Add test for drawing modal window component

--- client/src/app/components/drawing-modal-window/drawing-modal-window.component.spec.ts
+++ client/src/app/components/drawing-modal-window/drawing-modal-window.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatDialogRef } from '@angular/material';
 
+import { BehaviorSubject } from 'rxjs';
 import { DrawingModalWindowService } from 'src/app/services/drawing-modal-window/drawing-modal-window.service';
 import { ShortcutManagerService } from 'src/app/services/shortcut-manager/shortcut-manager.service';
 import { ColorToolService } from 'src/app/services/tools/color-tool/color-tool.service';
@@ -15,41 +16,56 @@ fdescribe('DrawingModalWindowComponent', () => {
 
     let drawingModalService: DrawingModalWindowService;
     let colorToolService: ColorToolService;
-    let shortCutManagerService: ShortcutManagerService;
+    // let shortCutManagerService: ShortcutManagerService;
+
+    const testColor = '23fe45';
+    const testOpacity = 0.4;
+
+    const dialogMock = {
+        close: () => null,
+    };
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [DrawingModalWindowComponent],
-            providers: [{
-                provide: MatDialogRef,
-                useValue: { },
-            }, {
-                provide: FormBuilder,
-                useValue: { },
-            }],
+            providers: [
+                FormBuilder,
+                {
+                    provide: MatDialogRef,
+                    useValue: {},
+                },
+            ],
             schemas: [NO_ERRORS_SCHEMA],
-        }).overrideComponent(DrawingModalWindowComponent, {
-            set: {
-                providers: [
-                    {
-                        provide: DrawingModalWindowService,
-                        useValue: {
-                        },
-                    },
-                    {
-                        provide: ColorToolService,
-                        useValue: {
-                        },
-                    },
-                    {
-                        provide: ShortcutManagerService,
-                        useValue: {
-                        },
-                    },
-                ],
-            },
         })
-        .compileComponents();
+            .overrideComponent(DrawingModalWindowComponent, {
+                set: {
+                    providers: [
+                        {
+                            provide: MatDialogRef,
+                            useValue: dialogMock,
+                        },
+                        {
+                            provide: DrawingModalWindowService,
+                            useValue: {
+                                changeDisplayNewDrawingModalWindow: () => null,
+                                changeDrawingInfoWidthHeight: () => null,
+                            },
+                        },
+                        {
+                            provide: ColorToolService,
+                            useValue: {
+                                changeBackgroundColor: () => null,
+                                addColorToQueue: () => null,
+                            },
+                        },
+                        {
+                            provide: ShortcutManagerService,
+                            useValue: {},
+                        },
+                    ],
+                },
+            })
+            .compileComponents();
 
         fixture = TestBed.createComponent(DrawingModalWindowComponent);
         component = fixture.componentInstance;
@@ -59,14 +75,60 @@ fdescribe('DrawingModalWindowComponent', () => {
 
         drawingModalService = fixture.debugElement.injector.get(DrawingModalWindowService);
         colorToolService = fixture.debugElement.injector.get(ColorToolService);
-        shortCutManagerService = fixture.debugElement.injector.get(ShortcutManagerService);
+        // shortCutManagerService = fixture.debugElement.injector.get(ShortcutManagerService);
     }));
 
     it('should create', () => {
         expect(component).toBeTruthy();
     });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
+    it('should call Drawing Modal Window service changeDisplayNewDrawingModalWindow function on form submit', () => {
+        const SPY = spyOn(drawingModalService, 'changeDisplayNewDrawingModalWindow');
+        drawingModalService.blankDrawingZone = new BehaviorSubject<boolean>(false);
+        component.onSubmit();
+        expect(SPY).toHaveBeenCalledWith(false);
+    });
+
+    it('should call Drawing Modal Window service changeDrawingInfoWidthHeight function on form submit', () => {
+        const SPY = spyOn(drawingModalService, 'changeDrawingInfoWidthHeight');
+        drawingModalService.blankDrawingZone = new BehaviorSubject<boolean>(false);
+        component.onSubmit();
+        expect(SPY).toHaveBeenCalledWith(form.controls.width.value, form.controls.height.value);
+    });
+
+    it('should call Color Tool service changeBackgroundColor function on form submit', () => {
+        const SPY = spyOn(colorToolService, 'changeBackgroundColor');
+        drawingModalService.blankDrawingZone = new BehaviorSubject<boolean>(false);
+        component.onSubmit();
+        expect(SPY).toHaveBeenCalledWith(component.previewColor);
+    });
+
+    it('should set Drawing Modal Window service blankDrawingZone to false on form submit', () => {
+        drawingModalService.blankDrawingZone = new BehaviorSubject<boolean>(true);
+        component.onSubmit();
+        expect(drawingModalService.blankDrawingZone.value).toEqual(false);
+    });
+
+    it('should call Color Tool service addColorToQueue function on form submit', () => {
+        const SPY = spyOn(colorToolService, 'addColorToQueue');
+        drawingModalService.blankDrawingZone = new BehaviorSubject<boolean>(false);
+        component.onSubmit();
+        expect(SPY).toHaveBeenCalledWith(component.previewColor);
+    });
+
+    it('should close dialog on onCancel', () => {
+        const SPY = spyOn(component[`dialogRef`], 'close');
+        component.onCancel();
+        expect(SPY).toHaveBeenCalled();
+    });
+
+    it('should return the right preview color and opacity when getUserColorIcon funtion is called', () => {
+        component.previewColor = testColor;
+        form.controls.A.setValue(testOpacity);
+        const iconStyle = component.getUserColorIcon();
+        expect(iconStyle).toEqual({
+            backgroundColor: '#' + testColor,
+            opacity: String(testOpacity),
+        });
     });
 });
