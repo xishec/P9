22452d9bab4f48c7c24900f16cff31e7bf4de442
Xi Chen <xishec@gmail.com>
2019-09-30 22:42:41 -0400

    XS : D Add test for attribute-panel

--- client/src/app/components/sidebar-tools/attribute-panel/attribute-panel.component.spec.ts
+++ client/src/app/components/sidebar-tools/attribute-panel/attribute-panel.component.spec.ts
@@ -1,14 +1,35 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AttributePanelComponent } from './attribute-panel.component';
+import { ColorToolService } from '../../../services/tools/color-tool/color-tool.service';
+import { ToolSelectorService } from '../../../services/tools/tool-selector/tool-selector.service';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { ToolName } from 'src/constants/tool-constants';
 
-describe('AttributePanelComponent', () => {
+fdescribe('AttributePanelComponent', () => {
     let component: AttributePanelComponent;
     let fixture: ComponentFixture<AttributePanelComponent>;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [AttributePanelComponent],
+            providers: [
+                AttributePanelComponent,
+                {
+                    provide: ToolSelectorService,
+                    useValue: {
+                        currentToolName: new BehaviorSubject<ToolName>(ToolName.ArtGallery),
+                    },
+                },
+                {
+                    provide: ColorToolService,
+                    useValue: {
+                        showColorPalette: new BehaviorSubject<boolean>(false),
+                    },
+                },
+            ],
+            schemas: [NO_ERRORS_SCHEMA],
         }).compileComponents();
     }));
 
