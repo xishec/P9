4f4fde13e81b73e48d9a6c27987d2a2b4c33d449
Xi Chen <xishec@gmail.com>
2019-09-30 23:16:32 -0400

    XS : Add async test for subscribe

--- client/src/app/components/sidebar-tools/attribute-panel/attribute-panel.component.spec.ts
+++ client/src/app/components/sidebar-tools/attribute-panel/attribute-panel.component.spec.ts
@@ -1,10 +1,10 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 
 import { AttributePanelComponent } from './attribute-panel.component';
 import { ColorToolService } from '../../../services/tools/color-tool/color-tool.service';
 import { ToolSelectorService } from '../../../services/tools/tool-selector/tool-selector.service';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { MatDialog } from '@angular/material';
 import { ToolName } from 'src/constants/tool-constants';
 
 describe('AttributePanelComponent', () => {
@@ -17,16 +17,7 @@ describe('AttributePanelComponent', () => {
             providers: [
                 AttributePanelComponent,
                 {
-                    provide: ToolSelectorService,
-                    useValue: {
-                        currentToolName: new BehaviorSubject<ToolName>(ToolName.ArtGallery),
-                    },
-                },
-                {
-                    provide: ColorToolService,
-                    useValue: {
-                        showColorPalette: new BehaviorSubject<boolean>(false),
-                    },
+                    provide: MatDialog,
                 },
             ],
             schemas: [NO_ERRORS_SCHEMA],
@@ -40,6 +31,29 @@ describe('AttributePanelComponent', () => {
     });
 
     it('should create', () => {
+        inject([ToolSelectorService], (toolSelectorService: ToolSelectorService) => {
+            component[`toolSelectorService`] = toolSelectorService;
+        });
+        inject([ColorToolService], (colorToolService: ColorToolService) => {
+            component[`colorToolService`] = colorToolService;
+        });
         expect(component).toBeTruthy();
     });
+
+    it('should onInit', async(
+        inject([ToolSelectorService], (toolSelectorService: ToolSelectorService) => {
+            const toolName = ToolName.Brush;
+            component.ngOnInit();
+            fixture
+                .whenStable()
+                .then(() => {
+                    expect(component.currentToolName).toBeDefined();
+                    toolSelectorService.changeCurrentToolName(toolName);
+                    return fixture.whenStable();
+                })
+                .then(() => {
+                    expect(component.currentToolName).toEqual(ToolName.Brush);
+                });
+        }),
+    ));
 });
