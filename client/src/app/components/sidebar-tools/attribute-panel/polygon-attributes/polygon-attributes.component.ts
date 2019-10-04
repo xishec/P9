890bb3f3a8f553936a4fa935f0f86a0048d332eb
Kevi <kevin.ciliento@polymtl.ca>
2019-10-04 15:53:16 -0400

    KC : D Add polygon component

---
+++ client/src/app/components/sidebar-tools/attribute-panel/polygon-attributes/polygon-attributes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-polygon-attributes',
+  templateUrl: './polygon-attributes.component.html',
+  styleUrls: ['./polygon-attributes.component.scss']
+})
+export class PolygonAttributesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
