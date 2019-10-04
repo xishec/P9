979312c50d557a29ad9bb02a95ae75ba4c9c024d
Roman <Romazho123@gmail.com>
2019-10-04 13:08:42 -0400

    RZ : D Add basic mouse and key functions from AbstractShapeToolService

--- client/src/app/services/tools/polygon-tool/polygon-tool.service.ts
+++ client/src/app/services/tools/polygon-tool/polygon-tool.service.ts
@@ -1,15 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable, ElementRef, Renderer2 } from '@angular/core';
 import { ColorToolService } from '../color-tool/color-tool.service';
+import { DrawStackService } from '../../draw-stack/draw-stack.service';
+import { AbstractShapeToolService } from '../abstract-tools/abstract-shape-tool/abstract-shape-tool.service';
 
 @Injectable({
     providedIn: 'root',
 })
-export class PolygonToolService {
+export class PolygonToolService extends AbstractShapeToolService {
     colorToolService: ColorToolService;
     fillColor = '';
     strokeColor = '';
 
-    constructor() {}
+    constructor(public drawStack: DrawStackService, public svgReference: ElementRef<SVGElement>, renderer: Renderer2) {
+        super(renderer);
+    }
 
     initializeColorToolService(colorToolService: ColorToolService) {
         this.colorToolService = colorToolService;
@@ -21,5 +25,44 @@ export class PolygonToolService {
             this.strokeColor = strokeColor;
             //this.updateTraceType(this.traceType);
         });
+    }
+
+    createSVG(): void {}
+
+    onMouseMove(event: MouseEvent): void {
+        this.currentMouseX = event.clientX - this.svgReference.nativeElement.getBoundingClientRect().left;
+        this.currentMouseY = event.clientY - this.svgReference.nativeElement.getBoundingClientRect().top;
+
+        // if (this.isPreviewing) {
+        //     this.updateDrawing();
+        // }
+    }
+
+    onMouseDown(event: MouseEvent): void {}
+
+    onMouseUp(event: MouseEvent): void {}
+
+    onMouseEnter(event: MouseEvent): void {
+        this.isIn = true;
+    }
+
+    onMouseLeave(event: MouseEvent): void {
+        this.isIn = false;
+    }
+
+    onKeyDown(event: KeyboardEvent): void {}
+
+    onKeyUp(event: KeyboardEvent): void {
+        // const key = event.key;
+        // switch (key) {
+        //     case Keys.Shift:
+        //         if (this.isSquarePreview) {
+        //             this.isSquarePreview = false;
+        //             this.updateDrawing();
+        //         }
+        //         break;
+        //     default:
+        //         break;
+        // }
     }
 }
