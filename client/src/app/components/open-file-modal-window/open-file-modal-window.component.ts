16e612c490ecdc4602dc2f363b884d68684eb875
Noboru Yoshida <noboruyoshida1997@gmail.com>
2019-10-04 15:46:21 -0400

    NY : D Create open file modal window component

---
+++ client/src/app/components/open-file-modal-window/open-file-modal-window.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-open-file-modal-window',
+  templateUrl: './open-file-modal-window.component.html',
+  styleUrls: ['./open-file-modal-window.component.scss']
+})
+export class OpenFileModalWindowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
