53f74c709ffc8f22b5cc46bddb3584ee4e2e4a14
HPay <hakimpayman@gmail.com>
2019-10-04 13:54:05 -0400

    HP : D Create dropper logic

--- client/src/app/services/tools/dropper-tool/dropper-tool.service.ts
+++ client/src/app/services/tools/dropper-tool/dropper-tool.service.ts
@@ -6,10 +6,12 @@ import { DrawStackService } from '../../draw-stack/draw-stack.service';
     providedIn: 'root',
 })
 export class DropperToolService extends AbstractToolService {
+    currentMouseX = 0;
+    currentMouseY = 0;
     pixelColor: string;
     canvas: HTMLCanvasElement;
     context2D: CanvasRenderingContext2D;
-    svgCopy: SVGElement;
+    SVGImg: HTMLImageElement = this.renderer.createElement('img');
 
     constructor(public drawStack: DrawStackService, public svgReference: ElementRef<SVGElement>, public renderer: Renderer2) {
         super();
@@ -19,11 +21,25 @@ export class DropperToolService extends AbstractToolService {
     }
 
     updateSVGCopy(): void {
-        this.context2D = this.canvas.getContext('2d') as CanvasRenderingContext2D;
+        const serializedSVG = new XMLSerializer().serializeToString(this.svgReference.nativeElement);
+        const base64SVG = btoa(serializedSVG);
+        this.SVGImg.src = base64SVG;
+        this.context2D.drawImage(this.SVGImg, 0, 0);
+    }
+
+    pickColor(): void {
+        console.log(this.currentMouseX + ' ' + this.currentMouseY);
+        const data = this.context2D.getImageData(this.currentMouseX, this.currentMouseY, 1, 1);
+        console.log(data);
     }
 
     onMouseMove(event: MouseEvent): void {}
-    onMouseDown(event: MouseEvent): void {}
+    onMouseDown(event: MouseEvent): void {
+        this.currentMouseX = event.clientX - this.svgReference.nativeElement.getBoundingClientRect().left;
+        this.currentMouseY = event.clientY - this.svgReference.nativeElement.getBoundingClientRect().top;
+        this.updateSVGCopy();
+        this.pickColor();
+    }
     onMouseUp(event: MouseEvent): void {}
     onMouseEnter(event: MouseEvent): void {}
     onMouseLeave(event: MouseEvent): void {}
