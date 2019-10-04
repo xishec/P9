c00381acf224bdf735507bca3ccaed615b002dc5
Louis Popovic <louis.popovic@polymtl.ca>
2019-10-04 13:19:22 -0400

    LP : D Create line-attributes.component and line-tool.service

---
+++ client/src/app/components/sidebar-tools/attribute-panel/line-attributes/line-attributes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-line-attributes',
+  templateUrl: './line-attributes.component.html',
+  styleUrls: ['./line-attributes.component.scss']
+})
+export class LineAttributesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
