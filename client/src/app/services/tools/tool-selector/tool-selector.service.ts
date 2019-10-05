4e6e60d59bb5b5350e4f739758ec3267521e5d53
Xi Chen <xishec@gmail.com>
2019-10-05 15:58:33 -0400

    XS : B Disable auto close of new drawing modal window

--- client/src/app/services/tools/tool-selector/tool-selector.service.ts
+++ client/src/app/services/tools/tool-selector/tool-selector.service.ts
@@ -54,6 +54,7 @@ export class ToolSelectorService {
     displayNewDrawingModal(): void {
         const dialogRef = this.dialog.open(DrawingModalWindowComponent, {
             panelClass: 'myapp-max-width-dialog',
+            disableClose: true,
         });
         this.drawingModalWindowService.changeDisplayNewDrawingModalWindow(true);
         dialogRef.afterClosed().subscribe(() => {
