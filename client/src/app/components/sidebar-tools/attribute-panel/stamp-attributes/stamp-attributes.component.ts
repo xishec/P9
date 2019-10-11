b6905210646ee83dbbd5aa46fb949017ff13ae8f
HPay <hakimpayman@gmail.com>
2019-10-11 10:46:14 -0400

    HP : D Add Stamp tool service and stamp attributes component

---
+++ client/src/app/components/sidebar-tools/attribute-panel/stamp-attributes/stamp-attributes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stamp-attributes',
+  templateUrl: './stamp-attributes.component.html',
+  styleUrls: ['./stamp-attributes.component.scss']
+})
+export class StampAttributesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
