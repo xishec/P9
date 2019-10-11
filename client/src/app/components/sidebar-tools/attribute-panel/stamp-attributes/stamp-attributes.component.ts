4cf97415262b7c42f824ef47a7398f829b12cb4f
Roman <Romazho123@gmail.com>
2019-10-11 18:53:33 -0400

    RZ : WP Add scaling slider for stamp

--- client/src/app/components/sidebar-tools/attribute-panel/stamp-attributes/stamp-attributes.component.ts
+++ client/src/app/components/sidebar-tools/attribute-panel/stamp-attributes/stamp-attributes.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { ToolName } from 'src/constants/tool-constants';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSliderChange } from '@angular/material';
+
+import { ShortcutManagerService } from 'src/app/services/shortcut-manager/shortcut-manager.service';
+import { ToolName, StampScaling } from 'src/constants/tool-constants';
+import { AttributesManagerService } from '../../../../services/tools/attributes-manager/attributes-manager.service';
+import { ToolSelectorService } from '../../../../services/tools/tool-selector/tool-selector.service';
+import { StampToolService } from 'src/app/services/tools/stamp-tool/stamp-tool.service';
 
 @Component({
     selector: 'app-stamp-attributes',
@@ -9,7 +16,63 @@ import { ToolName } from 'src/constants/tool-constants';
 export class StampAttributesComponent implements OnInit {
     toolName = ToolName.Stamp;
 
-    constructor() {}
+    stampAttributesForm: FormGroup;
+    stampToolService: StampToolService;
+    readonly stampScaling = StampScaling;
 
-    ngOnInit() {}
+    constructor(
+        private formBuilder: FormBuilder,
+        private attributesManagerService: AttributesManagerService,
+        private toolSelectorService: ToolSelectorService,
+        private shortcutManagerService: ShortcutManagerService,
+    ) {
+        this.formBuilder = formBuilder;
+    }
+
+    ngOnInit(): void {
+        this.initializeForm();
+        this.onScalingChange();
+    }
+
+    ngAfterViewInit(): void {
+        //this.stampToolService = this.toolSelectorService.getRectangleTool();
+        //this.stampToolService.initializeAttributesManagerService(this.attributesManagerService);
+    }
+
+    initializeForm(): void {
+        this.stampAttributesForm = this.formBuilder.group({
+            scaling: [
+                StampScaling.Default,
+                [Validators.required, Validators.min(StampScaling.Min), Validators.max(StampScaling.Max)],
+            ],
+            traceType: ['Contour'],
+        });
+    }
+
+    onSliderChange(event: MatSliderChange): void {
+        if (event.value !== null && event.value <= StampScaling.Max && event.value >= StampScaling.Min) {
+            this.stampAttributesForm.controls.scaling.setValue(event.value);
+            this.onScalingChange();
+        }
+    }
+
+    onScalingChange(): void {
+        const stampScaling: number = this.stampAttributesForm.value.scaling;
+        if (stampScaling >= StampScaling.Min && stampScaling <= StampScaling.Max) {
+            this.attributesManagerService.changeThickness(stampScaling);
+        }
+    }
+
+    onTraceTypeChange(): void {
+        const tracetype: string = this.stampAttributesForm.value.traceType;
+        this.attributesManagerService.changeTraceType(tracetype);
+    }
+
+    onFocus() {
+        this.shortcutManagerService.changeIsOnInput(true);
+    }
+
+    onFocusOut() {
+        this.shortcutManagerService.changeIsOnInput(false);
+    }
 }
