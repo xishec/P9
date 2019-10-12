16b660f363b620746709e963c1efc6f2eadefc63
Kevin <kevinscottsciliento@gmail.com>
2019-10-12 11:25:30 -0400

    KC : D Add grid-attribute component

---
+++ client/src/app/components/sidebar-tools/attribute-panel/grid-attributes/grid-attributes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-attributes',
+  templateUrl: './grid-attributes.component.html',
+  styleUrls: ['./grid-attributes.component.scss']
+})
+export class GridAttributesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
