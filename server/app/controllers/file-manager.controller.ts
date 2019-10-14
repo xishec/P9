502ca8ec91de298cc9e0e92039d001f0d9836d71
Xi Chen <xishec@gmail.com>
2019-10-14 00:21:29 -0400

    XS : D Update and format file manager router

--- server/app/controllers/file-manager.controller.ts
+++ server/app/controllers/file-manager.controller.ts
@@ -1,25 +1,33 @@
-import { NextFunction, Request, Response, Router } from "express";
-import { injectable } from "inversify";
+import { NextFunction, Request, Response, Router } from 'express';
+import { injectable } from 'inversify';
+
+import { Message } from '../../../common/communication/message';
+const Post = require('../model/post');
 
 @injectable()
 export class FileManagerController {
-    public router: Router;
+	public router: Router;
 
-    constructor() {
-        this.configureRouter();
-    }
+	constructor() {
+		this.configureRouter();
+	}
 
-    private configureRouter(): void {
-        this.router = Router();
+	private configureRouter(): void {
+		this.router = Router();
 
-        this.router.get("/open", async (req: Request, res: Response, next: NextFunction) => {
-            // Send the request to the service and send the response
-            res.json({AILLLLLE: "HELLLOOOOOO P9"});
-        });
+		this.router.get('/open', async (req: Request, res: Response, next: NextFunction) => {
+			// Send the request to the service and send the response
+			let message: Message = { title: 'AILLLLLE', body: 'HELLLOOOOOO P9' };
+			res.json(message);
+		});
 
-        this.router.get("/save", (req: Request, res: Response, next: NextFunction) => {
-            // Send the request to the service and send the response
-            res.json({OMG: "IL FAUT SAVEEEEE"});
-        });
-    }
+		this.router.post('/save', (req: Request, res: Response, next: NextFunction) => {
+			// Send the request to the service and send the response
+			const message = req.body;
+			let post = new Post(message);
+			post.save();
+			// console.log('hi ->', message);
+			res.json(message);
+		});
+	}
 }
