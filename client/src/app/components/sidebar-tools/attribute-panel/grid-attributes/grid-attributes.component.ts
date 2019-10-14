16a1250daafefc1b4dacee03224f107ccdf5bc7e
Kevin <kevinscottsciliento@gmail.com>
2019-10-14 16:49:56 -0400

    KC : D Add verification on user input for gridsize and grid opacity

--- client/src/app/components/sidebar-tools/attribute-panel/grid-attributes/grid-attributes.component.ts
+++ client/src/app/components/sidebar-tools/attribute-panel/grid-attributes/grid-attributes.component.ts
@@ -2,10 +2,7 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { MatSliderChange } from '@angular/material';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ShortcutManagerService } from 'src/app/services/shortcut-manager/shortcut-manager.service';
-import { ColorToolService } from 'src/app/services/tools/color-tool/color-tool.service';
 import { GridSize, GridOpacity, ToolName } from 'src/constants/tool-constants';
-import { AttributesManagerService } from '../../../../services/tools/attributes-manager/attributes-manager.service';
-import { ToolSelectorService } from '../../../../services/tools/tool-selector/tool-selector.service';
 import { GridToolService } from 'src/app/services/tools/grid-tool/grid-tool.service';
 
 @Component({
@@ -16,31 +13,46 @@ import { GridToolService } from 'src/app/services/tools/grid-tool/grid-tool.serv
 export class GridAttributesComponent implements OnInit, AfterViewInit {
     toolName = ToolName.Grid;
     gridAttributesForm: FormGroup;
-    gridToolService: GridToolService = new GridToolService();
 
     readonly gridSize = GridSize;
     readonly gridOpacity = GridOpacity;
 
-    constructor(private formBuilder: FormBuilder, private shortcutManagerService: ShortcutManagerService) {
+    constructor(
+        private formBuilder: FormBuilder,
+        private shortcutManagerService: ShortcutManagerService,
+        private gridToolService: GridToolService
+    ) {
         this.formBuilder = formBuilder;
     }
 
     eventIsValid<T>(event: MatSliderChange, range: T): Boolean {
         const value = event.value;
-        if (value !== null) return value >= range.Min && value <= range.Max;
-        else return false;
+        //@ts-ignore
+        if (value !== null) {
+            return this.IsBetween(value, range);
+        } else {
+            return false;
+        }
+    }
+
+    IsBetween<T>(value: number | boolean, range: T): Boolean {
+        //@ts-ignore
+        return value >= range.Min && value <= range.Max;
     }
 
     ngOnInit(): void {
         this.initializeForm();
         this.onSizeChange();
+        this.gridToolService.currentState.subscribe((state) => {
+            this.gridAttributesForm.controls.state.setValue(state);
+        });
     }
 
     ngAfterViewInit(): void {}
 
     initializeForm(): void {
         this.gridAttributesForm = this.formBuilder.group({
-            state: ['false'],
+            state: [false],
             size: [GridSize.Default, [Validators.required, Validators.min(GridSize.Min), Validators.max(GridSize.Max)]],
             opacity: [
                 GridOpacity.Max,
@@ -52,7 +64,7 @@ export class GridAttributesComponent implements OnInit, AfterViewInit {
     onSizeSliderChange(event: MatSliderChange) {
         if (this.eventIsValid(event, this.gridSize)) {
             this.gridAttributesForm.controls.size.setValue(event.value);
-            this.onOpacityChange();
+            this.onSizeChange();
         }
     }
 
@@ -68,9 +80,23 @@ export class GridAttributesComponent implements OnInit, AfterViewInit {
         this.gridToolService.changeState(state);
     }
 
-    onSizeChange() {}
+    onSizeChange() {
+        const size = this.gridAttributesForm.value.size;
+        if (this.IsBetween(size, GridSize)) {
+            this.gridToolService.changeSize(size);
+        }
+    }
 
-    onOpacityChange() {}
+    onOpacityChange() {
+        const opacity = this.gridAttributesForm.value.opacity;
+        if (this.IsBetween(opacity, GridOpacity)) {
+            this.gridToolService.changeOpacity(opacity);
+        }
+    }
+
+    changeState(state: boolean) {
+        this.gridAttributesForm.controls.state.setValue(state);
+    }
 
     onFocus() {
         this.shortcutManagerService.changeIsOnInput(true);
