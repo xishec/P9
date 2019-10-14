5bb4129d688586d581b418422c8a7dae6cc7cb5c
Kevin <kevinscottsciliento@gmail.com>
2019-10-14 16:39:17 -0400

    KC : D Add Observable size and opacity for grid

--- client/src/app/services/tools/grid-tool/grid-tool.service.ts
+++ client/src/app/services/tools/grid-tool/grid-tool.service.ts
@@ -1,16 +1,28 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { GridSize, GridOpacity } from 'src/constants/tool-constants';
 
 @Injectable({
     providedIn: 'root',
 })
 export class GridToolService {
     state: BehaviorSubject<boolean> = new BehaviorSubject(false);
+    size: BehaviorSubject<number> = new BehaviorSubject(GridSize.Default);
+    opacity: BehaviorSubject<number> = new BehaviorSubject(GridOpacity.Max);
 
     currentState: Observable<boolean> = this.state.asObservable();
+    currentSize: Observable<number> = this.size.asObservable();
+    currentOpacity: Observable<number> = this.opacity.asObservable();
 
     changeState(state: boolean) {
         this.state.next(state);
-        console.log(state);
+    }
+
+    changeSize(size: number) {
+        this.size.next(size);
+    }
+
+    changeOpacity(opacity: number) {
+        this.opacity.next(opacity);
     }
 }
