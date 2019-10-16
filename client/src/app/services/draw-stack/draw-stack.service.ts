316c2b0b6f74c366ee3aa7bb9c819959e843c2f7
HPay <hakimpayman@gmail.com>
2019-10-16 12:53:08 -0400

    HP : D Add idStack to drawStack

--- client/src/app/services/draw-stack/draw-stack.service.ts
+++ client/src/app/services/draw-stack/draw-stack.service.ts
@@ -8,6 +8,7 @@ import { StackTargetInfo } from 'src/classes/StackTargetInfo';
 })
 export class DrawStackService {
     private drawStack: SVGGElement[] = new Array<SVGGElement>();
+    private idStack: string[] = new Array<string>();
     private stackTarget: BehaviorSubject<StackTargetInfo> = new BehaviorSubject(new StackTargetInfo());
     currentStackTarget: Observable<StackTargetInfo> = this.stackTarget.asObservable();
     renderer: Renderer2;
@@ -30,6 +31,7 @@ export class DrawStackService {
         const position = this.drawStack.length;
         const tool = el.getAttribute('title');
         this.renderer.setAttribute(el, 'id', position.toString());
+        this.idStack.push(el.getAttribute('id') as string);
 
         for (let i = 0; i < el.children.length; i++) {
             this.renderer.listen(el.children.item(i), 'mousedown', () => {
@@ -49,6 +51,7 @@ export class DrawStackService {
     }
 
     reset(): SVGGElement[] {
+        this.idStack.splice(0, this.idStack.length);
         return this.drawStack.splice(0, this.drawStack.length);
     }
 }
