afe68828aaaaab62a68d2e3fc298e1398496476e
Xi Chen <xishec@gmail.com>
2019-10-16 17:30:40 -0400

    XS : R Remove index service in app.component

--- client/src/app/components/app/app.component.ts
+++ client/src/app/components/app/app.component.ts
@@ -1,13 +1,9 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
-import { BehaviorSubject } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 import { ToolName } from 'src/constants/tool-constants';
-import { Message } from '../../../../../common/communication/message';
 import { WelcomeModalWindowComponent } from '../../components/welcome-modal-window/welcome-modal-window.component';
 import { DrawingModalWindowService } from '../../services/drawing-modal-window/drawing-modal-window.service';
-import { IndexService } from '../../services/server/index/index.service';
 import { ShortcutManagerService } from '../../services/shortcut-manager/shortcut-manager.service';
 import { ToolSelectorService } from '../../services/tools/tool-selector/tool-selector.service';
 import { WelcomeModalWindowService } from '../../services/welcome-modal-window/welcome-modal-window.service';
@@ -18,26 +14,18 @@ import { WelcomeModalWindowService } from '../../services/welcome-modal-window/w
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-    readonly title = 'LOG2990';
-    message = new BehaviorSubject<string>('');
     displayNewDrawingModalWindow = false;
     displayWelcomeModalWindow = false;
     welcomeModalWindowClosed = false;
     isOnInput = false;
 
     constructor(
-        private basicService: IndexService,
         private welcomeModalWindowService: WelcomeModalWindowService,
         private dialog: MatDialog,
         private toolSelectorService: ToolSelectorService,
         private drawingModalWindowService: DrawingModalWindowService,
         private shortcutManagerService: ShortcutManagerService,
-    ) {
-        this.basicService
-            .basicGet()
-            .pipe(map((message: Message) => `${message.title} ${message.body}`))
-            .subscribe(this.message);
-    }
+    ) {}
 
     ngOnInit(): void {
         this.drawingModalWindowService.currentDisplayNewDrawingModalWindow.subscribe(
