7d2d43092359662ade28e8aab56595c4f8976583
Noboru Yoshida <noboruyoshida1997@gmail.com>
2019-10-17 14:49:55 -0400

    NY : D Add some spice to the app's UI

--- client/src/app/components/modal-windows/save-file-modal-window/save-file-modal-window.component.ts
+++ client/src/app/components/modal-windows/save-file-modal-window/save-file-modal-window.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { MatDialogRef } from '@angular/material';
+import { ModalManagerService } from 'src/app/services/modal-manager/modal-manager.service';
 
 @Component({
-  selector: 'app-save-file-modal-window',
-  templateUrl: './save-file-modal-window.component.html',
-  styleUrls: ['./save-file-modal-window.component.scss']
+    selector: 'app-save-file-modal-window',
+    templateUrl: './save-file-modal-window.component.html',
+    styleUrls: ['./save-file-modal-window.component.scss'],
 })
 export class SaveFileModalWindowComponent implements OnInit {
+    saveFileModalForm: FormGroup;
+    formBuilder: FormBuilder;
 
-  constructor() { }
+    drawingLabels: string[] = ['flower', 'hakim', 'kevin', 'roman'];
 
-  ngOnInit() {
+    constructor(
+        formBuilder: FormBuilder,
+        private dialogRef: MatDialogRef<SaveFileModalWindowComponent>,
+        private modalManagerService: ModalManagerService
+    ) {
+      this.formBuilder = formBuilder;
+    }
+
+    ngOnInit() {
+      this.initializeForm();
+    }
+
+    initializeForm(): void {
+      this.saveFileModalForm = this.formBuilder.group({
+      });
   }
 
+    onCancel(): void {
+      this.dialogRef.close();
+      this.modalManagerService.setModalIsDisplayed(false);
+  }
 }
