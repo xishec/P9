d53088b3c45b9ebc75ec3d3b8470dfb817aa7380
Louis Popovic <louispop@gmail.com>
2019-10-19 17:09:53 -0400

    LP : D Add configureTestingModule

--- client/src/app/services/event-listener/event-listener.service.spec.ts
+++ client/src/app/services/event-listener/event-listener.service.spec.ts
@@ -1,9 +1,29 @@
 import { TestBed } from '@angular/core/testing';
 
-import { EventListenerService } from '../event-listener.service';
+import { EventListenerService } from '../event-listener/event-listener.service';
+import { ElementRef } from '@angular/core';
+import { MatDialog } from '@angular/material';
 
 fdescribe('EventListenerService', () => {
-    beforeEach(() => TestBed.configureTestingModule({}));
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                {
+                    provide: ElementRef,
+                    useValue : {
+
+                    }
+                },
+                {
+                    provide: MatDialog,
+                    useValue: {
+
+                    }
+                }
+            ]
+        });
+    });
 
     it('should be created', () => {
         const service: EventListenerService = TestBed.get(EventListenerService);
