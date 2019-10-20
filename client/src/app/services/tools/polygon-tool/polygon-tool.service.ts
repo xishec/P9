af450dc7c6b4657943be4cbe3539a3d46d880379
Louis Popovic <louispop@gmail.com>
2019-10-20 12:16:04 -0400

    LP : R  Change attributes for enum in polygon tool

--- client/src/app/services/tools/polygon-tool/polygon-tool.service.ts
+++ client/src/app/services/tools/polygon-tool/polygon-tool.service.ts
@@ -1,7 +1,7 @@
 import { ElementRef, Injectable, Renderer2 } from '@angular/core';
 
 import { Mouse, SVG_NS } from 'src/constants/constants';
-import { ToolName, TraceType, PolygonRadiusCorrection, PolygonOffsetAngles } from 'src/constants/tool-constants';
+import { ToolName, TraceType, PolygonRadiusCorrection, PolygonOffsetAngles, svgAttribute } from 'src/constants/tool-constants';
 import { DrawStackService } from '../../draw-stack/draw-stack.service';
 import { AbstractShapeToolService } from '../abstract-tools/abstract-shape-tool/abstract-shape-tool.service';
 import { AttributesManagerService } from '../attributes-manager/attributes-manager.service';
@@ -109,7 +109,7 @@ export class PolygonToolService extends AbstractShapeToolService {
             vertices += vertex.x + ',' + vertex.y + ' ';
         }
 
-        this.renderer.setAttribute(drawPolygon, 'points', vertices);
+        this.renderer.setAttribute(drawPolygon, svgAttribute.points, vertices);
     }
 
     updatePreviewRectangle() {
@@ -122,38 +122,38 @@ export class PolygonToolService extends AbstractShapeToolService {
 
         if (deltaX < 0) {
             this.renderer.setAttribute(this.previewRectangle, 'x', (this.initialMouseX - minLength).toString());
-            this.renderer.setAttribute(this.previewRectangle, 'width', minLength.toString());
+            this.renderer.setAttribute(this.previewRectangle, svgAttribute.width, minLength.toString());
         } else {
             this.renderer.setAttribute(this.previewRectangle, 'x', this.initialMouseX.toString());
-            this.renderer.setAttribute(this.previewRectangle, 'width', minLength.toString());
+            this.renderer.setAttribute(this.previewRectangle, svgAttribute.width, minLength.toString());
         }
 
         if (deltaY < 0) {
             this.renderer.setAttribute(this.previewRectangle, 'y', (this.initialMouseY - minLength).toString());
-            this.renderer.setAttribute(this.previewRectangle, 'height', minLength.toString());
+            this.renderer.setAttribute(this.previewRectangle, svgAttribute.height, minLength.toString());
         } else {
             this.renderer.setAttribute(this.previewRectangle, 'y', this.initialMouseY.toString());
-            this.renderer.setAttribute(this.previewRectangle, 'height', minLength.toString());
+            this.renderer.setAttribute(this.previewRectangle, svgAttribute.height, minLength.toString());
         }
 
-        this.renderer.setAttribute(this.previewRectangle, 'fill', 'white');
+        this.renderer.setAttribute(this.previewRectangle, svgAttribute.fill, 'white');
         this.renderer.setAttribute(this.previewRectangle, 'fill-opacity', '0.3');
-        this.renderer.setAttribute(this.previewRectangle, 'stroke', 'black');
-        this.renderer.setAttribute(this.previewRectangle, 'stroke-dasharray', '5 5');
+        this.renderer.setAttribute(this.previewRectangle, svgAttribute.stroke, 'black');
+        this.renderer.setAttribute(this.previewRectangle, svgAttribute.stroke_dasharray, '5 5');
     }
 
     renderdrawPolygon(drawPolygon: SVGPolygonElement = this.drawPolygon): void {
         if (this.isValidePolygon()) {
             this.userFillColor === 'none'
-                ? this.renderer.setAttribute(drawPolygon, 'fill', this.userFillColor)
-                : this.renderer.setAttribute(drawPolygon, 'fill', '#' + this.userFillColor);
-            this.renderer.setAttribute(drawPolygon, 'stroke', '#' + this.userStrokeColor);
-            this.renderer.setAttribute(drawPolygon, 'stroke-width', this.userStrokeWidth.toString());
-            this.renderer.setAttribute(drawPolygon, 'stroke-linejoin', 'round');
+                ? this.renderer.setAttribute(drawPolygon, svgAttribute.fill, this.userFillColor)
+                : this.renderer.setAttribute(drawPolygon, svgAttribute.fill, '#' + this.userFillColor);
+            this.renderer.setAttribute(drawPolygon, svgAttribute.stroke, '#' + this.userStrokeColor);
+            this.renderer.setAttribute(drawPolygon, svgAttribute.stroke_width, this.userStrokeWidth.toString());
+            this.renderer.setAttribute(drawPolygon, svgAttribute.stroke_linejoin, 'round');
         } else {
-            this.renderer.setAttribute(drawPolygon, 'fill', 'none');
-            this.renderer.setAttribute(drawPolygon, 'stroke', 'none');
-            this.renderer.setAttribute(drawPolygon, 'stroke-width', '0');
+            this.renderer.setAttribute(drawPolygon, svgAttribute.fill, 'none');
+            this.renderer.setAttribute(drawPolygon, svgAttribute.stroke, 'none');
+            this.renderer.setAttribute(drawPolygon, svgAttribute.stroke_width, '0');
         }
     }
 
@@ -169,7 +169,7 @@ export class PolygonToolService extends AbstractShapeToolService {
         this.copyPreviewRectangleAttributes(drawPolygon);
         this.renderdrawPolygon(drawPolygon);
 
-        this.renderer.setAttribute(el, 'title', ToolName.Polygon);
+        this.renderer.setAttribute(el, svgAttribute.title, ToolName.Polygon);
         this.renderer.appendChild(el, drawPolygon);
         this.drawStack.push(el);
         this.renderer.appendChild(this.svgReference.nativeElement, el);
