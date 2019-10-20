a5b6ad83c8982cfb97eeda9f14a5596f6e94b791
Roman <Romazho123@gmail.com>
2019-10-20 12:44:00 -0400

    RZ : T Make the "it should be created" test pass

--- client/src/app/services/tools/ellipsis-tool/ellipsis-tool.service.spec.ts
+++ client/src/app/services/tools/ellipsis-tool/ellipsis-tool.service.spec.ts
@@ -1,12 +1,56 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, getTestBed } from '@angular/core/testing';
 
 import { EllipsisToolService } from './ellipsis-tool.service';
+import { ElementRef, Renderer2 } from '@angular/core';
+import { MatDialog } from '@angular/material';
 
-describe('EllipsisToolService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+fdescribe('EllipsisToolService', () => {
+    let injector: TestBed;
+    let service: EllipsisToolService;
 
-  it('should be created', () => {
-    const service: EllipsisToolService = TestBed.get(EllipsisToolService);
-    expect(service).toBeTruthy();
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                EllipsisToolService,
+                {
+                    provide: MatDialog,
+                    useValue: {},
+                },
+                {
+                    provide: Renderer2,
+                    useValue: {
+                        createElement: () => null,
+                        setAttribute: () => null,
+                        appendChild: () => null,
+                        removeChild: () => null,
+                    },
+                },
+                {
+                    provide: ElementRef,
+                    useValue: {
+                        nativeElement: {
+                            getBoundingClientRect: () => {
+                                const boundleft = 0;
+                                const boundtop = 0;
+                                const boundRect = {
+                                    left: boundleft,
+                                    top: boundtop,
+                                };
+                                return boundRect;
+                            },
+                        },
+                    },
+                },
+            ],
+        });
+
+        injector = getTestBed();
+        service = injector.get(EllipsisToolService);
+
+
+        // onAltKeyDown = createKeyBoardEvent(Keys.Alt);
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
