144be4f7979365d28311fc96c1ee5e05923c6c96
Roman <Romazho123@gmail.com>
2019-10-20 16:07:24 -0400

    RZ : T Add all necessary components to be able to test all functions

--- client/src/app/services/tools/ellipsis-tool/ellipsis-tool.service.spec.ts
+++ client/src/app/services/tools/ellipsis-tool/ellipsis-tool.service.spec.ts
@@ -1,3 +1,102 @@
+import { ElementRef, Renderer2 } from '@angular/core';
+import { TestBed, getTestBed } from '@angular/core/testing';
+
+import { EllipsisToolService } from './ellipsis-tool.service';
+import { createMouseEvent, createKeyBoardEvent } from 'src/classes/test-helpers';
+import { Keys } from 'src/constants/constants';
+
+fdescribe('EllipsisToolService', () => {
+    const NONE: string = 'none';
+    const NOTNONE: string = 'not none';
+
+    let injector: TestBed;
+    let service: EllipsisToolService;
+    let leftMouseEvent: MouseEvent;
+    let rightMouseEvent: MouseEvent;
+    let onShiftKeyDown: KeyboardEvent;
+    let onOtherKeyDown: KeyboardEvent;
+
+    let spyOnPreviewRectangleWidth: jasmine.Spy;
+    let spyOnPreviewRectangleHeight: jasmine.Spy;
+    let spyOnPreviewRectangleX: jasmine.Spy;
+    let spyOnPreviewRectangleY: jasmine.Spy;
+    let spyOnSetAttribute: jasmine.Spy;
+    let spyOnDrawEllipseCenterX: jasmine.Spy;
+    let spyOnDrawEllipseCenterY: jasmine.Spy;
+    let spyOnDrawEllipseRadiusX: jasmine.Spy;
+    let spyOnDrawEllipseRadiusY: jasmine.Spy;
+    let spyOnDrawStackPush: jasmine.Spy;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                EllipsisToolService,
+                {
+                    provide: Renderer2,
+                    useValue: {
+                        createElement: () => null,
+                        setAttribute: () => null,
+                        appendChild: () => null,
+                        removeChild: () => null,
+                    },
+                },
+                {
+                    provide: ElementRef,
+                    useValue: {
+                        nativeElement: {
+                            getBoundingClientRect: () => {
+                                const boundleft = 0;
+                                const boundtop = 0;
+                                const boundRect = {
+                                    left: boundleft,
+                                    top: boundtop,
+                                };
+                                return boundRect;
+                            },
+                        },
+                    },
+                },
+            ],
+        });
+
+        injector = getTestBed();
+        service = injector.get(EllipsisToolService);
+
+        leftMouseEvent = createMouseEvent(10, 10, 0);
+        rightMouseEvent = createMouseEvent(10, 10, 2);
+
+        onShiftKeyDown = createKeyBoardEvent(Keys.Shift);
+        onOtherKeyDown = createKeyBoardEvent(Keys.Alt);
+
+        spyOnSetAttribute = spyOn(service.renderer, 'setAttribute').and.returnValue();
+        spyOnDrawStackPush = spyOn(service.drawStack, 'push').and.returnValue();
+
+        spyOnPreviewRectangleWidth = spyOnProperty(service, 'previewRectangleWidth', 'get').and.callFake(() => {
+            return 20;
+        });
+        spyOnPreviewRectangleHeight = spyOnProperty(service, 'previewRectangleHeight', 'get').and.callFake(() => {
+            return 10;
+        });
+        spyOnPreviewRectangleX = spyOnProperty(service, 'previewRectangleX', 'get').and.callFake(() => {
+            return 15;
+        });
+        spyOnPreviewRectangleY = spyOnProperty(service, 'previewRectangleY', 'get').and.callFake(() => {
+            return 15;
+        });
+        spyOnDrawEllipseCenterX = spyOnProperty(service, 'drawEllipseCenterX', 'get').and.callFake(() => {
+            return 30;
+        });
+        spyOnDrawEllipseCenterY = spyOnProperty(service, 'drawEllipseCenterY', 'get').and.callFake(() => {
+            return 30;
+        });
+        spyOnDrawEllipseRadiusX = spyOnProperty(service, 'drawEllipseRadiusX', 'get').and.callFake(() => {
+            return 30;
+        });
+        spyOnDrawEllipseRadiusY = spyOnProperty(service, 'drawEllipseRadiusY', 'get').and.callFake(() => {
+            return 30;
+        });
+    });
+
 import { TestBed, getTestBed } from '@angular/core/testing';
 
 import { EllipsisToolService } from './ellipsis-tool.service';
