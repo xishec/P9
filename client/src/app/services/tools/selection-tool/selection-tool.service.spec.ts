9d31891b634429dd81e7356d991d5224a1c1223d
Roman <Romazho123@gmail.com>
2019-10-20 11:46:59 -0400

    RZ : T Make the test "it should be created" pass

--- client/src/app/services/tools/selection-tool/selection-tool.service.spec.ts
+++ client/src/app/services/tools/selection-tool/selection-tool.service.spec.ts
@@ -1,12 +1,54 @@
-import { TestBed } from '@angular/core/testing';
+import { MatDialog } from '@angular/material';
+import { Renderer2, ElementRef } from '@angular/core';
+import { TestBed, getTestBed } from '@angular/core/testing';
 
 import { SelectionToolService } from './selection-tool.service';
 
-describe('SelectionToolService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+fdescribe('SelectionToolService', () => {
+    let injector: TestBed;
+    let service: SelectionToolService;
 
-  it('should be created', () => {
-    const service: SelectionToolService = TestBed.get(SelectionToolService);
-    expect(service).toBeTruthy();
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                SelectionToolService,
+                {
+                    provide: MatDialog,
+                    useValue: {},
+                },
+                {
+                    provide: Renderer2,
+                    useValue: {
+                        createElement: () => null,
+                        setAttribute: () => null,
+                        appendChild: () => null,
+                        removeChild: () => null,
+                    },
+                },
+                {
+                    provide: ElementRef,
+                    useValue: {
+                        nativeElement: {
+                            getBoundingClientRect: () => {
+                                const boundleft = 0;
+                                const boundtop = 0;
+                                const boundRect = {
+                                    left: boundleft,
+                                    top: boundtop,
+                                };
+                                return boundRect;
+                            },
+                        },
+                    },
+                },
+            ],
+        });
+
+        injector = getTestBed();
+        service = injector.get(SelectionToolService);
+
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
