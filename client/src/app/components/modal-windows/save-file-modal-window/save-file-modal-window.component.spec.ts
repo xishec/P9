2e2c9e8ed6350c2270e8ce3e70b9a0dfe2b4827b
Louis Popovic <louispop@gmail.com>
2019-10-21 16:55:17 -0400

    LP : D itShouldCreate saveFileModal component

--- client/src/app/components/modal-windows/save-file-modal-window/save-file-modal-window.component.spec.ts
+++ client/src/app/components/modal-windows/save-file-modal-window/save-file-modal-window.component.spec.ts
@@ -1,25 +1,63 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SaveFileModalWindowComponent } from './save-file-modal-window.component';
+import { FormBuilder, FormsModule } from '@angular/forms';
+import { MatDialogRef } from '@angular/material';
+import { NO_ERRORS_SCHEMA } from '@angular/compiler/src/core';
+import { ModalManagerService } from 'src/app/services/modal-manager/modal-manager.service';
+import { DrawingSaverService } from 'src/app/services/server/drawing-saver/drawing-saver.service';
+import { DrawingLoaderService } from 'src/app/services/server/drawing-loader/drawing-loader.service';
 
-describe('SaveFileModalWindowComponent', () => {
-  let component: SaveFileModalWindowComponent;
-  let fixture: ComponentFixture<SaveFileModalWindowComponent>;
+fdescribe('SaveFileModalWindowComponent', () => {
+    let component: SaveFileModalWindowComponent;
+    let fixture: ComponentFixture<SaveFileModalWindowComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SaveFileModalWindowComponent ]
-    })
-    .compileComponents();
-  }));
+    const dialogMock = {
+        close: () => null,
+    };
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SaveFileModalWindowComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+        declarations: [ SaveFileModalWindowComponent ],
+        providers: [
+            FormBuilder,
+            {
+                provide: MatDialogRef,
+                useValue: {},
+            },
+        ],
+        schemas: [NO_ERRORS_SCHEMA],
+        }).overrideComponent(SaveFileModalWindowComponent, {
+            set: {
+                providers: [
+                    FormBuilder,
+                    FormsModule,
+                    {
+                        provide: MatDialogRef,
+                        useValue: dialogMock,
+                    },
+                    {
+                        provide: ModalManagerService,
+                        useValue: {},
+                    },
+                    {
+                        provide: DrawingSaverService,
+                        useValue: {},
+                    },
+                    {
+                        provide: DrawingLoaderService,
+                        useValue: {},
+                    }
+                ]
+            }
+        })
+        .compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture = TestBed.createComponent(SaveFileModalWindowComponent);
+        component = fixture.componentInstance;
+    }));
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
