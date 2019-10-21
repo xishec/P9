a7827aab9c4a48c7e5bd7d49706ad46f2529c824
Louis Popovic <louispop@gmail.com>
2019-10-21 16:55:00 -0400

    LP : D ItShouldCreate open-file-modal component

--- client/src/app/components/modal-windows/open-file-modal-window/open-file-modal-window.component.spec.ts
+++ client/src/app/components/modal-windows/open-file-modal-window/open-file-modal-window.component.spec.ts
@@ -1,25 +1,107 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OpenFileModalWindowComponent } from './open-file-modal-window.component';
+import { FormBuilder, FormsModule } from '@angular/forms';
+import { MatDialogRef } from '@angular/material';
+import { NO_ERRORS_SCHEMA, Pipe, PipeTransform } from '@angular/core';
+import { ModalManagerService } from 'src/app/services/modal-manager/modal-manager.service';
+import { DrawingSaverService } from 'src/app/services/server/drawing-saver/drawing-saver.service';
+import { DrawingLoaderService } from 'src/app/services/server/drawing-loader/drawing-loader.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('OpenFileModalWindowComponent', () => {
-  let component: OpenFileModalWindowComponent;
-  let fixture: ComponentFixture<OpenFileModalWindowComponent>;
+    let component: OpenFileModalWindowComponent;
+    let fixture: ComponentFixture<OpenFileModalWindowComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ OpenFileModalWindowComponent ]
-    })
-    .compileComponents();
-  }));
+    const dialogMock = {
+        close: () => null,
+    };
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(OpenFileModalWindowComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    @Pipe({name: 'mySlice'})
+    class MockMySclicePipe implements PipeTransform {
+        transform(value: number): number {
+            //
+            return value;
+        }
+    }
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    @Pipe({name: 'labelFilter'})
+    class MockLabelFilterPipe implements PipeTransform {
+        transform(value: number): number {
+            //
+            return value;
+        }
+    }
+
+    @Pipe({name: 'nameFilter'})
+    class MockNameFilterPipe implements PipeTransform {
+        transform(value: number): number {
+            //
+            return value;
+        }
+    }
+
+    @Pipe({name: 'toTrustHtml'})
+    class MockToTrustHtmlPipe implements PipeTransform {
+        transform(value: number): number {
+            //
+            return value;
+        }
+    }
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+        declarations: [ 
+            OpenFileModalWindowComponent, 
+            MockMySclicePipe, 
+            MockLabelFilterPipe, 
+            MockNameFilterPipe,
+            MockToTrustHtmlPipe,
+        ],
+        imports: [HttpClientModule],
+        providers: [
+            FormBuilder,
+            {
+                provide: MatDialogRef,
+                useValue: {},
+            },
+        ],
+        schemas: [NO_ERRORS_SCHEMA],
+        }).overrideComponent(OpenFileModalWindowComponent, {
+            set: {
+                providers: [
+                    FormBuilder,
+                    FormsModule,
+                    {
+                        provide: MatDialogRef,
+                        useValue: dialogMock,
+                    },
+                    {
+                        provide: ModalManagerService,
+                        useValue: {},
+                    },
+                    {
+                        provide: DrawingSaverService,
+                        useValue: {},
+                    },
+                    {
+                        provide: DrawingLoaderService,
+                        useValue: {},
+                    }
+                ]
+            }
+        })
+        .compileComponents();
+
+        fixture = TestBed.createComponent(OpenFileModalWindowComponent);
+        component = fixture.componentInstance;
+    }));
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
