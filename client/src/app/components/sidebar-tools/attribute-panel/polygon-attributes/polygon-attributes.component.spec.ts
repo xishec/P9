9f32535fdb92a313f8225c8322700a0921bd7ae1
Kevin <kevinscottsciliento@gmail.com>
2019-10-21 22:57:32 -0400

    KC : B Fix Polygon component test

--- client/src/app/components/sidebar-tools/attribute-panel/polygon-attributes/polygon-attributes.component.spec.ts
+++ client/src/app/components/sidebar-tools/attribute-panel/polygon-attributes/polygon-attributes.component.spec.ts
@@ -1,25 +1,36 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PolygonAttributesComponent } from './polygon-attributes.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { MatDialog } from '@angular/material';
 
 describe('PolygonAttributesComponent', () => {
-  let component: PolygonAttributesComponent;
-  let fixture: ComponentFixture<PolygonAttributesComponent>;
+    let component: PolygonAttributesComponent;
+    let fixture: ComponentFixture<PolygonAttributesComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ PolygonAttributesComponent ],
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [PolygonAttributesComponent],
+            schemas: [NO_ERRORS_SCHEMA],
+            providers: [
+                FormBuilder,
+                {
+                    provide: MatDialog,
+                    useValue: {},
+                },
+            ],
+        });
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(PolygonAttributesComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(PolygonAttributesComponent);
+        component = fixture.componentInstance;
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        component.ngOnInit();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
