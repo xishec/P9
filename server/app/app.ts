35eff2970e5631e219cd495c0af759df3eab98eb
Noboru Yoshida <noboruyoshida1997@gmail.com>
2019-10-22 00:12:25 -0400

    NY : T Remove index, date controllers and services and their references

--- server/app/app.ts
+++ server/app/app.ts
@@ -4,89 +4,81 @@ import * as cors from 'cors';
 import * as express from 'express';
 import { inject, injectable } from 'inversify';
 import * as logger from 'morgan';
-import { DateController } from './controllers/date.controller';
 import { FileManagerController } from './controllers/file-manager.controller';
-import { IndexController } from './controllers/index.controller';
 import Types from './types';
 
 const mongoose = require('mongoose');
 
 @injectable()
 export class Application {
-	private readonly internalError: number = 500;
-	app: express.Application;
+    private readonly internalError: number = 500;
+    app: express.Application;
 
-	constructor(
-		@inject(Types.IndexController) private indexController: IndexController,
-		@inject(Types.DateController) private dateController: DateController,
-		@inject(Types.FileManagerController) private fileManagerController: FileManagerController,
-	) {
-		this.app = express();
+    constructor(@inject(Types.FileManagerController) private fileManagerController: FileManagerController) {
+        this.app = express();
 
-		this.config();
+        this.config();
 
-		this.bindRoutes();
-	}
+        this.bindRoutes();
+    }
 
-	private config(): void {
-		// Middlewares configuration
-		this.app.use(logger('dev'));
-		this.app.use(bodyParser.json());
-		this.app.use(bodyParser.urlencoded({ extended: true }));
-		this.app.use(cookieParser());
-		this.app.use(cors());
-	}
+    private config(): void {
+        // Middlewares configuration
+        this.app.use(logger('dev'));
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: true }));
+        this.app.use(cookieParser());
+        this.app.use(cors());
+    }
 
-	bindRoutes(): void {
-		// Notre application utilise le routeur de notre API `Index`
-		this.app.use('/api/file-manager', this.fileManagerController.router);
-		this.app.use('/api/index', this.indexController.router);
-		this.app.use('/api/date', this.dateController.router);
-		this.errorHandling();
+    bindRoutes(): void {
+        // Notre application utilise le routeur de notre API `Index`
+        this.app.use('/api/file-manager', this.fileManagerController.router);
+        this.errorHandling();
 
-		mongoose
-			.connect('mongodb+srv://P9_client:p9123@p9-mgkks.gcp.mongodb.net/test?retryWrites=true&w=majority', {
-				useUnifiedTopology: true,
-				useNewUrlParser: true,
-				useFindAndModify: false,
-			})
-			.then(() => {
-				console.log('Connected to BD!');
-			})
-			.catch((err: Error) => {
-				console.log(err, 'fuck');
-			});
-	}
+        mongoose
+            .connect('mongodb+srv://P9_client:p9123@p9-mgkks.gcp.mongodb.net/test?retryWrites=true&w=majority', {
+                useUnifiedTopology: true,
+                useNewUrlParser: true,
+                useFindAndModify: false,
+            })
+            .then(() => {
+                console.log('Connected to BD!');
+            })
+            .catch((err: Error) => {
+                console.log(err, 'fuck');
+            });
+    }
 
-	private errorHandling(): void {
-		// When previous handlers have not served a request: path wasn't found
-		this.app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
-			const err: Error = new Error('Not Found');
-			next(err);
-		});
+    private errorHandling(): void {
+        // When previous handlers have not served a request: path wasn't found
+        this.app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+            const err: Error = new Error('Not Found');
+            next(err);
+        });
 
-		// development error handler
-		// will print stacktrace
-		if (this.app.get('env') === 'development') {
-			// tslint:disable-next-line:no-any
-			this.app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
-				res.status(err.status || this.internalError);
-				res.send({
-					message: err.message,
-					error: err,
-				});
-			});
-		}
+        // development error handler
+        // will print stacktrace
+        if (this.app.get('env') === 'development') {
+            // tslint:disable-next-line:no-any
+            this.app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+                res.status(err.status || this.internalError);
+                res.send({
+                    message: err.message,
+                    error: err,
+                });
+            });
+        }
 
-		// production error handler
-		// no stacktraces leaked to user (in production env only)
-		// tslint:disable-next-line:no-any
-		this.app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
-			res.status(err.status || this.internalError);
-			res.send({
-				message: err.message,
-				error: {},
-			});
-		});
-	}
+        // production error handler
+        // no stacktraces leaked to user (in production env only)
+        // tslint:disable-next-line:no-any
+        this.app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+            res.status(err.status || this.internalError);
+            res.send({
+                message: err.message,
+                error: {},
+            });
+        });
+    }
 }
--- server/app/types.ts
+++ server/app/types.ts
@@ -2,8 +2,4 @@ export default {
     Server: Symbol("Server"),
     Application: Symbol("Application"),
     FileManagerController: Symbol("FileManagerController"),
-    IndexController: Symbol("IndexController"),
-    DateController: Symbol("DateController"),
-    IndexService: Symbol("IndexService"),
-    DateService: Symbol("DateService"),
 };
