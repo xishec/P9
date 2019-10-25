89525ed315bc631936ef204763a6fa3b82908459
Louis Popovic <louis.popovic@polymtl.ca>
2019-10-25 15:45:07 -0400

    LP : D Add logic to function saveState and constructor

--- client/src/app/services/undo-redoer/undo-redoer.service.ts
+++ client/src/app/services/undo-redoer/undo-redoer.service.ts
@@ -1,25 +1,52 @@
-import { Injectable } from '@angular/core';
+import { ElementRef, Injectable } from '@angular/core';
+
+import { Drawing } from '../../../../../common/communication/Drawing';
+import { DrawingInfo } from '../../../../../common/communication/DrawingInfo';
+import { DrawingLoaderService } from '../server/drawing-loader/drawing-loader.service';
 
 @Injectable({
     providedIn: 'root';
 })
 export class UndoRedoerService {
 
-    statesArray = new Array<>();
+    statesArray = new Array<Drawing>();
+    currentStateIndexPosition = 0;
+    workzoneRef: ElementRef<SVGElement>;
+    drawingInfo: DrawingInfo;
+
+    constructor(private drawingLoaderService: DrawingLoaderService, workzoneRef: ElementRef<SVGElement>, drawingInfo: DrawingInfo) {
+        this.workzoneRef = workzoneRef;
+        this.drawingInfo = drawingInfo;
+    }
 
     saveCurrentState(): void {
+
+        let currentState: Drawing = {
+            name: '',
+            labels: [],
+            svg: this.workzoneRef.nativeElement.innerHTML,
+            idStack: [],
+            drawingInfo: this.drawingInfo,
+
+        }
         // this should add the current state of the work-zone into the statesArray[];
     }
 
     loadPreviousState(): void {
-        // when pressing control+z the previous state should load (if there is a previous state);
+        // verify if there is a previous state
+        // when pressing control+z the previous state should load;
+        const drawingToLoad = this.statesArray[this.currentStateIndexPosition - 1];
+        this.drawingLoaderService.currentDrawing.next(drawingToLoad);
         // current state should be updated
     }
 
     loadNextState(): void {
-        // when pressing control-y the next state should load (if there is a next state);
+        // verify if there is a next state
+        // when pressing control-y the next state should load;
         // current state should be updated
     }
 
-    constructor() { };
+    reset(): void {
+
+    }
 }
