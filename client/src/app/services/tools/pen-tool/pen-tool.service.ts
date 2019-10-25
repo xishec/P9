98aa5ea35d381e65d29c8051d045f886dad6693b
Xi Chen <xishec@gmail.com>
2019-10-25 14:18:37 -0400

    XS : D Add dummy pen tool

--- client/src/app/services/tools/pen-tool/pen-tool.service.ts
+++ client/src/app/services/tools/pen-tool/pen-tool.service.ts
@@ -7,6 +7,14 @@ import { TracingToolService } from '../abstract-tools/tracing-tool/tracing-tool.
     providedIn: 'root',
 })
 export class PenToolService extends TracingToolService {
+    oldTimeStamp: number = -1;
+    lastMouseX: number;
+    lastMouseY: number;
+    speedX: number;
+    oldSpeedX: number = 0;
+    oldSpeedY: number = 0;
+    speedY: number;
+
     constructor(elementRef: ElementRef<SVGElement>, renderer: Renderer2, drawStack: DrawStackService) {
         super(elementRef, renderer, drawStack);
     }
@@ -18,5 +26,38 @@ export class PenToolService extends TracingToolService {
 
     createSVGPath(): void {
         super.createSVGPath();
+    }
+
+    onMouseMove(e: MouseEvent): void {
+        super.onMouseMove(e);
+
+        if (this.oldTimeStamp === -1) {
+            this.speedX = 0;
+            this.speedY = 0;
+            this.oldTimeStamp = Date.now();
+            this.lastMouseX = e.screenX;
+            this.lastMouseY = e.screenY;
+            return;
+        }
+
+        let now = Date.now();
+        let dt = now - this.oldTimeStamp;
+        let dx = e.screenX - this.lastMouseX;
+        let dy = e.screenY - this.lastMouseY;
+
+        this.speedX = Math.abs(Math.round((dx / dt) * 100));
+        this.speedY = Math.abs(Math.round((dy / dt) * 100));
+        this.oldTimeStamp = now;
+        this.lastMouseX = e.screenX;
+        this.lastMouseY = e.screenY;
+
+        let targetWidth = 12 * ((this.speedX + this.speedY) / 600) + 1;
+        this.currentWidth += (targetWidth - this.currentWidth) / 8;
+        if (this.speedX != this.oldSpeedX || this.speedY != this.oldSpeedY) {
+            super.onMouseDown(e);
+        }
+        this.oldSpeedX = this.speedX;
+        this.oldSpeedY = this.speedY;
+        return;
     }
 }
