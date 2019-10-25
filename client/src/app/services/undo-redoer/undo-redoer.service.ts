24f89949f8283b800d5ee58dbde3f8a44322291e
Louis Popovic <louis.popovic@polymtl.ca>
2019-10-25 14:39:05 -0400

    LP : D Add structure of service

--- client/src/app/services/undo-redoer/undo-redoer.service.ts
+++ client/src/app/services/undo-redoer/undo-redoer.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root';
 })
 export class UndoRedoerService {
 
-  constructor() { }
+    statesArray = new Array<>();
+
+    saveCurrentState(): void {
+        // this should add the current state of the work-zone into the statesArray[];
+    }
+
+    loadPreviousState(): void {
+        // when pressing control+z the previous state should load (if there is a previous state);
+        // current state should be updated
+    }
+
+    loadNextState(): void {
+        // when pressing control-y the next state should load (if there is a next state);
+        // current state should be updated
+    }
+
+    constructor() { };
 }
