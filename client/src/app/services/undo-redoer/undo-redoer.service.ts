9b7727cb0ec54402b8aac17d98df48c800f4731e
Louis Popovic <louispop@gmail.com>
2019-10-27 17:38:17 -0400

    LP : R Change index usage for the currentStateIndex

--- client/src/app/services/undo-redoer/undo-redoer.service.ts
+++ client/src/app/services/undo-redoer/undo-redoer.service.ts
@@ -14,12 +14,17 @@ export class UndoRedoerService {
     currentStateIndexPosition = 0;
     workzoneRef: ElementRef<SVGElement>;
 
-    constructor(private drawingLoaderService: DrawingLoaderService, workzoneRef: ElementRef<SVGElement>) {
+    constructor(private drawingLoaderService: DrawingLoaderService) {
+    }
+
+    setWorkZoneRef(workzoneRef: ElementRef<SVGElement>) {
         this.workzoneRef = workzoneRef;
     }
 
-    initializeStateArray() : void {
-
+    initializeStateArray(idStackArray: string[]) : void {
+        this.statesArray = new Array<Drawing>();
+        this.currentStateIndexPosition = 0;
+        this.saveCurrentState(idStackArray);
     }
 
     saveCurrentState(idStackArray: string[]): void {
@@ -27,12 +32,18 @@ export class UndoRedoerService {
             name: '',
             labels: [],
             svg: this.workzoneRef.nativeElement.innerHTML,
-            idStack: idStackArray,
+            idStack:  idStackArray,
             drawingInfo: new DrawingInfo(700, 700, DEFAULT_WHITE),
         };
+
+        // verify
+        if (this.currentStateIndexPosition < this.statesArray.length - 2) {
+            this.statesArray = this.statesArray.slice(0, this.currentStateIndexPosition + 1);
+        }
         this.statesArray.push(currentState);
-        this.currentStateIndexPosition++;
+        this.currentStateIndexPosition = this.statesArray.length - 1;
         // this should add the current state of the work-zone into the statesArray[];
+        console.log('save : ' + currentState.idStack);
     }
 
     loadPreviousState(): void {
@@ -42,6 +53,7 @@ export class UndoRedoerService {
             const drawingToLoad = this.statesArray[--this.currentStateIndexPosition];
             this.drawingLoaderService.currentDrawing.next(drawingToLoad);
             // current state should be updated
+            console.log('previous : ' + drawingToLoad.idStack);
         }
     }
 
@@ -51,6 +63,7 @@ export class UndoRedoerService {
             // when pressing control-y the next state should load;
             const drawingToLoad = this.statesArray[++this.currentStateIndexPosition];
             this.drawingLoaderService.currentDrawing.next(drawingToLoad);
+            console.log('next ' + drawingToLoad.idStack);
         }
     }
 
