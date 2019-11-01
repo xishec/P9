c3870c2c823ac9194df3922b8ac9ce5176a20fa8
xishec <xishe@gmail.com>
2019-11-01 15:49:54 -0400

    XS : D Add  tracing-tool button group

--- client/src/app/components/sidebar-tools/sidebar-tools.component.ts
+++ client/src/app/components/sidebar-tools/sidebar-tools.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 
-import { FILES_BUTTON_INFO, ToolName, TOOLS_BUTTON_INFO } from 'src/constants/tool-constants';
+import { FILES_BUTTON_INFO, ToolName, TOOLS_BUTTON_INFO, TRACING_BUTTON_INFO } from 'src/constants/tool-constants';
 import { ToolSelectorService } from '../../services/tools/tool-selector/tool-selector.service';
 
 @Component({
@@ -10,9 +10,12 @@ import { ToolSelectorService } from '../../services/tools/tool-selector/tool-sel
 })
 export class SidebarToolsComponent implements OnInit, AfterViewInit {
     readonly TOOLS_BUTTON_INFO = TOOLS_BUTTON_INFO;
+    readonly TRACING_BUTTON_INFO = TRACING_BUTTON_INFO;
     readonly FILES_BUTTON_INFO = FILES_BUTTON_INFO;
 
     currentToolName: ToolName;
+    currentTracingTool: ToolName;
+    showTracingTools = false;
 
     constructor(private toolSelectorService: ToolSelectorService) {}
 
@@ -20,13 +23,63 @@ export class SidebarToolsComponent implements OnInit, AfterViewInit {
         this.toolSelectorService.currentToolName.subscribe((currentToolName) => {
             this.currentToolName = currentToolName;
         });
+        this.currentTracingTool = ToolName.Pencil;
     }
 
     ngAfterViewInit(): void {
         this.toolSelectorService.changeTool(ToolName.Selection);
     }
 
-    onChangeTool(tooltipName: ToolName): void {
+    onChangeTool(i: number): void {
+        this.showTracingTools = false;
+        let tooltipName: ToolName;
+        if (i === 1) {
+            tooltipName = this.currentTracingTool;
+        } else {
+            tooltipName = this.TOOLS_BUTTON_INFO[i].tooltipName as ToolName;
+        }
+        console.log(tooltipName);
         this.toolSelectorService.changeTool(tooltipName);
+    }
+    onChangeTracingTool(tooltipName: ToolName): void {
+        this.showTracingTools = false;
+        this.currentTracingTool = tooltipName;
+        this.currentToolName = ToolName.TracingTool;
+        this.toolSelectorService.changeTool(tooltipName);
+    }
+
+    onRightClick() {
+        this.showTracingTools = true;
+    }
+
+    getChecked(i: number): boolean {
+        let tooltipName: ToolName = this.TOOLS_BUTTON_INFO[i].tooltipName as ToolName;
+        if (i === 1) {
+            return this.currentToolName === this.currentTracingTool;
+        } else {
+            return this.currentToolName === tooltipName;
+        }
+    }
+
+    getTracingToolClass(): string {
+        switch (this.currentTracingTool) {
+            case ToolName.Pencil:
+                return 'fas fa-pencil-alt';
+
+            case ToolName.Brush:
+                return 'fas fa-paint-brush';
+
+            case ToolName.Pen:
+                return 'fas fa-pen-nib';
+
+            case ToolName.Quill:
+                return 'fas fa-pen-alt';
+
+            case ToolName.SprayCan:
+                return 'fas fa-spray-can';
+
+            default:
+                return 'fas fa-pencil-alt';
+        }
     }
 }
