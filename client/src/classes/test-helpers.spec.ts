5d5c957d20efe741b87135fc6c60dfec4e26e161
Roman <Romazho123@gmail.com>
2019-11-03 22:14:21 -0500

    RZ : add getBoundingClientRect method to createMockSVGGElementWithAttribute

--- client/src/classes/test-helpers.spec.ts
+++ client/src/classes/test-helpers.spec.ts
@@ -60,12 +60,16 @@ export const createMockSVGGElement = (): any => {
 export const createMockSVGGElementWithAttribute = (att: string): any => {
     const attribute = att;
     const mockSVGElement = {
-        getAttribute : (attToGet: string) => {
+        getAttribute: (attToGet: string) => {
             if (attToGet === attribute) {
                 return '10';
             } else {
                 return false;
             }
+        },
+        getBoundingClientRect: () => {
+            const mockDOMRect = { x: 500, y: 500, width: 50, height: 50 };
+            return (mockDOMRect as unknown) as DOMRect;
         },
     };
     return (mockSVGElement as unknown) as SVGGElement;
