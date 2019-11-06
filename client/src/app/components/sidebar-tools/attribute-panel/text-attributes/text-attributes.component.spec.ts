2988bc042c374e5987fcd10eb19b8a64246b887e
Roman <Romazho123@gmail.com>
2019-11-05 23:13:44 -0500

    RZ : T Make the "it should create" test pass for the component

--- client/src/app/components/sidebar-tools/attribute-panel/text-attributes/text-attributes.component.spec.ts
+++ client/src/app/components/sidebar-tools/attribute-panel/text-attributes/text-attributes.component.spec.ts
@@ -1,25 +1,49 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { MatDialog } from '@angular/material';
 
 import { TextAttributesComponent } from './text-attributes.component';
+import { ShortcutManagerService } from 'src/app/services/shortcut-manager/shortcut-manager.service';
+import { AttributesManagerService } from 'src/app/services/tools/attributes-manager/attributes-manager.service';
 
-describe('TextAttributesComponent', () => {
-  let component: TextAttributesComponent;
-  let fixture: ComponentFixture<TextAttributesComponent>;
+fdescribe('TextAttributesComponent', () => {
+    let component: TextAttributesComponent;
+    let fixture: ComponentFixture<TextAttributesComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TextAttributesComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [TextAttributesComponent],
+            schemas: [NO_ERRORS_SCHEMA],
+            providers: [
+                FormBuilder,
+                {
+                    provide: MatDialog,
+                    useValue: {},
+                },
+            ],
+        })
+            .overrideComponent(TextAttributesComponent, {
+                set: {
+                    providers: [
+                        {
+                            provide: AttributesManagerService,
+                            useValue: {},
+                        },
+                        {
+                            provide: ShortcutManagerService,
+                            useValue: {},
+                        },
+                    ],
+                },
+            })
+            .compileComponents();
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TextAttributesComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(TextAttributesComponent);
+        component = fixture.componentInstance;
+    }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
