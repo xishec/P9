093d431bbbe733b5c8c156430267f6e9f10cfaec
HPay <hakimpayman@gmail.com>
2019-11-06 00:48:21 -0500

    HP : D Ugly clipboard buttons added to attribute panel

--- client/src/app/components/sidebar-tools/attribute-panel/selection-attributes/selection-attributes.component.ts
+++ client/src/app/components/sidebar-tools/attribute-panel/selection-attributes/selection-attributes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { ToolName } from 'src/constants/tool-constants';
+import { ToolName, CLIPBOARD_BUTTON_INFO } from 'src/constants/tool-constants';
 
 @Component({
     selector: 'app-selection-attributes',
@@ -8,5 +8,12 @@ import { ToolName } from 'src/constants/tool-constants';
     styleUrls: ['./selection-attributes.component.scss'],
 })
 export class SelectionAttributesComponent {
+    readonly CLIPBOARD_BUTTONS = CLIPBOARD_BUTTON_INFO;
     toolName = ToolName.Selection;
+
+
+
+    onButtonTrigger(): void {
+
+    }
 }
