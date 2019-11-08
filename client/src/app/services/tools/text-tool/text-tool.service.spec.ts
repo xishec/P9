9a753b4ee5ef20bc21239d82a72653e904388385
Roman <Romazho123@gmail.com>
2019-11-08 12:40:41 -0500

    RZ : T Make the test "it should be created" pass

--- client/src/app/services/tools/text-tool/text-tool.service.spec.ts
+++ client/src/app/services/tools/text-tool/text-tool.service.spec.ts
@@ -1,9 +1,92 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, getTestBed } from '@angular/core/testing';
+import { Renderer2, ElementRef, Type } from '@angular/core';
 
 import { TextToolService } from './text-tool.service';
+import { DrawStackService } from '../../draw-stack/draw-stack.service';
 
-describe('TextToolService', () => {
-    beforeEach(() => TestBed.configureTestingModule({}));
+fdescribe('TextToolService', () => {
+    let injector: TestBed;
+    let service: TextToolService;
+    // let leftMouseEvent: MouseEvent;
+    // let rightMouseEvent: MouseEvent;
+    // let keyboardEvent: KeyboardEvent
+
+    //let spyOnremoveChild: jasmine.Spy;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                TextToolService,
+                {
+                    provide: Renderer2,
+                    useValue: {
+                        createElement: () => null,
+                        setAttribute: () => null,
+                        appendChild: () => null,
+                        removeChild: () => null,
+                        setProperty: () => null,
+                    },
+                },
+                {
+                    provide: DrawStackService,
+                    useValue: {
+                        push: () => null,
+                        currentStackTarget: {
+                            subscribe: () => null,
+                        },
+                        getElementByPosition: () => {
+                            const mockSVGGElement = {
+                                getAttribute: () => null,
+                            };
+                            return (mockSVGGElement as unknown) as SVGGElement;
+                        },
+                        removeElementByPosition: () => null,
+                        getDrawStackLength: () => 1,
+                        drawStack: () => {
+                            const mockdrawStack = {
+                                getAttribute: () => null,
+                            };
+                            return (mockdrawStack as unknown) as SVGGElement[];
+                        },
+                        renderer: () => {
+                            const mockrenderer = {};
+                            return (mockrenderer as unknown) as Renderer2;
+                        },
+                        changeTargetElement: () => null,
+                        delete: () => null,
+                    },
+                },
+                {
+                    provide: ElementRef,
+                    useValue: {
+                        nativeElement: {
+                            getBoundingClientRect: () => {
+                                const boundleft = 0;
+                                const boundtop = 0;
+                                const boundRect = {
+                                    left: boundleft,
+                                    top: boundtop,
+                                };
+                                return boundRect;
+                            },
+                        },
+                    },
+                },
+            ],
+        });
+
+        injector = getTestBed();
+        service = injector.get(TextToolService);
+        const rendererMock = injector.get<Renderer2>(Renderer2 as Type<Renderer2>);
+        const drawStackMock = injector.get<DrawStackService>(DrawStackService as Type<DrawStackService>);
+        const elementRefMock = injector.get<ElementRef>(ElementRef as Type<ElementRef>);
+        service.initializeService(elementRefMock, rendererMock, drawStackMock);
+
+        // leftMouseEvent = createMouseEvent(10, 10, 0);
+        //rightMouseEvent = createMouseEvent(10, 10, 2);
+
+        //spyOnremoveChild = spyOn(service.renderer, 'removeChild').and.returnValue();
+    });
 
     it('should be created', () => {
         const service: TextToolService = TestBed.get(TextToolService);
