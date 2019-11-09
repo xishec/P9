a2e53da65b78dce7f02eeb82119465f0df903fb1
Kevin <kevinscottsciliento@gmail.com>
2019-11-09 11:55:04 -0500

    KC : R Remove multiples if else for switch case

--- client/src/app/services/tools/text-tool/text-tool.service.ts
+++ client/src/app/services/tools/text-tool/text-tool.service.ts
@@ -299,26 +299,29 @@ export class TextToolService extends AbstractToolService {
         event.preventDefault();
         this.currentCursorIndex = this.text.indexOf(TEXT_CURSOR);
 
-        if (event.key === Keys.Enter) {
-            this.createNewLine();
-        } else if (event.key === Keys.Backspace) {
-            this.erase();
-        } else if (event.key === Keys.Space) {
-            this.addText(TEXT_SPACE);
-        } else if (event.key === Keys.ArrowLeft || event.key === Keys.ArrowRight) {
-            this.moveCursor(event.key);
-        } else if (event.key === Keys.SmallerThan || event.key === Keys.GreaterThan) {
-            this.snackBar.open(
-                `Les carractères ${Keys.SmallerThan} et ${Keys.GreaterThan} ne sont malheureusement pas disponible`,
-                '',
-                {
+        switch (event.key) {
+            case Keys.Enter:
+                this.createNewLine();
+                break;
+            case Keys.Backspace:
+                this.erase();
+                break;
+
+            case Keys.Space:
+                this.addText(TEXT_SPACE);
+                break;
+            case Keys.ArrowLeft:
+            case Keys.ArrowRight:
+                this.moveCursor(event.key);
+                break;
+            case Keys.SmallerThan:
+                this.snackBar.open(`Le carractères ${Keys.SmallerThan}  n'est malheureusement pas disponible`, '', {
                     duration: SNACKBAR_DURATION,
-                }
-            );
-        } else {
-            if (event.key.length < 2) {
+                });
+                break;
+            default:
                 this.addText(event.key);
-            }
+                break;
         }
         this.renderer.setProperty(this.currentLine, 'innerHTML', this.text);
         setTimeout(() => {
@@ -396,6 +399,4 @@ export class TextToolService extends AbstractToolService {
             return el === this.currentLine;
         });
     }
-
-    openSnackBar() {}
 }
