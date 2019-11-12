dddb9dbdf40ee1e3161ef2cd91c3ad65f823a8f6
HPay <hakimpayman@gmail.com>
2019-11-12 12:24:26 -0500

    HP : T Provide Renderer2 and ElementRef

--- client/src/app/services/server/drawing-saver/drawing-saver.service.spec.ts
+++ client/src/app/services/server/drawing-saver/drawing-saver.service.spec.ts
@@ -1,23 +1,42 @@
 import { TestBed, getTestBed } from '@angular/core/testing';
 import { DomSanitizer } from '@angular/platform-browser';
-import { ElementRef, Type } from '@angular/core';
+import { ElementRef, Type, Renderer2} from '@angular/core';
 
 import { DrawingModalWindowService } from '../../drawing-modal-window/drawing-modal-window.service';
 import { DrawingLoaderService } from '../drawing-loader/drawing-loader.service';
 import { FileManagerService } from '../file-manager/file-manager.service';
 import { DrawingSaverService } from './drawing-saver.service';
-// import { createMockSVGElement } from 'src/classes/test-helpers.spec';
+//import { createMockSVGElement } from 'src/classes/test-helpers.spec';
 import { DrawStackService } from '../../draw-stack/draw-stack.service';
 
 fdescribe('DrawingSaverService', () => {
     let injector: TestBed;
-    let service: DrawingSaverService;    
+    let service: DrawingSaverService;
     // let rendererMock: Renderer2;
+    let drawStackMock: DrawStackService;
+    let elementRefMock: ElementRef<SVGElement>;
     beforeEach(() => {
         TestBed.configureTestingModule({
             providers: [
                 DrawingSaverService,
                 [DomSanitizer],
+                {
+                    provide: Renderer2,
+                    useValue:
+                    {
+                        createElement: () => null,
+                        setAttribute: () => null,
+                        appendChild: () => null,
+                    },
+                },
+                {
+                    provide: ElementRef,
+                    useValue: {
+                        nativeElement: {
+                            innerHTML: '',
+                        },
+                    },
+                },
                 {
                     provide: DrawingModalWindowService,
                     useValue: {},
@@ -35,11 +54,10 @@ fdescribe('DrawingSaverService', () => {
         service = TestBed.get(DrawingSaverService);
         injector = getTestBed();
         service = injector.get(DrawingSaverService);
-        // rendererMock = injector.get<Renderer2>(Renderer2 as Type<Renderer2>);
-        const drawStackMock = injector.get<DrawStackService>(DrawStackService as Type<DrawStackService>);
-        const elementRefMock = injector.get<ElementRef>(ElementRef as Type<ElementRef>);
+        //rendererMock = injector.get<Renderer2>(Renderer2 as Type<Renderer2>);
+        drawStackMock = injector.get<DrawStackService>(DrawStackService as Type<DrawStackService>);
+        elementRefMock = injector.get<ElementRef>(ElementRef as Type<ElementRef>);
         service.initializeDrawingSaverService(elementRefMock, drawStackMock);
-
     });
 
     it('should be created', () => {
@@ -47,7 +65,7 @@ fdescribe('DrawingSaverService', () => {
     });
 
     it('should set values retrieved from workzone when initializeDrawingSaverService is called ', () => {
-        // const TEST_REF: ElementRef<SVGElement> = createMockSVGElement();
+        //const TEST_REF: ElementRef<SVGElement> = createMockSVGElement() as unknown as ElementRef<SVGElement>;
 
         expect(service).toBeTruthy();
     });
