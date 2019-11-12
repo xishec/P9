c3c34417160d5480484ac94c06b22c1913e9e40f
Kevin <kevinscottsciliento@gmail.com>
2019-11-11 22:30:18 -0500

    KC : T add text for swapcursor

--- client/src/classes/test-helpers.spec.ts
+++ client/src/classes/test-helpers.spec.ts
@@ -2,132 +2,132 @@ import { ElementRef } from '@angular/core';
 import { Keys } from 'src/constants/constants';
 
 export class MockRect {
-    x = 0;
-    y = 0;
-    width = 0;
-    height = 0;
-    // tslint:disable-next-line: no-empty
-    addEventListener(): void {}
+	x = 0;
+	y = 0;
+	width = 0;
+	height = 0;
+	// tslint:disable-next-line: no-empty
+	addEventListener(): void {}
 }
 
 // tslint:disable-next-line: max-classes-per-file
 export class MockPolygon {
-    points = '200,10 250,190 160,210';
+	points = '200,10 250,190 160,210';
 }
 
 export const createMouseEvent = (
-    x: number,
-    y: number,
-    buttonPressed: number,
-    offSetx?: number,
-    offSety?: number,
+	x: number,
+	y: number,
+	buttonPressed: number,
+	offSetx?: number,
+	offSety?: number,
 ): MouseEvent => {
-    const mouseEvent = {
-        clientX: x,
-        clientY: y,
-        button: buttonPressed,
-        offsetX: offSetx,
-        offsetY: offSety,
-    };
-    return (mouseEvent as unknown) as MouseEvent;
+	const mouseEvent = {
+		clientX: x,
+		clientY: y,
+		button: buttonPressed,
+		offsetX: offSetx,
+		offsetY: offSety,
+	};
+	return (mouseEvent as unknown) as MouseEvent;
 };
 
 export const createKeyBoardEvent = (keyPressed: Keys): KeyboardEvent => {
-    const keyboardEvent = {
-        key: keyPressed,
-        preventDefault: () => null,
-    };
-    return (keyboardEvent as unknown) as KeyboardEvent;
+	const keyboardEvent = {
+		key: keyPressed,
+		preventDefault: () => null,
+	};
+	return (keyboardEvent as unknown) as KeyboardEvent;
 };
 
 export const createMockSVGCircle = (): any => {
-    const mockCircle = {
-        addEventListener: () => null,
-    };
-    return mockCircle;
+	const mockCircle = {
+		addEventListener: () => null,
+	};
+	return mockCircle;
 };
 
 export const createMockSVGLine = (): any => {
-    const mockLine = {};
-    return (mockLine as unknown) as SVGLineElement;
+	const mockLine = {};
+	return (mockLine as unknown) as SVGLineElement;
 };
 
 export const createMockSVGGElement = (): any => {
-    const mockSVGElement = {};
-    return (mockSVGElement as unknown) as SVGGElement;
+	const mockSVGElement = {};
+	return (mockSVGElement as unknown) as SVGGElement;
 };
 
 export const createMockSVGElement = (): any => {
-    const mockSVGElement = {
-        nativeElement: {
-            getBoundingClientRect: () => {
-                const boundleft = 0;
-                const boundtop = 0;
-                const boundRect = {
-                    left: boundleft,
-                    top: boundtop,
-                    width: boundleft * 2,
-                    height: boundtop * 2,
-                };
-                return boundRect;
-            },
-        },
-    };
-    return (mockSVGElement as unknown) as ElementRef<SVGElement>;
+	const mockSVGElement = {
+		nativeElement: {
+			getBoundingClientRect: () => {
+				const boundleft = 0;
+				const boundtop = 0;
+				const boundRect = {
+					left: boundleft,
+					top: boundtop,
+					width: boundleft * 2,
+					height: boundtop * 2,
+				};
+				return boundRect;
+			},
+		},
+	};
+	return (mockSVGElement as unknown) as ElementRef<SVGElement>;
 };
 
 export const createMockCanvasElement = (): any => {
-    const mockCanvasElement = {};
-    return (mockCanvasElement as unknown) as HTMLCanvasElement;
+	const mockCanvasElement = {};
+	return (mockCanvasElement as unknown) as HTMLCanvasElement;
 };
 
 export const createMockSVGGElementWithAttribute = (att: string): any => {
-    const attribute = att;
-    const mockSVGElement = {
-        getAttribute: (attToGet: string) => {
-            return attToGet === attribute ? '10' : false;
-        },
-        getBoundingClientRect: () => {
-            const mockDOMRect = { x: 500, y: 500, width: 50, height: 50 };
-            return (mockDOMRect as unknown) as DOMRect;
-        },
-        childElementCount: 3,
-        childNodes: () => {
-            const mockGelementArray: SVGGElement[] = [
-                createMockSVGGElement(),
-                createMockSVGGElement(),
-                createMockSVGGElement(),
-            ];
-            return mockGelementArray;
-        },
-    };
-    return (mockSVGElement as unknown) as SVGGElement;
+	const attribute = att;
+	const mockSVGElement = {
+		getAttribute: (attToGet: string) => {
+			return attToGet === attribute ? '10' : false;
+		},
+		getBoundingClientRect: () => {
+			const mockDOMRect = { x: 500, y: 500, width: 50, height: 50 };
+			return (mockDOMRect as unknown) as DOMRect;
+		},
+		childElementCount: 3,
+		childNodes: () => {
+			const mockGelementArray: SVGGElement[] = [
+				createMockSVGGElement(),
+				createMockSVGGElement(),
+				createMockSVGGElement(),
+			];
+			return mockGelementArray;
+		},
+	};
+	return (mockSVGElement as unknown) as SVGGElement;
 };
 
 export const createMockFilter = (): SVGFilterElement => {
-    const mockFilter = {};
-    return (mockFilter as unknown) as SVGFilterElement;
+	const mockFilter = {};
+	return (mockFilter as unknown) as SVGFilterElement;
 };
 
 // tslint:disable-next-line: max-classes-per-file
 export class MockElementRef extends ElementRef {
-    constructor() {
-        super(null);
-    }
+	constructor() {
+		super(null);
+	}
 }
 
 export const createMockSVGTextElement = (): SVGTextElement => {
-    const mockSVGTextElement = {
-        childNodes: [createMockSVGElement(), createMockSVGElement()] as SVGTSpanElement[],
-        getBBox: () => {
-            const mockDOMRect = { x: 500, y: 500, width: 50, height: 50 };
-            return (mockDOMRect as unknown) as DOMRect;
-        },
-    };
-    return (mockSVGTextElement as unknown) as SVGTextElement;
+	const mockSVGTextElement = {
+		childNodes: [createMockSVGElement(), createMockSVGElement()] as SVGTSpanElement[],
+		getBBox: () => {
+			const mockDOMRect = { x: 500, y: 500, width: 50, height: 50 };
+			return (mockDOMRect as unknown) as DOMRect;
+		},
+	};
+	return (mockSVGTextElement as unknown) as SVGTextElement;
 };
 
 export const createMockSVGTSpanElement = (): SVGTSpanElement => {
-    const mockFilter = {};
-    return (mockFilter as unknown) as SVGTSpanElement;
+	const mockTSpanElement = { textcontent: '' };
+	return (mockTSpanElement as unknown) as SVGTSpanElement;
 };
--- client/src/classes/textStyle/textCursor.spec.ts
+++ client/src/classes/textStyle/textCursor.spec.ts
@@ -1,78 +1,137 @@
-import { Renderer2 } from '@angular/core';
 import { getTestBed, TestBed } from '@angular/core/testing';
 
 import { BehaviorSubject } from 'rxjs';
 
-import { TEXT_CURSOR } from 'src/constants/tool-constants';
+import { Renderer2 } from '@angular/core';
+import { HTMLAttribute, TEXT_CURSOR, TEXT_LINEBREAK } from 'src/constants/tool-constants';
+import { createMockSVGTSpanElement } from '../test-helpers.spec';
 import { TextCursor } from './textCursor';
 
-describe('TextCursor', () => {
-    let injector: TestBed;
-    let service: TextCursor;
-    const TEXT = `te${TEXT_CURSOR}st`;
+fdescribe('TextCursor', () => {
+        let injector: TestBed;
+        let service: TextCursor;
+        let currentLine: SVGTSpanElement;
+        const TEXT = `te${TEXT_CURSOR}st`;
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            providers: [
-                TextCursor,
-                {
-                    provide: Renderer2,
-                    useValue: {
-                        setProperty: () => null,
-                    },
-                },
-                {
-                    provide: BehaviorSubject,
-                    useValue: {
-                        subscribe: () => null,
-                    },
-                },
-            ],
+        beforeEach(() => {
+                TestBed.configureTestingModule({
+                        providers: [
+                                TextCursor,
+                                {
+                                        provide: Renderer2,
+                                        useValue: {
+                                                setProperty: (
+                                                        tspan: SVGTSpanElement,
+                                                        name: HTMLAttribute,
+                                                        text: string,
+                                                ) => {
+                                                        tspan.textContent = text;
+                                                },
+                                        },
+                                },
+
+                                {
+                                        provide: BehaviorSubject,
+                                        useValue: {
+                                                subscribe: () => null,
+                                        },
+                                },
+                        ],
+                });
+
+                injector = getTestBed();
+                service = injector.get(TextCursor);
+                service.currentCursorIndex = 2;
+                currentLine = createMockSVGTSpanElement();
+                currentLine.textContent = '';
         });
 
-        injector = getTestBed();
-        service = injector.get(TextCursor);
-        service.currentCursorIndex = 2;
-    });
+        it('should be created', () => {
+                expect(service).toBeTruthy();
+        });
 
-    it('should be created', () => {
-        expect(service).toBeTruthy();
-    });
+        it(`swapInCurrentLine should return t${TEXT_CURSOR}est if string "${TEXT}" is passed,cursorindex is 2 and offset is -1`, () => {
+                service.text = TEXT;
 
-    it(`swapInCurrentLine should return t${TEXT_CURSOR}est if string "${TEXT}" is passed,cursorindex is 2 and offset is -1`, () => {
-        service.text = TEXT;
+                expect(service.swapInCurrentLine(-1)).toEqual(`t${TEXT_CURSOR}est`);
+        });
 
-        expect(service.swapInCurrentLine(-1)).toEqual(`t${TEXT_CURSOR}est`);
-    });
+        it(`erase should return test if string ${TEXT} is passed and currentCursorIndex is 2`, () => {
+                service.text = TEXT;
 
-    it(`erase should return test if string ${TEXT} is passed and currentCursorIndex is 2`, () => {
-        service.text = TEXT;
+                expect(service.erase()).toEqual('test');
+        });
 
-        expect(service.erase()).toEqual('test');
-    });
+        it(`leftSideText should return "te" if "${TEXT}" is passed and currentCursorIndex is 2`, () => {
+                service.text = TEXT;
 
-    it(`leftSideText should return "te" if "${TEXT}" is passed and currentCursorIndex is 2`, () => {
-        service.text = TEXT;
+                expect(service.leftSideText()).toEqual('te');
+        });
 
-        expect(service.leftSideText()).toEqual('te');
-    });
+        it(`rightSideText should return "st" if "${TEXT}" is passed and currentCursorIndex is 2`, () => {
+                service.text = TEXT;
 
-    it(`rightSideText should return "st" if "${TEXT}" is passed and currentCursorIndex is 2`, () => {
-        service.text = TEXT;
+                expect(service.rightSideText()).toEqual('st');
+        });
 
-        expect(service.rightSideText()).toEqual('st');
-    });
+        it('isAtStartOfLine should return true if text cursor is at start of line', () => {
+                service.text = TEXT_CURSOR + 'test';
+                service.currentCursorIndex = 0;
+                expect(service.isAtStartOfLine()).toBeTruthy();
+        });
 
-    it('isAtStartOfLine should return true if text cursor is at start of line', () => {
-        service.text = TEXT_CURSOR + 'test';
-        service.currentCursorIndex = 0;
-        expect(service.isAtStartOfLine()).toBeTruthy();
-    });
+        it('isAtEndOfLine should return true if text cursor is at end of line', () => {
+                service.text = 'test' + TEXT_CURSOR;
+                service.currentCursorIndex = service.text.indexOf(TEXT_CURSOR);
 
-    it('isAtEndOfLine should return true if text cursor is at end of line', () => {
-        service.text = 'test' + TEXT_CURSOR;
-        service.currentCursorIndex = service.text.indexOf(TEXT_CURSOR);
+                expect(service.isAtEndOfLine()).toBeTruthy();
+        });
 
-        expect(service.isAtEndOfLine()).toBeTruthy();
-    });
+        it('isAtStartOfLine should return false if text cursor is at end of line', () => {
+                service.text = 'test' + TEXT_CURSOR;
+                service.currentCursorIndex = service.text.indexOf(TEXT_CURSOR);
+                expect(service.isAtStartOfLine()).toBeFalsy();
+        });
+
+        it('isAtEndOfLine should return false if text cursor is at start of line', () => {
+                service.text = TEXT_CURSOR + 'test';
+                service.currentCursorIndex = service.text.indexOf(TEXT_CURSOR);
+
+                expect(service.isAtEndOfLine()).toBeFalsy();
+        });
+
+        it('findLinePosition should return 2 if currentLine is at index 1 in tspans', () => {
+                const tspans: SVGTSpanElement[] = new Array<SVGTSpanElement>(createMockSVGTSpanElement(), currentLine);
+                expect(service.findLinePosition(currentLine, tspans)).toEqual(1);
+        });
+
+        it('swapToAnotherLine should return the same text if there previous line doesnt exist', () => {
+                service.text = TEXT;
+                const tspans: SVGTSpanElement[] = new Array<SVGTSpanElement>(currentLine);
+                expect(service.swapToAnotherLine(-1, [currentLine], tspans)).toEqual(TEXT);
+        });
+
+        it('swapToAnotherLine should return the same text if there next line doesnt exist', () => {
+                service.text = TEXT;
+                const tspans: SVGTSpanElement[] = new Array<SVGTSpanElement>(currentLine);
+                expect(service.swapToAnotherLine(1, [currentLine], tspans)).toEqual(TEXT);
+        });
+
+        it('swapToAnotherLine should set text to linebreak if text is empty and should only return cursor if next line is a linebreak', () => {
+                service.text = '';
+                const tspans: SVGTSpanElement[] = new Array<SVGTSpanElement>(currentLine, createMockSVGTSpanElement());
+                tspans[1].textContent = TEXT_LINEBREAK;
+
+                expect(service.swapToAnotherLine(1, [currentLine], tspans)).toEqual(TEXT_CURSOR);
+                expect(tspans[0].textContent).toEqual(TEXT_LINEBREAK);
+        });
+
+        it('swapToAnotherLine should set text to linebreak if text is empty and should only return cursor if previous line is a linebreak', () => {
+                service.text = '';
+                const tspans: SVGTSpanElement[] = new Array<SVGTSpanElement>(createMockSVGTSpanElement(), currentLine);
+                tspans[0].textContent = TEXT_LINEBREAK;
+
+                expect(service.swapToAnotherLine(-1, [currentLine], tspans)).toEqual(TEXT_CURSOR);
+                expect(tspans[1].textContent).toEqual(TEXT_LINEBREAK);
+        });
 });
