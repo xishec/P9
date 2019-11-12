1f7824eaa7e540e90ba626898ac69e5440babaf0
Noboru Yoshida <noboruyoshida1997@gmail.com>
2019-11-12 12:08:44 -0500

    NY : T Drawing saver test fails

--- client/src/app/services/server/drawing-saver/drawing-saver.service.spec.ts
+++ client/src/app/services/server/drawing-saver/drawing-saver.service.spec.ts
@@ -1,14 +1,18 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, getTestBed } from '@angular/core/testing';
 import { DomSanitizer } from '@angular/platform-browser';
+import { ElementRef, Type } from '@angular/core';
 
 import { DrawingModalWindowService } from '../../drawing-modal-window/drawing-modal-window.service';
 import { DrawingLoaderService } from '../drawing-loader/drawing-loader.service';
 import { FileManagerService } from '../file-manager/file-manager.service';
 import { DrawingSaverService } from './drawing-saver.service';
+// import { createMockSVGElement } from 'src/classes/test-helpers.spec';
+import { DrawStackService } from '../../draw-stack/draw-stack.service';
 
-let service: DrawingSaverService;
-
-describe('DrawingSaverService', () => {
+fdescribe('DrawingSaverService', () => {
+    let injector: TestBed;
+    let service: DrawingSaverService;    
+    // let rendererMock: Renderer2;
     beforeEach(() => {
         TestBed.configureTestingModule({
             providers: [
@@ -29,9 +33,22 @@ describe('DrawingSaverService', () => {
             ],
         });
         service = TestBed.get(DrawingSaverService);
+        injector = getTestBed();
+        service = injector.get(DrawingSaverService);
+        // rendererMock = injector.get<Renderer2>(Renderer2 as Type<Renderer2>);
+        const drawStackMock = injector.get<DrawStackService>(DrawStackService as Type<DrawStackService>);
+        const elementRefMock = injector.get<ElementRef>(ElementRef as Type<ElementRef>);
+        service.initializeDrawingSaverService(elementRefMock, drawStackMock);
+
     });
 
     it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should set values retrieved from workzone when initializeDrawingSaverService is called ', () => {
+        // const TEST_REF: ElementRef<SVGElement> = createMockSVGElement();
+
         expect(service).toBeTruthy();
     });
 });
