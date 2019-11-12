2670916af915df5057681a4775501f3f890a4afd
Noboru Yoshida <noboruyoshida1997@gmail.com>
2019-11-12 13:49:24 -0500

    NY : T Add test for drawing server service

--- client/src/app/services/server/drawing-saver/drawing-saver.service.spec.ts
+++ client/src/app/services/server/drawing-saver/drawing-saver.service.spec.ts
@@ -1,32 +1,40 @@
 import { TestBed, getTestBed } from '@angular/core/testing';
 import { DomSanitizer } from '@angular/platform-browser';
-import { ElementRef, Type, Renderer2} from '@angular/core';
+import { ElementRef, Type, Renderer2 } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 import { DrawingModalWindowService } from '../../drawing-modal-window/drawing-modal-window.service';
 import { DrawingLoaderService } from '../drawing-loader/drawing-loader.service';
 import { FileManagerService } from '../file-manager/file-manager.service';
 import { DrawingSaverService } from './drawing-saver.service';
-//import { createMockSVGElement } from 'src/classes/test-helpers.spec';
 import { DrawStackService } from '../../draw-stack/draw-stack.service';
+import { DrawingInfo } from '../../../../../../common/communication/DrawingInfo';
+import { DEFAULT_WHITE } from 'src/constants/color-constants';
+import { NameAndLabels } from 'src/classes/NameAndLabels';
 
-fdescribe('DrawingSaverService', () => {
+describe('DrawingSaverService', () => {
     let injector: TestBed;
     let service: DrawingSaverService;
-    // let rendererMock: Renderer2;
     let drawStackMock: DrawStackService;
+    let drawingLoaderService: DrawingLoaderService;
     let elementRefMock: ElementRef<SVGElement>;
+
     beforeEach(() => {
         TestBed.configureTestingModule({
             providers: [
                 DrawingSaverService,
-                [DomSanitizer],
                 {
                     provide: Renderer2,
-                    useValue:
-                    {
+                    useValue: {
                         createElement: () => null,
                         setAttribute: () => null,
                         appendChild: () => null,
+                    },
+                },
+                {
+                    provide: DomSanitizer,
+                    useValue: {
+                        bypassSecurityTrustResourceUrl: () => 'safeString',
                     },
                 },
                 {
@@ -39,24 +47,37 @@ fdescribe('DrawingSaverService', () => {
                 },
                 {
                     provide: DrawingModalWindowService,
-                    useValue: {},
+                    useValue: {
+                        drawingInfo: new BehaviorSubject(new DrawingInfo(0, 0, DEFAULT_WHITE)),
+                    },
+                },
+                {
+                    provide: DrawStackService,
+                    useValue: {
+                        idStack: ['1', '2', '3'],
+                    },
                 },
                 {
                     provide: DrawingLoaderService,
-                    useValue: {},
+                    useValue: {
+                        emptyDrawStack: new BehaviorSubject(true),
+                    },
                 },
                 {
                     provide: FileManagerService,
-                    useValue: {},
+                    useValue: {
+                        postDrawing: () => null,
+                    },
                 },
             ],
         });
         service = TestBed.get(DrawingSaverService);
         injector = getTestBed();
         service = injector.get(DrawingSaverService);
-        //rendererMock = injector.get<Renderer2>(Renderer2 as Type<Renderer2>);
+        drawingLoaderService = injector.get(DrawingLoaderService);
         drawStackMock = injector.get<DrawStackService>(DrawStackService as Type<DrawStackService>);
         elementRefMock = injector.get<ElementRef>(ElementRef as Type<ElementRef>);
+
         service.initializeDrawingSaverService(elementRefMock, drawStackMock);
     });
 
@@ -64,9 +85,41 @@ fdescribe('DrawingSaverService', () => {
         expect(service).toBeTruthy();
     });
 
-    it('should set values retrieved from workzone when initializeDrawingSaverService is called ', () => {
-        //const TEST_REF: ElementRef<SVGElement> = createMockSVGElement() as unknown as ElementRef<SVGElement>;
+    it('should create and return a url to download drawing file', () => {
+        const url = service.getLocalFileDownloadUrl();
 
-        expect(service).toBeTruthy();
+        expect(url).toEqual('safeString');
+    });
+
+    it('should display error message if draw stack is empty on sendFileToServer call', () => {
+        const nameAndLabels = new NameAndLabels('name', ['label1', 'label2']);
+        drawingLoaderService.emptyDrawStack.next(true);
+
+        service.sendFileToServer(nameAndLabels);
+
+        expect(service.currentIsSaved).toBeFalsy;
+        expect(service.currentErrorMesaage.value).toEqual('Aucun dessin dans le zone de travail!');
+    });
+
+    it('should post valid name and labels if draw stack is not empty on sendFileToServer call', () => {
+        const SPY = spyOn(service, 'postDrawing');
+
+        const nameAndLabels = new NameAndLabels('name', ['label1', 'label2']);
+        drawingLoaderService.emptyDrawStack.next(false);
+
+        service.sendFileToServer(nameAndLabels);
+
+        expect(SPY).toHaveBeenCalledWith(nameAndLabels);
+    });
+
+    it('should do nothing if draw stack is not empty and name and lables is not valid on sendFileToServer call', () => {
+        const SPY = spyOn(service, 'postDrawing');
+
+        const nameAndLabels = new NameAndLabels('', ['label1', 'label2']);
+        drawingLoaderService.emptyDrawStack.next(false);
+
+        service.sendFileToServer(nameAndLabels);
+
+        expect(SPY).not.toHaveBeenCalled();
     });
 });
