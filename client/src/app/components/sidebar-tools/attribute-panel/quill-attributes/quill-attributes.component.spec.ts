831cd3789ba94e7f1ba7c2930d8bbb4a2dc803b3
Noboru Yoshida <noboruyoshida1997@gmail.com>
2019-11-15 16:06:04 -0500

    NY & LP : Add test and style files for quill attributes component

---
+++ client/src/app/components/sidebar-tools/attribute-panel/quill-attributes/quill-attributes.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { QuillAttributesComponent } from './quill-attributes.component';
+
+describe('QuillAttributesComponent', () => {
+  let component: QuillAttributesComponent;
+  let fixture: ComponentFixture<QuillAttributesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ QuillAttributesComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(QuillAttributesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
