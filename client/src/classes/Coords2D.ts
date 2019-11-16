563dad2565b262b8a6e5e07175fea43a24c52ca5
Xi Chen <xishec@gmail.com>
2019-11-16 18:10:48 -0500

    XS : D Add method to calculate distance between 2 points

--- client/src/classes/Coords2D.ts
+++ client/src/classes/Coords2D.ts
@@ -6,4 +6,8 @@ export class Coords2D {
         this.x = x;
         this.y = y;
     }
+
+    distanceTo(point: Coords2D): number {
+        return Math.sqrt((Math.abs(point.x - this.x) ^ 2) + (Math.abs(point.y - this.y) ^ 2));
+    }
 }
