d042085b7c486ef9f5651aa5f915b3c5374153b4
Kevi <kevin.ciliento@polymtl.ca>
2019-11-18 07:29:40 -0500

    KC : D Add magnetism component

---
+++ client/src/app/components/sidebar-tools/attribute-panel/magnetism-attributes/magnetism-attributes.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MagnetismAttributesComponent } from './magnetism-attributes.component';
+
+describe('MagnetismAttributesComponent', () => {
+  let component: MagnetismAttributesComponent;
+  let fixture: ComponentFixture<MagnetismAttributesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ MagnetismAttributesComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MagnetismAttributesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client/src/app/components/sidebar-tools/attribute-panel/magnetism-attributes/magnetism-attributes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-magnetism-attributes',
+  templateUrl: './magnetism-attributes.component.html',
+  styleUrls: ['./magnetism-attributes.component.scss']
+})
+export class MagnetismAttributesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
