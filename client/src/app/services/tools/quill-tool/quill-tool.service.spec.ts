b6b23c3ef6394a2490395e0c9681feb5c327e7d4
Noboru Yoshida <noboruyoshida1997@gmail.com>
2019-11-19 17:53:29 -0500

    NY : T Modify foreach in test

--- client/src/app/services/tools/quill-tool/quill-tool.service.spec.ts
+++ client/src/app/services/tools/quill-tool/quill-tool.service.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed, getTestBed } from '@angular/core/testing';
 
 import { QuillToolService } from './quill-tool.service';
-import { Renderer2, ElementRef } from '@angular/core';
+import { Renderer2, ElementRef, Type } from '@angular/core';
 import { DrawStackService } from '../../draw-stack/draw-stack.service';
 
 fdescribe('QuillToolService', () => {
     let injector: TestBed;
     let service: QuillToolService;
+    let rendererMock: Renderer2;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -32,11 +33,14 @@ fdescribe('QuillToolService', () => {
         });
         injector = getTestBed();
         service = injector.get(QuillToolService);
+
+        rendererMock = injector.get<Renderer2>(Renderer2 as Type<Renderer2>);
+        const drawStackMock = injector.get<DrawStackService>(DrawStackService as Type<DrawStackService>);
+        const elementRefMock = injector.get<ElementRef>(ElementRef as Type<ElementRef>);
+        service.initializeService(elementRefMock, rendererMock, drawStackMock);
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
     });
-
-    
 });
