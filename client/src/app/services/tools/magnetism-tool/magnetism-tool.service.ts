566a9cc151f823bb865fab792d4d1840838e2cd9
Roman <Romazho123@gmail.com>
2019-11-22 18:59:19 -0500

    RZ & KC : D Add magnetize X and Y functions

--- client/src/app/services/tools/magnetism-tool/magnetism-tool.service.ts
+++ client/src/app/services/tools/magnetism-tool/magnetism-tool.service.ts
@@ -1,49 +1,56 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { CONTROL_POINTS, GRID_SIZE } from 'src/constants/tool-constants';
 import { GridToolService } from '../grid-tool/grid-tool.service';
-import { SelectionToolService } from '../selection-tool/selection-tool.service';
+//import { SelectionToolService } from '../selection-tool/selection-tool.service';
 import { Coords2D } from 'src/classes/Coords2D';
 import { BehaviorSubject } from 'rxjs';
 import { DrawingLoaderService } from '../../server/drawing-loader/drawing-loader.service';
+import { Selection } from 'src/classes/selection/selection';
 
 @Injectable({
     providedIn: 'root',
 })
-export class MagnetismToolService implements OnInit {
+export class MagnetismToolService {
     currentPoint: CONTROL_POINTS;
     currentPointPosition: Coords2D;
     currentGridSize: number;
 
+    selection: Selection;
+
     state: BehaviorSubject<boolean> = new BehaviorSubject(false);
 
-    magnetize(deltaX: number, deltaY: number) {}
     constructor(
         private gridToolService: GridToolService,
-        private selectionToolService: SelectionToolService,
+
         private drawingLoaderService: DrawingLoaderService,
     ) {
         // Prendre du tool selector si ca chie
         this.currentPoint = CONTROL_POINTS.TopLeft;
         this.currentPointPosition = new Coords2D(0, 0);
         this.currentGridSize = GRID_SIZE.Default;
-    }
 
-    ngOnInit() {
         this.gridToolService.size.subscribe((size: number) => {
             this.currentGridSize = size;
+            console.log(this.currentGridSize);
         });
+    }
+
+    //  ngOnInit() {}
+
+    initializeService(selection: Selection) {
+        this.selection = selection;
     }
 
     updateControlPointPosition(): void {
         if (this.currentPoint === CONTROL_POINTS.CenterMiddle) {
-            const selectionBox: SVGRectElement = this.selectionToolService.selection.selectionBox;
-            const x = (selectionBox.x.baseVal.value + selectionBox.width.baseVal.value) / 2;
-            const y = (selectionBox.y.baseVal.value + selectionBox.height.baseVal.value) / 2;
+            const selectionBox: SVGRectElement = this.selection.selectionBox;
+            const x = selectionBox.x.baseVal.value + selectionBox.width.baseVal.value / 2;
+            const y = selectionBox.y.baseVal.value + selectionBox.height.baseVal.value / 2;
             this.currentPointPosition.x = x;
             this.currentPointPosition.y = y;
         } else {
-            const x = this.selectionToolService.selection.controlPoints[this.currentPoint].cx.baseVal.value;
-            const y = this.selectionToolService.selection.controlPoints[this.currentPoint].cy.baseVal.value;
+            const x = this.selection.controlPoints[this.currentPoint].cx.baseVal.value;
+            const y = this.selection.controlPoints[this.currentPoint].cy.baseVal.value;
             this.currentPointPosition.x = x;
             this.currentPointPosition.y = y;
         }
@@ -57,5 +64,30 @@ export class MagnetismToolService implements OnInit {
 
     switchState(): void {
         this.state.value ? this.changeState(false) : this.changeState(true);
+    }
+
+    // grid size n'est pas bon
+    // remainder semble aussi etre weird
+    magnetizeX(deltaX: number): number {
+        let remainder = deltaX % this.currentGridSize;
+        console.log(deltaX);
+
+        console.log('remainder: ' + remainder);
+        console.log('grid size: ' + this.currentGridSize);
+
+        if (remainder < this.currentGridSize / 2) {
+            return deltaX - remainder;
+        } else {
+            return deltaX + remainder;
+        }
+    }
+
+    magnetizeY(deltaY: number): number {
+        let remainder = deltaY % this.currentGridSize;
+        if (remainder < this.currentGridSize / 2) {
+            return deltaY - remainder;
+        } else {
+            return deltaY + remainder;
+        }
     }
 }
