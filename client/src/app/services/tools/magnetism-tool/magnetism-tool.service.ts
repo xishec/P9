dd2172824746ddc69180ca82fef00f6e87cc55f7
Kevi <kevin.ciliento@polymtl.ca>
2019-11-22 15:12:09 -0500

    KC : D Add update controlpoint function

--- client/src/app/services/tools/magnetism-tool/magnetism-tool.service.ts
+++ client/src/app/services/tools/magnetism-tool/magnetism-tool.service.ts
@@ -1,9 +1,40 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { CONTROL_POINTS } from 'src/constants/tool-constants';
+import { GridToolService } from '../grid-tool/grid-tool.service';
+import { SelectionToolService } from '../selection-tool/selection-tool.service';
+import { Coords2D } from 'src/classes/Coords2D';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
-export class MagnetismToolService {
+export class MagnetismToolService implements OnInit {
+    currentPoint: CONTROL_POINTS;
+    currentPointPosition: Coords2D;
+    currentGridSize: number;
 
-  constructor() { }
+    constructor(private gridToolService: GridToolService, private selectionToolService: SelectionToolService) {
+        // Prendre du tool selector si ca chie
+        this.currentPointPosition = new Coords2D(0, 0);
+    }
+
+    ngOnInit() {
+        this.gridToolService.size.subscribe((size: number) => {
+            this.currentGridSize = size;
+        });
+    }
+
+    updateControlPointPosition(): void {
+        if (this.currentPoint === CONTROL_POINTS.CenterMiddle) {
+            const selectionBox: SVGRectElement = this.selectionToolService.selection.selectionBox;
+            const x = (selectionBox.x.baseVal.value + selectionBox.width.baseVal.value) / 2;
+            const y = (selectionBox.y.baseVal.value + selectionBox.height.baseVal.value) / 2;
+            this.currentPointPosition.x = x;
+            this.currentPointPosition.y = y;
+        } else {
+            const x = this.selectionToolService.selection.controlPoints[this.currentPoint].cx.baseVal.value;
+            const y = this.selectionToolService.selection.controlPoints[this.currentPoint].cy.baseVal.value;
+            this.currentPointPosition.x = x;
+            this.currentPointPosition.y = y;
+        }
+    }
 }
