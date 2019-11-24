671dfa735d268f3f44cb931232561303bcbc5c30
Roman <Romazho123@gmail.com>
2019-11-23 19:00:42 -0500

    RZ : T Make the "it should create" test pass for the component

--- client/src/app/components/sidebar-tools/attribute-panel/magnetism-attributes/magnetism-attributes.component.spec.ts
+++ client/src/app/components/sidebar-tools/attribute-panel/magnetism-attributes/magnetism-attributes.component.spec.ts
@@ -1,25 +1,42 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
 
+import { ShortcutManagerService } from 'src/app/services/shortcut-manager/shortcut-manager.service';
+import { AttributesManagerService } from 'src/app/services/tools/attributes-manager/attributes-manager.service';
 import { MagnetismAttributesComponent } from './magnetism-attributes.component';
 
 describe('MagnetismAttributesComponent', () => {
-  let component: MagnetismAttributesComponent;
-  let fixture: ComponentFixture<MagnetismAttributesComponent>;
+    let component: MagnetismAttributesComponent;
+    let fixture: ComponentFixture<MagnetismAttributesComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MagnetismAttributesComponent ],
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [MagnetismAttributesComponent],
+            schemas: [NO_ERRORS_SCHEMA],
+            providers: [FormBuilder],
+        })
+            .overrideComponent(MagnetismAttributesComponent, {
+                set: {
+                    providers: [
+                        {
+                            provide: AttributesManagerService,
+                            useValue: {},
+                        },
+                        {
+                            provide: ShortcutManagerService,
+                            useValue: {},
+                        },
+                    ],
+                },
+            })
+            .compileComponents();
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MagnetismAttributesComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(MagnetismAttributesComponent);
+        component = fixture.componentInstance;
+    }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
