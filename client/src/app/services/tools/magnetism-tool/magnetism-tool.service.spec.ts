75bb7830477e44a089e70c8f7194b1038512ba31
Roman <Romazho123@gmail.com>
2019-11-23 19:06:53 -0500

    RZ : T Make  the "it should be created" test pass for the service

--- client/src/app/services/tools/magnetism-tool/magnetism-tool.service.spec.ts
+++ client/src/app/services/tools/magnetism-tool/magnetism-tool.service.spec.ts
@@ -1,12 +1,46 @@
-import { TestBed } from '@angular/core/testing';
+import { ElementRef, Renderer2 } from '@angular/core';
+import { getTestBed, TestBed } from '@angular/core/testing';
+import { MatDialog, MatSnackBar } from '@angular/material';
 
+import { provideAutoMock } from 'src/classes/test.helper.msTeams.spec';
+import { DrawStackService } from '../../draw-stack/draw-stack.service';
 import { MagnetismToolService } from './magnetism-tool.service';
 
-describe('MagnetismToolService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+fdescribe('MagnetismToolService', () => {
+    let injector: TestBed;
+    let service: MagnetismToolService;
 
-  it('should be created', () => {
-    const service: MagnetismToolService = TestBed.get(MagnetismToolService);
-    expect(service).toBeTruthy();
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                MagnetismToolService,
+                {
+                    provide: MatDialog,
+                    useValue: {},
+                },
+                {
+                    provide: Renderer2,
+                    useValue: {
+                        setAttribute: () => null,
+                        createElement: () => null,
+                        appendChild: () => null,
+                        removeChild: () => null,
+                    },
+                },
+                {
+                    provide: MatSnackBar,
+                    useValue: {},
+                },
+                provideAutoMock(ElementRef),
+                provideAutoMock(DrawStackService),
+            ],
+        });
+
+        injector = getTestBed();
+        service = injector.get(MagnetismToolService);
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
