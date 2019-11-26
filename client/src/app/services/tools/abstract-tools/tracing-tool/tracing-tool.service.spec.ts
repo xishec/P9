6f921caee09fb369c4a7590f35d93d59785fa9df
Kevi <kevin.ciliento@polymtl.ca>
2019-11-26 17:03:25 -0500

    KC : T Adapt Tracing tool test to spray modification

--- client/src/app/services/tools/abstract-tools/tracing-tool/tracing-tool.service.spec.ts
+++ client/src/app/services/tools/abstract-tools/tracing-tool/tracing-tool.service.spec.ts
@@ -158,7 +158,7 @@ describe('TracingToolService', () => {
     it('when createSVGCircle setAttribute is called 4 times', () => {
         service.createSVGCircle(MOCK_X, MOCK_Y);
 
-        expect(spyOnSetAttribute).toHaveBeenCalledTimes(4);
+        expect(spyOnSetAttribute).toHaveBeenCalledTimes(5);
     });
 
     it('when createSVGPath then setAttribute is called 4 times and before appendChild', () => {
