3f7f637ef7c4d032dfd11e489b2d29cce0675800
Xi Chen <xishec@gmail.com>
2019-11-25 22:20:18 -0500

    XS : D Add methods for Coords2D class

--- client/src/classes/Coords2D.ts
+++ client/src/classes/Coords2D.ts
@@ -10,4 +10,17 @@ export class Coords2D {
     distanceTo(point: Coords2D): number {
         return Math.pow(point.x - this.x, 2) + Math.pow(point.y - this.y, 2);
     }
+
+    isValide(): boolean {
+        return this.x >= 0 && this.y >= 0;
+    }
+
+    setCoords(coords2D: Coords2D): void {
+        this.x = coords2D.x;
+        this.y = coords2D.y;
+    }
+
+    clone(): Coords2D {
+        return new Coords2D(this.x, this.y);
+    }
 }
