981cd3d30800472b4cc568dfbfdba9ab09c5171e
Xi Chen <xishec@gmail.com>
2019-12-05 10:57:14 -0500

    XS : D Add prod config

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import { IEnvironment } from './IEnvironment';
 
 export const environment: IEnvironment = {
     production: true,
-    BASE_URL: '',
+    BASE_URL: 'https://cors-anywhere.herokuapp.com/https://p9-server.web.app',
 };
